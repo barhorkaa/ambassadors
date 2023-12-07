6a1de219a0d9f7f3ed3b33d771acf72726e83a36
Barbora Hornakova <xhornak2@fi.muni.cz>
2023-12-07 12:01:32 +0100

    feat(migration/run): created run file for migrations

---
+++ src/database/runMigration.ts
@@ -0,0 +1,57 @@
+import * as path from 'path'
+import { createPool } from 'mysql2'
+import { promises as fs } from 'fs'
+import {
+  Kysely,
+  Migrator,
+  FileMigrationProvider,
+  MysqlDialect,
+} from 'kysely'
+import {Database} from "@/database/types";
+
+import * as dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' })
+
+async function migrateToLatest() {
+  const db = new Kysely<Database>({
+    dialect: new MysqlDialect({
+      pool: createPool({
+        host: process.env['MYSQL_HOST'],
+        port: +process.env['MYSQL_PORT']!,
+        user: process.env['MYSQL_USER'],
+        password: process.env['MYSQL_PASSWORD'],
+        database: process.env['MYSQL_DATABASE'],
+      })
+    }),
+  })
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      // This needs to be an absolute path.
+      migrationFolder: path.join(__dirname, 'migrations'),
+    }),
+  })
+
+  const { error, results } = await migrator.migrateToLatest()
+
+  results?.forEach((it) => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`)
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`)
+    }
+  })
+
+  if (error) {
+    console.error('failed to migrate')
+    console.error(error)
+    process.exit(1)
+  }
+
+  await db.destroy()
+}
+
+migrateToLatest()
