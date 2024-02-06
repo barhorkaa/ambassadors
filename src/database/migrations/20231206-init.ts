54cb5e52ee0bfc9eb29279ac54536be2875dbfa1
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-06 21:04:56 +0100

    fix(migrations): changed id column types from number to uuid (auth problems)

--- src/database/migrations/20231206-init.ts
+++ src/database/migrations/20231206-init.ts
@@ -1,9 +1,10 @@
 import { Kysely, sql } from 'kysely'
+import {randomUUID} from "crypto";
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable('ambassador')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(randomUUID()))
     .addColumn('uco', 'integer', (col) => col.notNull())
     .addColumn('name', 'varchar(50)', (col) => col.notNull())
     .addColumn('email', 'varchar(50)', (col) => col.notNull())
@@ -21,7 +22,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('event')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(randomUUID()))
     .addColumn('name', 'varchar(69)', (col) => col.notNull())
     .addColumn('date', 'timestamp', (col) => col.notNull())
     .addColumn('event_type_id', 'integer', (col) =>
@@ -40,7 +41,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('eventType')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(randomUUID()))
     .addColumn('name', 'varchar(50)', (col) => col.notNull())
     .addColumn('description', 'varchar(500)', (col) => col.notNull())
     .addColumn('instructions', 'varchar(500)', (col) => col.notNull())
@@ -57,7 +58,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('report')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(randomUUID()))
     .addColumn('ambassador_id', 'integer', (col) => col.references('ambassador.id').notNull())
     .addColumn('number_of_attendees', 'integer', (col) => col.notNull())
     .addColumn('notes', 'varchar(500)')
@@ -75,7 +76,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('material')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(randomUUID()))
     .addColumn('name', 'varchar(50)', (col) => col.notNull())
     .addColumn('description', 'varchar(500)')
     .addColumn('created_at', 'timestamp', (col) =>
@@ -91,7 +92,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('registerForm')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(randomUUID()))
     .addColumn('ambassador_id', 'integer', (col) => col.references('ambassador.id').notNull())
     .addColumn('created_at', 'timestamp', (col) =>
       col.defaultTo(sql`now()`).notNull()
@@ -106,7 +107,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('eventAmbassador')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(randomUUID()))
     .addColumn('ambassador_id', 'integer', (col) => col.references('ambassador.id').notNull())
     .addColumn('event_id', 'integer', (col) => col.references('event.id').notNull())
     .addColumn('created_at', 'timestamp', (col) =>
@@ -122,7 +123,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('materialReport')
-    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(randomUUID()))
     .addColumn('material_id', 'integer', (col) => col.references('material.id').notNull())
     .addColumn('report_id', 'integer', (col) => col.references('report.id').notNull())
     .addColumn('amount', 'integer')
