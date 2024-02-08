b77d2ec5171312abef53183f4ff00fe3f50774de
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-08 11:57:51 +0100

    feat(login): does not work

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,29 @@
-import LoginForm from "../ui/login-form";
-import React from "react";
-import NavBar from "../ui/nav-bar";
-import {useFormState} from "react-dom";
+// import LoginForm from "../ui/login-form";
+// import React from "react";
+// import NavBar from "../ui/nav-bar";
+// import {useFormState} from "react-dom";
+// import {authenticate} from "../lib/actions";
+
+// export default function Login() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center">
+//       <NavBar></NavBar>
+//       <LoginForm/>
+//     </main>
+//   )
+// }
+
+// import { authenticate } from '/app/lib/actions'
+
 import {authenticate} from "../lib/actions";
 
 export default function Login() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <NavBar></NavBar>
-      <LoginForm/>
-    </main>
+    <form action={authenticate}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
   )
 }
 
