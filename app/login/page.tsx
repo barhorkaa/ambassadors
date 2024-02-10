2004bc2265d75f3c1a0feace1cf711e8d43ef791
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-10 17:36:37 +0100

    fix(layout): moved navbar to layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ import LoginForm from "../ui/login-form";
 export default function Login() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <NavBar></NavBar>
+      {/*<NavBar></NavBar>*/}
       <LoginForm></LoginForm>
     </main>
   )
