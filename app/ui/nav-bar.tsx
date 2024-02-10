57a261dee06655e8f4b0a2203144a6ddcedb0e85
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-10 17:38:21 +0100

    feat(navBar): changed navBar with DaisyUI

--- app/ui/nav-bar.tsx
+++ app/ui/nav-bar.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return(
-    <nav className="relative select-none bg-amber-200 lg:flex lg:items-stretch w-full">
-      <div className="flex flex-no-shrink items-stretch h-12">
-        <a href="/" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal  no-underline flex items-center hover:bg-grey-dark">Home</a>
+    <nav className="navbar bg-amber-200">
+      <div className="flex-1">
+        <Link className="btn btn-ghost text-xl" href={"/"}>AmbassadorsFIMU</Link>
       </div>
-      <div className="lg:flex lg:items-stretch lg:flex-no-shrink lg:flex-grow">
-        <div className="lg:flex lg:items-stretch lg:justify-end ml-auto">
-          <a href="/login" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal no-underline flex items-center hover:bg-grey-dark">Login</a>
-          <a href="/register" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal no-underline flex items-center hover:bg-grey-dark">Register</a>
-        </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li><Link href={"/login"}>Login</Link></li>
+          <li><Link href={"/"}>Register</Link></li>
+        </ul>
       </div>
     </nav>
   );
