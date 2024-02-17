79b67af6b8ff7e8095733c1c8f118cd645c0ea34
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-17 18:01:49 +0100

    fix(database/user): renamed to getUserByEmail

--- database/repository/user.ts
+++ database/repository/user.ts
@@ -2,6 +2,6 @@ import {db} from "@/database/database";
 import {User} from "kysely-codegen";
 import {Selectable} from "kysely";
 
-export async function getUser(email: string): Promise<Selectable<User> | undefined> {
+export async function getUserByEmail(email: string): Promise<Selectable<User> | undefined> {
   return await db.selectFrom("user").where("email", "=", email).selectAll().executeTakeFirst();
 }
