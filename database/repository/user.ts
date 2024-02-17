47a84ae624b6dd2ad705ffa4abf00472abc26ce5
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-17 18:02:07 +0100

    feat(database/user): added function getUserRole

--- database/repository/user.ts
+++ database/repository/user.ts
@@ -5,3 +5,16 @@ import {Selectable} from "kysely";
 export async function getUserByEmail(email: string): Promise<Selectable<User> | undefined> {
   return await db.selectFrom("user").where("email", "=", email).selectAll().executeTakeFirst();
 }
+
+export async function getUserRole(email: string) : Promise<String> {
+  try {
+    const role = await db.selectFrom('user').where('email', '=', email).select('type').executeTakeFirstOrThrow();
+    return role.type;
+  } catch (e) {
+    throw new Error('Failed to fetch user role:');
+  }
+}
+
+export async function getUser() {
+
+}
