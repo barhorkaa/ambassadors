f6e4eb311a832dd5c98f5b9ab0978975e754b45d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-20 16:02:44 +0100

    feat(login/page): added padding for better displaying

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
+        <div className="text-center lg:text-left p-4">
           <h1 className="text-5xl font-bold">Přihlašte se a naplánujte svou akci!</h1>
           <p className="py-6">Po přihlášení budete míť přístup ke všem akcím a možnost se na přihlásit. Pojďte s námi představit Fakultu informatiky Masarykovy Univerzity světu!</p>
         </div>
