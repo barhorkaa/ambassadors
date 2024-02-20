3f5b63caae56ec22f92f4ad2fc7296a3f35012e9
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-20 15:43:00 +0100

    fox(login/page): improved login page visuals

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,14 @@ import LoginForm from "../ui/login-form";
 
 export default function Login() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <LoginForm></LoginForm>
-    </main>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Přihlašte se a naplánujte svou akci!</h1>
+          <p className="py-6">Po přihlášení budete míť přístup ke všem akcím a možnost se na přihlásit. Pojďte s námi představit Fakultu informatiky Masarykovy Univerzity světu!</p>
+        </div>
+        <LoginForm/>
+      </div>
+    </div>
   )
 }
