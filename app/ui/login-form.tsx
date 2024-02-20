10e968a8d17ff5681b70c578cee37c764e1a56a2
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-20 15:43:30 +0100

    fix(login/form): improved login form visuals using Hero

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -10,67 +10,101 @@ export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-    <form action={dispatch} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={'mb-3 text-2xl'}>
-          Please log in to continue.
-        </h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-              {/*<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
-            </div>
-          </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-              {/*<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
-            </div>
-          </div>
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form action={dispatch} className="card-body">
+        <div className="form-control">
+          <label className="label" htmlFor="email">
+            <span className="label-text">Email</span>
+          </label>
+          <input id="email" type="email" name="email" placeholder="email" className="input input-bordered" required />
         </div>
-        <LoginButton />
+        <div className="form-control">
+          <label className="label"
+                 htmlFor="password">
+            <span className="label-text">Password</span>
+          </label>
+          <input id="password" type="password" name="password" placeholder="password" className="input input-bordered" required />
+          {/*<label className="label">*/}
+          {/*  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>*/}
+          {/*</label>*/}
+        </div>
+        {/*<div className="form-control mt-6">*/}
+        {/*  <button className="btn btn-primary">Login</button>*/}
+        {/*</div>*/}
+        <LoginButton/>
         <div
-          className="flex h-8 items-end space-x-1"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          {errorMessage && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorMessage}</p>
-            </>
-          )}
+            className="flex h-8 items-end space-x-1"
+            aria-live="polite"
+            aria-atomic="true"
+          >
+            {errorMessage && (
+              <>
+                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                <p className="text-sm text-red-500">{errorMessage}</p>
+              </>
+            )}
         </div>
-      </div>
     </form>
+  </div>
+
+    // <form action={dispatch} className="space-y-3">
+    //   <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+    //     <div className="w-full">
+    //       <div>
+    //         <label
+    //           className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+    //           htmlFor="email"
+    //         >
+    //           Email
+    //         </label>
+    //         <div className="relative">
+    //           <input
+    //             className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+    //             id="email"
+    //             type="email"
+    //             name="email"
+    //             placeholder="Enter your email address"
+    //             required
+    //           />
+    //           {/*<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
+    //         </div>
+    //       </div>
+    //       <div className="mt-4">
+    //         <label
+    //           className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+    //           htmlFor="password"
+    //         >
+    //           Password
+    //         </label>
+    //         <div className="relative">
+    //           <input
+    //             className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+    //             id="password"
+    //             type="password"
+    //             name="password"
+    //             placeholder="Enter password"
+    //             required
+    //             minLength={6}
+    //           />
+    //           {/*<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />*/}
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <LoginButton />
+    //     <div
+    //       className="flex h-8 items-end space-x-1"
+    //       aria-live="polite"
+    //       aria-atomic="true"
+    //     >
+    //       {errorMessage && (
+    //         <>
+    //           <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+    //           <p className="text-sm text-red-500">{errorMessage}</p>
+    //         </>
+    //       )}
+    //     </div>
+    //   </div>
+    // </form>
   );
 }
 
