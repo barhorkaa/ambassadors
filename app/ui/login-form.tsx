284e80ffe51e6ed2bc81159d8db78f68a8587512
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-20 16:01:54 +0100

    feat(login/form): changed buttons color

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -49,8 +49,8 @@ function LoginButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    <button className="mt-4 w-full bg-amber-200 p-2" aria-disabled={pending}>
+      Log in
     </button>
   );
 }
