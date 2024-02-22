361f28d42018790e95b5f38844fc4512bb4302ca
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-22 20:26:38 +0100

    feat(api/user): created POST method for new user creation

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,6 @@
 // import {getUser} from "@/database/repository/user";
 import { type NextRequest } from 'next/server'
-import {getUserByEmail} from "../../../database/repository/user";
+import {createNewUser, getUserByEmail} from "@/database/repository/user";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
@@ -11,4 +11,33 @@ export async function GET(request: NextRequest) {
   }
   const user = await getUserByEmail(email);
   return Response.json(user);
+}
+
+export async function POST(request: NextRequest) {
+  console.log("Successfully called fetch POST");
+  console.log(request.body);
+  const searchParams = request.nextUrl.searchParams
+  const name = searchParams.get("name");
+  const email = searchParams.get("email");
+  const password = searchParams.get("password");
+  const uco = searchParams.get("uco");
+  const phone_number = searchParams.get("phone_number");
+  console.log(name, email, password, uco, phone_number);
+  console.log("name", name);
+  console.log("email", email);
+
+
+  if (!email || !name || !password || !uco || !phone_number) {
+    console.log("something is off")
+    return Response.json(false);
+  }
+
+  try {
+    await createNewUser(name, email, password, Number(uco), phone_number);
+  }
+  catch (error) {
+    console.log(error);
+  }
+
+  return Response.json(true);
 }
