2891c6fd5b45e0db912cbd0624f82bedf3989d9e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-22 20:25:40 +0100

    feat(register/form): created button for form submission

--- app/ui/register-form.tsx
+++ app/ui/register-form.tsx
@@ -45,9 +45,11 @@ export default function RegisterForm() {
 }
 
 function RegisterButton() {
-  return(
-    <div>
+  const { pending } = useFormStatus();
 
-    </div>
+  return (
+    <button className="mt-4 w-full bg-amber-200 p-2" aria-disabled={pending}>
+      Register
+    </button>
   )
 }
