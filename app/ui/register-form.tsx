33ed42daa06b0c96b3d9f360daf70d6b4a82720e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-22 20:25:59 +0100

    feat(register/form): added error handling for form

--- app/ui/register-form.tsx
+++ app/ui/register-form.tsx
@@ -39,6 +39,19 @@ export default function RegisterForm() {
           <input id="phone_number" type="number" name="phone_number" placeholder="Telefónní číslo" className="input input-bordered" required />
         </div>
         <RegisterButton/>
+        <div
+        className="flex h-8 items-end space-x-1"
+        aria-live="polite"
+        aria-atomic="true"
+        >
+          {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
+        </div>
+
       </form>
     </div>
   );
