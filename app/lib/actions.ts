d6ceb596048c9bc4371f9a159cd30379263f8e40
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-22 20:28:24 +0100

    feat(actions): created action method for handling new user registration

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,8 @@
 'use server';
 
 import { AuthError } from 'next-auth';
-import {signIn} from "../../auth";
-// import {signIn} from "../../../auth";
-
-// ...
+import {signIn} from "@/auth";
+import bcrypt from 'bcryptjs';
 
 export async function authenticate(
   prevState: string | undefined,
@@ -23,4 +21,29 @@ export async function authenticate(
     }
     throw error;
   }
+}
+
+export async function createUser(prevState: string | undefined, formData: FormData) {
+  try {
+    console.log("Successfully called createUser");
+    console.log(formData.values());
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const password_hashed = await bcrypt.hash(password, 11);
+    const uco = Number(formData.get("uco") as string);
+    const phone_number = formData.get("phone_number") as string;
+
+    console.log("Calling fetch");
+    await fetch(`http://localhost:3000/api/user?name=${name}&email=${email}&password=${password_hashed}&uco=${uco}&phone_number=${phone_number}`,
+      {
+        method: 'POST',
+      });
+    console.log("Called fetch");
+    // await createNewUser(name, email, password, uco, phone_number)
+  } catch (error) {
+    return "Something went wrong"
+    throw error;
+  }
+
 }
