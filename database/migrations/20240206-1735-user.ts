6e2a82a767f486f64443812df0b4efe3683c5379
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-02-28 16:58:30 +0100

    fix(migration/user): changed column name from type to role in user

--- database/migrations/20240206-1735-user.ts
+++ database/migrations/20240206-1735-user.ts
@@ -4,13 +4,13 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable('user')
     .addColumn('password', 'varchar(100)', (col) => col.notNull())
-    .addColumn('type', sql`enum('manager', 'ambassador')`, (col) => col.notNull().defaultTo('ambassador'))
+    .addColumn('role', sql`enum('manager', 'ambassador')`, (col) => col.notNull().defaultTo('ambassador'))
     .execute()
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
   await db.schema.alterTable('user')
     .dropColumn('password')
-    .dropColumn('type')
+    .dropColumn('role')
     .execute()
 }
