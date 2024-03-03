a5d83164850387f5caee34e55b2e78acf73e7a4f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-03 23:05:22 +0100

    fix(motivation/form): fixed form types and props

--- app/ui/register-motivation-form.tsx
+++ app/ui/register-motivation-form.tsx
@@ -5,7 +5,7 @@ import {useFormState, useFormStatus} from "react-dom";
 import {createMotivationForm} from "@/app/lib/actions";
 
 export default function RegisterMotivationForm(props: {id: string}) {
-  const [errorMessage, dispatch] = useFormState(createMotivationForm, props.id);
+  const [errorMessage, dispatch] = useFormState(createMotivationForm, undefined);
 
   return (
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -21,28 +21,28 @@ export default function RegisterMotivationForm(props: {id: string}) {
           <label className="label" htmlFor="who">
             <span className="label-text">Doporučil vám někto přidat se k programu? Pokud ano, kto?</span>
           </label>
-          <input id="who" type="email" name="who" placeholder="Ne / Ano + kto" className="input input-bordered" required />
+          <input id="who" type="text" name="who" placeholder="Ne / Ano + kto" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label"
                  htmlFor="goals">
             <span className="label-text">Co byste chtěli jako ambasador dosáhnout?</span>
           </label>
-          <input id="goals" type="password" name="goals" placeholder="Vaše cíle" className="input input-bordered" required />
+          <input id="goals" type="text" name="goals" placeholder="Vaše cíle" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label"
-                 htmlFor="preffered">
+                 htmlFor="preferred_events">
             <span className="label-text">Jakých akcí byste se chtěli převážně zúčastňovat?</span>
           </label>
-          <input id="preffered" type="number" name="preffered" placeholder="Výjezdy, Dny otevřených dveří, Veltrhy ..." className="input input-bordered" required />
+          <input id="preferred_events" type="text" name="preferred_events" placeholder="Výjezdy, Dny otevřených dveří, Veltrhy ..." className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label"
                  htmlFor="time">
             <span className="label-text">Kolik času budete mít na ambasadorskou činnost?</span>
           </label>
-          <input id="time" type="number" name="time" placeholder="Vaše časové možnosti" className="input input-bordered" required />
+          <input id="time" type="text" name="time" placeholder="Vaše časové možnosti" className="input input-bordered" required />
         </div>
         <input id="id" type="hidden" name="id" value={props.id} className="input input-bordered" required />
         <RegisterButton/>
