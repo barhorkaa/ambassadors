f3b363742a0985b70b6390a057c261d582424b13
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-03 11:32:35 +0100

    feat(migration): fixed init migration for postgres compiling

--- database/migrations/20231206-init.ts
+++ database/migrations/20231206-init.ts
@@ -4,7 +4,7 @@ import {randomUUID} from "crypto";
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable('ambassador')
-    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`(UUID())`))
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('uco', 'integer', (col) => col.notNull())
     .addColumn('name', 'varchar(50)', (col) => col.notNull())
     .addColumn('email', 'varchar(50)', (col) => col.notNull())
@@ -21,27 +21,8 @@ export async function up(db: Kysely<any>): Promise<void> {
     .execute()
 
   await db.schema
-    .createTable('event')
-    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`(UUID())`))
-    .addColumn('name', 'varchar(69)', (col) => col.notNull())
-    .addColumn('date', 'timestamp', (col) => col.notNull())
-    .addColumn('event_type_id', 'integer', (col) =>
-      col.references('eventType.id').onDelete('cascade').notNull()
-    )
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
-    .execute()
-
-  await db.schema
     .createTable('eventType')
-    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`(UUID())`))
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('name', 'varchar(50)', (col) => col.notNull())
     .addColumn('description', 'varchar(500)', (col) => col.notNull())
     .addColumn('instructions', 'varchar(500)', (col) => col.notNull())
@@ -57,9 +38,28 @@ export async function up(db: Kysely<any>): Promise<void> {
     .execute()
 
   await db.schema
+    .createTable('event')
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn('name', 'varchar(69)', (col) => col.notNull())
+    .addColumn('date', 'timestamp', (col) => col.notNull())
+    .addColumn('event_type_id', 'char(36)', (col) =>
+      col.references('eventType.id').onDelete('cascade').notNull()
+    )
+    .addColumn('created_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull()
+    )
+    .addColumn('updated_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull()
+    )
+    .addColumn('deleted_at', 'timestamp', (col) =>
+      col.defaultTo(sql`null`)
+    )
+    .execute()
+
+  await db.schema
     .createTable('report')
-    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`(UUID())`))
-    .addColumn('ambassador_id', 'integer', (col) => col.references('ambassador.id').notNull())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn('ambassador_id', 'char(36)', (col) => col.references('ambassador.id').notNull())
     .addColumn('number_of_attendees', 'integer', (col) => col.notNull())
     .addColumn('notes', 'varchar(500)')
     .addColumn('ideas', 'varchar(500)')
@@ -76,7 +76,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('material')
-    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`(UUID())`))
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('name', 'varchar(50)', (col) => col.notNull())
     .addColumn('description', 'varchar(500)')
     .addColumn('created_at', 'timestamp', (col) =>
@@ -92,8 +92,8 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('registerForm')
-    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`(UUID())`))
-    .addColumn('ambassador_id', 'integer', (col) => col.references('ambassador.id').notNull())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn('ambassador_id', 'char(36)', (col) => col.references('ambassador.id').notNull())
     .addColumn('created_at', 'timestamp', (col) =>
       col.defaultTo(sql`now()`).notNull()
     )
@@ -107,9 +107,9 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('eventAmbassador')
-    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`(UUID())`))
-    .addColumn('ambassador_id', 'integer', (col) => col.references('ambassador.id').notNull())
-    .addColumn('event_id', 'integer', (col) => col.references('event.id').notNull())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn('ambassador_id', 'char(36)', (col) => col.references('ambassador.id').notNull())
+    .addColumn('event_id', 'char(36)', (col) => col.references('event.id').notNull())
     .addColumn('created_at', 'timestamp', (col) =>
       col.defaultTo(sql`now()`).notNull()
     )
@@ -123,9 +123,9 @@ export async function up(db: Kysely<any>): Promise<void> {
 
   await db.schema
     .createTable('materialReport')
-    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`(UUID())`))
-    .addColumn('material_id', 'integer', (col) => col.references('material.id').notNull())
-    .addColumn('report_id', 'integer', (col) => col.references('report.id').notNull())
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn('material_id', 'char(36)', (col) => col.references('material.id').notNull())
+    .addColumn('report_id', 'char(36)', (col) => col.references('report.id').notNull())
     .addColumn('amount', 'integer')
     .addColumn('created_at', 'timestamp', (col) =>
       col.defaultTo(sql`now()`).notNull()
@@ -153,12 +153,12 @@ export async function up(db: Kysely<any>): Promise<void> {
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.dropTable('ambassador').execute()
-  await db.schema.dropTable('event').execute()
-  await db.schema.dropTable('eventType').execute()
-  await db.schema.dropTable('report').execute()
-  await db.schema.dropTable('material').execute()
   await db.schema.dropTable('eventAmbassador').execute()
   await db.schema.dropTable('materialReport').execute()
+  await db.schema.dropTable('report').execute()
+  await db.schema.dropTable('material').execute()
   await db.schema.dropTable('registerForm').execute()
+  await db.schema.dropTable('event').execute()
+  await db.schema.dropTable('eventType').execute()
+  await db.schema.dropTable('ambassador').execute()
 }
