d2281f39a2d67a3c9917babc08c4e716b319b8e2
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-03 15:36:16 +0100

    fix(motivation/form): updated motivation form questions

--- app/ui/register-motivation-form.tsx
+++ app/ui/register-motivation-form.tsx
@@ -1,52 +1,61 @@
 'use client';
 
 import React from "react";
-import {useFormState} from "react-dom";
+import {useFormState, useFormStatus} from "react-dom";
 import {createMotivationForm} from "@/app/lib/actions";
-import {useSearchParams} from "next/navigation";
 
 export default function RegisterMotivationForm(props: {id: string}) {
   const [errorMessage, dispatch] = useFormState(createMotivationForm, props.id);
 
   return (
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form action={"dispatch"} className="card-body">
+      <form action={dispatch} className="card-body">
         <div className="form-control">
           <label className="label"
                  htmlFor="name">
-            <span className="label-text">Celé jméno</span>
+            <span className="label-text">Proč jste se rozhodli k nám přidat?</span>
           </label>
-          <input id="name" type="text" name="name" placeholder="Jméno" className="input input-bordered" required />
+          <input id="why" type="text" name="why" placeholder="Proč?" className="input input-bordered" required />
         </div>
         <div className="form-control">
-          <label className="label" htmlFor="email">
-            <span className="label-text">Email</span>
+          <label className="label" htmlFor="who">
+            <span className="label-text">Doporučil vám někto přidat se k programu? Pokud ano, kto?</span>
           </label>
-          <input id="email" type="email" name="email" placeholder="E-mail" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label"
-                 htmlFor="password">
-            <span className="label-text">Heslo</span>
-          </label>
-          <input id="password" type="password" name="password" placeholder="Heslo" className="input input-bordered" required />
+          <input id="who" type="email" name="who" placeholder="Ne / Ano + kto" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label"
-                 htmlFor="uco">
-            <span className="label-text">UČO</span>
+                 htmlFor="goals">
+            <span className="label-text">Co byste chtěli jako ambasador dosáhnout?</span>
           </label>
-          <input id="uco" type="number" name="uco" placeholder="UČO" className="input input-bordered" required />
+          <input id="goals" type="password" name="goals" placeholder="Vaše cíle" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label"
-                 htmlFor="phone_number">
-            <span className="label-text">Telefonní číslo</span>
+                 htmlFor="preffered">
+            <span className="label-text">Jakých akcí byste se chtěli převážně zúčastňovat?</span>
           </label>
-          <input id="phone_number" type="number" name="phone_number" placeholder="Telefónní číslo" className="input input-bordered" required />
+          <input id="preffered" type="number" name="preffered" placeholder="Výjezdy, Dny otevřených dveří, Veltrhy ..." className="input input-bordered" required />
         </div>
-        {/*<RegisterButton/>*/}
+        <div className="form-control">
+          <label className="label"
+                 htmlFor="time">
+            <span className="label-text">Kolik času budete mít na ambasadorskou činnost?</span>
+          </label>
+          <input id="time" type="number" name="time" placeholder="Vaše časové možnosti" className="input input-bordered" required />
+        </div>
+        <RegisterButton/>
       </form>
     </div>
+  )
+}
+
+function RegisterButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button className="mt-4 w-full bg-amber-200 p-2" aria-disabled={pending}>
+      Odeslat
+    </button>
   )
 }
