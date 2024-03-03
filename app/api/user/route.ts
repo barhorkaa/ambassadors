14fe6462627b153fdba3556cbd323517f882517e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-03 12:14:16 +0100

    feat(api/user): added retuning of id from new user creation

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -19,16 +19,18 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   console.log("Successfully called fetch POST");
   console.log(request.body);
+
   const searchParams = request.nextUrl.searchParams
   const name = searchParams.get("name");
   const email = searchParams.get("email");
   const password = searchParams.get("password");
   const uco = searchParams.get("uco");
   const phone_number = searchParams.get("phone_number");
+
   console.log(name, email, password, uco, phone_number);
   console.log("name", name);
   console.log("email", email);
-
+  console.log("type of password is", typeof(password))
 
   if (!email || !name || !password || !uco || !phone_number) {
     console.log("something is off")
@@ -36,12 +38,11 @@ export async function POST(request: NextRequest) {
   }
 
   try {
-    // const maybeId = await createNewUser(name, email, password, Number(uco), phone_number);
-    //
-    // if (maybeId) {
-    //   return Response.json(maybeId);
-    // }
-    await createNewUser(name, email, password, Number(uco), phone_number);
+    const maybeId = await createNewUser(name, email, password, Number(uco), phone_number);
+
+    if (maybeId) {
+      return Response.json(maybeId);
+    }
   }
   catch (error) {
     console.log(error);
