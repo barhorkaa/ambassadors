10c5b57f2fbed13e72dd2ed83fb24f836e15ea8f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-04 18:32:45 +0100

    fix(migration/init): added unique modifiers to necessary fields

--- database/migrations/20231206-init.ts
+++ database/migrations/20231206-init.ts
@@ -1,40 +1,27 @@
 import { Kysely, sql } from 'kysely'
-import {randomUUID} from "crypto";
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable('ambassador')
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
-    .addColumn('uco', 'integer', (col) => col.notNull())
+    .addColumn('uco', 'integer', (col) => col.notNull().unique())
     .addColumn('name', 'varchar(50)', (col) => col.notNull())
-    .addColumn('email', 'varchar(50)', (col) => col.notNull())
+    .addColumn('email', 'varchar(50)', (col) => col.notNull().unique())
     .addColumn('phone_number', 'varchar(15)', (col) => col.notNull())
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(sql`null`))
     .execute()
 
   await db.schema
     .createTable('eventType')
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
-    .addColumn('name', 'varchar(50)', (col) => col.notNull())
+    .addColumn('name', 'varchar(50)', (col) => col.notNull().unique())
     .addColumn('description', 'varchar(500)', (col) => col.notNull())
     .addColumn('instructions', 'varchar(500)', (col) => col.notNull())
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(sql`null`))
     .execute()
 
   await db.schema
@@ -42,18 +29,10 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('name', 'varchar(69)', (col) => col.notNull())
     .addColumn('date', 'timestamp', (col) => col.notNull())
-    .addColumn('event_type_id', 'char(36)', (col) =>
-      col.references('eventType.id').onDelete('cascade').notNull()
-    )
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
+    .addColumn('event_type_id', 'char(36)', (col) => col.references('eventType.id').onDelete('cascade').notNull())
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(sql`null`))
     .execute()
 
   await db.schema
@@ -63,46 +42,28 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn('number_of_attendees', 'integer', (col) => col.notNull())
     .addColumn('notes', 'varchar(500)')
     .addColumn('ideas', 'varchar(500)')
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(sql`null`))
     .execute()
 
   await db.schema
     .createTable('material')
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
-    .addColumn('name', 'varchar(50)', (col) => col.notNull())
+    .addColumn('name', 'varchar(50)', (col) => col.notNull().unique())
     .addColumn('description', 'varchar(500)')
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(sql`null`))
     .execute()
 
   await db.schema
     .createTable('registerForm')
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('ambassador_id', 'char(36)', (col) => col.references('ambassador.id').notNull())
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(sql`null`))
     .execute()
 
   await db.schema
@@ -110,15 +71,9 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('ambassador_id', 'char(36)', (col) => col.references('ambassador.id').notNull())
     .addColumn('event_id', 'char(36)', (col) => col.references('event.id').notNull())
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(sql`null`))
     .execute()
 
   await db.schema
@@ -127,29 +82,10 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn('material_id', 'char(36)', (col) => col.references('material.id').notNull())
     .addColumn('report_id', 'char(36)', (col) => col.references('report.id').notNull())
     .addColumn('amount', 'integer')
-    .addColumn('created_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('updated_at', 'timestamp', (col) =>
-      col.defaultTo(sql`now()`).notNull()
-    )
-    .addColumn('deleted_at', 'timestamp', (col) =>
-      col.defaultTo(sql`null`)
-    )
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(sql`null`))
     .execute()
-
-
-  // await db.schema
-  //   .createIndex('pet_owner_id_index')
-  //   .on('pet')
-  //   .column('owner_id')
-  //   .execute()
-
-  //   na zrychlenie hladania
-  // b tree dobre na od do
-  // hashovanie na primarnych klucoch -> cez to sa joinuje
-  // pozriet ci ma mysql index na pk
-  // dobre vedie na statnice
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
