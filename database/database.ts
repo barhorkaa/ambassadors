2328c08d012b9c4fd7e33379f2a501405b52bc37
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-04 16:42:26 +0100

    cleanup(database): removed unused code (mySqlDialect, now using PostgresDialect)

--- database/database.ts
+++ database/database.ts
@@ -5,16 +5,6 @@ import * as dotenv from 'dotenv'
 import {DB} from "kysely-codegen";
 dotenv.config({ path: '.env.local' })
 
-// const dialect = new MysqlDialect({
-//   pool: createPool({
-//     host: process.env['MYSQL_HOST'],
-//     port: +process.env['MYSQL_PORT']!,
-//     user: process.env['MYSQL_USER'],
-//     password: process.env['MYSQL_PASSWORD'],
-//     database: process.env['MYSQL_DATABASE'],
-//   })
-// })
-
 const dialect = new PostgresDialect({
   pool: new Pool({
     host: process.env['POSTGRES_HOST'],
