c0cfe4d88c1fc5265828814ca1f79b3480af86d6
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-04 16:23:44 +0100

    feat(auth): removed fetch, calling repository directly

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { z } from 'zod';
 import bcrypt from 'bcryptjs';
 import {User} from "kysely-codegen";
 import {Selectable} from "kysely";
+import {getUserByEmail} from "@/database/repository/user";
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -17,8 +18,9 @@ export const { auth, signIn, signOut } = NextAuth({
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
-          const response = await fetch(`http://localhost:3000/api/user?email=${email}`)
-          const user = await response.json() as (Selectable<User> | undefined);
+          const user = await getUserByEmail(email);
+          // const response = await fetch(`http://localhost:3000/api/user?email=${email}`)
+          // const user = await response.json() as (Selectable<User> | undefined);
           if (!user) return null;
 
           const passwordsMatch = await bcrypt.compare(password, user.password);
