4d36df73dfc8c05c2e72b19f09ebd3c9d3760d67
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-04 16:27:05 +0100

    feat(repository/motivation): changed location of type declaration

--- database/repository/motivation.ts
+++ database/repository/motivation.ts
@@ -1,5 +1,5 @@
-import {MotivationFormData} from "@/app/api/user/motivation/route";
 import {db} from "@/database/database";
+import {MotivationFormData} from "@/app/lib/actions";
 
 export async function createMotivation(data: MotivationFormData) {
   await db.insertInto("motivationForm").values(data).execute()
