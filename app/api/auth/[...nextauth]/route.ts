0533bf93b15582a0f6d2e7eab1bc0e2723323edb
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-04 16:27:25 +0100

    feat(api): removed api directory

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth"
-import {authConfig} from "../../../../auth.config";
-
-// TODO skusit vymatzat a co sa stane
-
-const handler = NextAuth(authConfig);
-export { handler as GET, handler as POST }
