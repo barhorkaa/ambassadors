3caf3b404c687fce8d949fcc663588ebc3d88841
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-04 18:48:42 +0100

    feat(migration/rename): added renaming of all relevant fields and tables

--- database/migrations/20240206-1730-rename.ts
+++ database/migrations/20240206-1730-rename.ts
@@ -5,9 +5,50 @@ export async function up(db: Kysely<any>): Promise<void> {
     .alterTable('ambassador')
     .renameTo('user')
     .execute()
+
+
+  await db.schema
+    .alterTable('eventAmbassador')
+    .renameTo('eventUser')
+    .execute()
+
+  await db.schema
+    .alterTable('eventUser')
+    .renameColumn('ambassador_id', 'user_id')
+    .execute()
+
+  await db.schema
+    .alterTable('registerForm')
+    .renameColumn('ambassador_id', 'user_id')
+    .execute()
+
+  await db.schema
+    .alterTable('report')
+    .renameColumn('ambassador_id', 'user_id')
+    .execute()
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable('report')
+    .renameColumn('user_id', 'ambassador_id')
+    .execute()
+
+  await db.schema
+    .alterTable('registerForm')
+    .renameColumn('user_id', 'ambassador_id')
+    .execute()
+
+  await db.schema
+    .alterTable('eventUser')
+    .renameColumn('user_id', 'ambassador_id')
+    .execute()
+
+  await db.schema
+    .alterTable('eventUser')
+    .renameTo('eventAmbassador')
+    .execute()
+
   await db.schema
     .alterTable('user')
     .renameTo('ambassador')
