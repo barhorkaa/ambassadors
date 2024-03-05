1df4a3c10deee94c132ac735a9125e7a797eeb2a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-05 12:21:06 +0100

    cleanup(auth): removed unused code

--- auth.ts
+++ auth.ts
@@ -1,7 +1,6 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
-import { z } from 'zod';
 import bcrypt from 'bcryptjs';
 import {getUserByEmail} from "@/database/repository/user";
 import {UserLoginData} from "@/app/models/userLoginData";
@@ -16,8 +15,6 @@ export const { auth, signIn, signOut } = NextAuth({
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUserByEmail(email);
-          // const response = await fetch(`http://localhost:3000/api/user?email=${email}`)
-          // const user = await response.json() as (Selectable<User> | undefined);
           if (!user) return null;
 
           const passwordsMatch = await bcrypt.compare(password, user.password);
