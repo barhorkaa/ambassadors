b451a3658870488a51892e8d98731730a0fbac7e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-05 18:10:02 +0100

    feat(ui/faq): created component for displaying FAQs

---
+++ app/ui/main-page/faq.tsx
@@ -0,0 +1,58 @@
+export default function FAQ() {
+  return(
+    <div className="w-full">
+      <div className="collapse collapse-arrow border-2 border-amber-200 rounded-none">
+        <input type="checkbox" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">
+          Co je ambasadorský program?
+        </div>
+        <div className="collapse-content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed sollicitudin hendrerit accumsan. Fusce eu ex odio.
+            Quisque enim erat, dapibus in turpis nec, iaculis consectetur dui.
+            Nunc facilisis mi non ante porttitor, sed eleifend arcu condimentum.
+            Maecenas quis volutpat turpis. Sed blandit libero lacinia ex condimentum,
+            a aliquam velit pulvinar. Sed eros velit, bibendum sit amet vehicula sed,
+            scelerisque vitae dui.</p>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow border-2 border-amber-200 rounded-none">
+        <input type="checkbox" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">
+          Kam můžu jako ambasador jet?
+        </div>
+        <div className="collapse-content">
+          <p>In ut massa ipsum. Nunc ut convallis enim.
+            Pellentesque efficitur nunc mi, euismod blandit turpis aliquam fermentum.
+            Aenean fringilla sapien sapien, et auctor velit consequat eget. Nam porttitor
+            tortor ac purus fringilla ornare. Cras nec congue ligula, in mollis magna. Sed
+            lectus nibh, egestas in auctor quis, fringilla ut ex.</p>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow border-2 border-amber-200 rounded-none">
+        <input type="checkbox" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">
+          A to to jako budu dělat zdarma?
+        </div>
+        <div className="collapse-content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed sollicitudin hendrerit accumsan. Fusce eu ex odio.
+            Quisque enim erat, dapibus in turpis nec, iaculis consectetur dui.
+            Nunc facilisis mi non ante porttitor, sed eleifend arcu condimentum.</p>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow border-2 border-amber-200 rounded-none">
+        <input type="checkbox" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">
+          Koho kotaktovat jestli necomu nechapu?
+        </div>
+        <div className="collapse-content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Sed sollicitudin hendrerit accumsan. Fusce eu ex odio.
+            Quisque enim erat, dapibus in turpis nec, iaculis consectetur dui.
+            Nunc facilisis mi non ante porttitor, sed eleifend arcu condimentum.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
