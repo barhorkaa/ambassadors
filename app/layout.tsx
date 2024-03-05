241f9fbd46a944397acaf2d4e078021b007444ff
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-05 18:12:37 +0100

    feat(layout): added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,11 @@ export default async function RootLayout({
         <div className={"body"}>
           {children}
         </div>
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+          <aside>
+            <p>Copyright © 2024 - All right reserved by OVVSP FI MU</p>
+          </aside>
+        </footer>
       </body>
     </html>
   )
