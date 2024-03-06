dcf7cc7940d23e370b7dbde418bb70fe23f4e7c6
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-06 11:39:30 +0100

    attempt(auth/config): attempt to call server side function

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,23 @@
 import type { NextAuthConfig } from 'next-auth';
+import {getUserApproval} from "@/database/repository/user";
+import {isUserApproved} from "@/app/lib/actions/authentication";
 
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    async authorized({auth, request: {nextUrl}}) {
       const isLoggedIn = !!auth?.user;
+
+      // const user_id = auth?.user?.id;
+      // if (!user_id) {
+      //   return
+      // }
+      // const isApproved = await isUserApproved(user_id);
+      // if (!isApproved) {
+      //   return Response.redirect(new URL(`/register/motivation/${user_id}`))
+      // }
       const isOnDashboard = nextUrl.pathname.startsWith('/events');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
