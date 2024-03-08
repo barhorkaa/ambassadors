c82aba2316ae488a72af6a4b3dc52542cd1d555d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-08 16:26:13 +0100

    feat(auth/config): added console logs

--- auth.config.ts
+++ auth.config.ts
@@ -6,16 +6,27 @@ export const authConfig = {
   },
   callbacks: {
     async jwt({ token, user }) {
+      console.log("JWT token", token)
+      console.log("JWT user", user)
+
       if (user) token.role = user.role;
       return token;
     },
     async session({ session, token }) {
+      console.log("Session token", token)
+      console.log("Session session", token)
+
       if (session?.user) session.user.role = token.role;
       console.log("Session post role assign:", session)
       return session;
     },
     async authorized({auth, request: {nextUrl}}) {
       const isLoggedIn = !!auth?.user;
+      console.log("user in authorized is: ", auth?.user)
+      console.log("user id is:", auth?.user?.id)
+      console.log("user role is: ", auth?.user.role)
+      console.log("user expires is: ", auth)
+
       const isOnDashboard = nextUrl.pathname.startsWith('/events');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
