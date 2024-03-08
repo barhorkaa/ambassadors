f6b8b18315d86c42a3e9424fa9a5dbd4e4a9f75c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-08 17:26:19 +0100

    feat(auth): added check if user has filled out the motivation form

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import Credentials from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
 import {getUserByEmail} from "@/database/repository/user";
 import {UserLoginData} from "@/app/models/userLoginData";
+import {isUserMotivated} from "@/database/repository/motivation";
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -20,8 +21,11 @@ export const { auth, signIn, signOut } = NextAuth({
           const passwordsMatch = await bcrypt.compare(password, user.password);
           console.log("user is:", user)
           console.log("user role is:", user.role)
-          // const auth_user = user as User;
-          if (passwordsMatch) return user;
+
+          if (passwordsMatch) {
+            const hasMotivation = await isUserMotivated(user.id);
+            return {...user, password: null, motivated: hasMotivation};
+          }
         }
 
         console.log('Invalid credentials');
