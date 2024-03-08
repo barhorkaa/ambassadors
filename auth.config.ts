d2d4ffeb07b029d7c11e2a4c1116ae55048804f9
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-08 16:25:57 +0100

    feat(auth/config): added session and jwt callbacks for custom user field role

--- auth.config.ts
+++ auth.config.ts
@@ -5,6 +5,15 @@ export const authConfig = {
     signIn: '/login',
   },
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      console.log("Session post role assign:", session)
+      return session;
+    },
     async authorized({auth, request: {nextUrl}}) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/events');
