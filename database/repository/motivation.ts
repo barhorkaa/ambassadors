a9bab7e6754a123bfea5fa0ada6f6e2bbd963d65
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-08 17:25:16 +0100

    feat(repository/motivation): created function to find out if user has created a motivation form

--- database/repository/motivation.ts
+++ database/repository/motivation.ts
@@ -10,3 +10,12 @@ export async function createMotivation(data: MotivationFormData) {
     console.log(e)
   }
 }
+
+export async function isUserMotivated(user_id: string) {
+  try {
+    const userMotivation = await db.selectFrom("motivationForm").where("user_id", "=", user_id).select("id").executeTakeFirst();
+    return userMotivation != undefined;
+  } catch (e) {
+    console.log(e)
+  }
+}
