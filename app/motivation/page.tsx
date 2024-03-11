0816f365b091fa142354a322ecd851379796668c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-11 16:16:23 +0100

    feat(motivation/page): created new motivation page

---
+++ app/motivation/page.tsx
@@ -0,0 +1,17 @@
+import RegisterMotivationForm from "@/app/ui/register/register-motivation-form";
+import React from "react";
+
+export default function UserMotivation() {
+  return(
+    <div className="hero min-h-max">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left p-4">
+          <h1 className="text-5xl font-bold">Řekněte nám proč jsete se rozhodli propagovat FI</h1>
+          <p className="py-6">Tento dotazník slouží pro nás jako ukazatel, proč se studenti FI rozhodnou jít propagovat svojí fakultu. Zároveň vaše ochota vyplnit něco na víc dokazuje, že to chcete brát vážně :)</p>
+          <p className="py-6">Zároveň je jej potrebné vyplnit, abyste se programu mohli zúčastnit a přihlašovat se na akce.</p>
+        </div>
+        <RegisterMotivationForm />
+      </div>
+    </div>
+  )
+}
