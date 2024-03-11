001014928775668832adaeb6ffd4e25978b2892a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-11 09:58:48 +0100

    feat(denied/page): created page for login denial

---
+++ app/denied/page.tsx
@@ -0,0 +1,7 @@
+export default function Unsuccessful() {
+  return(
+    <div>
+      Your account has not been verified yet, please wait until one of our managers verifies your registration
+    </div>
+  );
+}
