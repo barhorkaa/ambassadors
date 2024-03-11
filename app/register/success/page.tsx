b692a24e7bdd0f54528fcf046ae3ab47c5435352
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-11 16:32:31 +0100

    feat(success/page): added basic layout for success page

--- app/register/success/page.tsx
+++ app/register/success/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Success(){
   return(
-    <div>
-      successfully registered
+    <div className="hero min-h-screen ">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Vaše registrace se zdařila!</h1>
+          <p className="py-6">Nyní se přihlaste a v aplikaci vyplňte motivační dotazník. Poté vaši registraci zchválí někdo z našich manažerů a se budete moct začín plně věnovat svým ambasadorským povinnostem.</p>
+          <Link className="btn bg-amber-300"  href={"/login"}>Prihlásit se</Link>
+        </div>
+      </div>
     </div>
   )
 }
