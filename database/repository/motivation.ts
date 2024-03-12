7beb4e65b9cebf4f9976b66ad856660010496fec
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-12 14:36:24 +0100

    feat(repository/motivation): setting motivated status to true after user creates motivation form

--- database/repository/motivation.ts
+++ database/repository/motivation.ts
@@ -1,11 +1,13 @@
 import {db} from "@/database/database";
 import {MotivationFormData} from "@/app/lib/actions/motivation";
+import {setUserMotivatedStatus} from "@/database/repository/user";
 
 export async function createMotivation(data: MotivationFormData) {
   console.log("Got to repository")
   console.log("Data on repo is: ", data)
   try {
-    await db.insertInto("motivationForm").values(data).execute()
+    await db.insertInto("motivationForm").values(data).execute();
+    await setUserMotivatedStatus(data.user_id);
   } catch (e) {
     console.log(e)
   }
