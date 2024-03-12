0df1bf159114874c26cc8df4712ef3e9acbfecc4
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-12 14:42:59 +0100

    temp-refactor(auth/config): commented out console logs

--- auth.config.ts
+++ auth.config.ts
@@ -6,8 +6,8 @@ export const authConfig = {
   },
   callbacks: {
     async jwt({ token, user }) {
-      console.log("JWT token", token)
-      console.log("JWT user", user)
+      // console.log("JWT token", token)
+      // console.log("JWT user", user)
 
       if (user) {
         token.role = user.role;
@@ -17,8 +17,8 @@ export const authConfig = {
       return token;
     },
     async session({ session, token }) {
-      console.log("Session token", token)
-      console.log("Session session", token)
+      // console.log("Session token", token)
+      // console.log("Session session", token)
 
       if (session?.user) {
         session.user.id = token.sub!;
@@ -26,7 +26,7 @@ export const authConfig = {
         session.user.approved = token.approved;
         session.user.motivated = token.motivated;
       }
-      console.log("Session post role assign:", session)
+      // console.log("Session post role assign:", session)
       return session;
     },
     async authorized({auth, request: {nextUrl}}) {
