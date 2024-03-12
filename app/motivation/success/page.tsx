559f0d634ee997d89bf3982bd9a58f6e6a4d144b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-12 16:41:44 +0100

    feat(motivation/success/page): created a new page for redirect after successful motivation form fill out

---
+++ app/motivation/success/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import {signOut} from "@/auth";
+
+export default function Success(){
+  return(
+    <div className="hero min-h-screen ">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Váš motivační dotazník byl úspešně zaslán!</h1>
+          <p className="py-6">Nyní se odhlaste aby se změny projevili. Po přihlášení uvidíte v aplikaci další kroky.</p>
+          <form action={async () => {
+            'use server';
+            await signOut();
+          }}>
+            <button className="btn bg-amber-300" >Odhlásit se</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
