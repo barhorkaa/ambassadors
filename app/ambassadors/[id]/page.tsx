84179c8cb589277c4300b03ebc3cd6769e5f57ae
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-20 13:01:05 +0100

    feat(ambassadors/page/id): added motivation form display to user detail page

--- app/ambassadors/[id]/page.tsx
+++ app/ambassadors/[id]/page.tsx
@@ -1,23 +1,36 @@
 import UserDetail from "@/app/ui/user/user-detail";
 import {getUserById} from "@/database/repository/user";
 import {UserModel} from "@/app/models/userModel";
+import MotivationDetail from "@/app/ui/motivation/motivation-detail";
+import {getMotivationById} from "@/database/repository/motivation";
+import {MotivationFormData} from "@/app/lib/actions/motivation";
 
 export default async function User({params}: { params: { id: string }}) {
   const user: UserModel | undefined = await getUserById(params.id);
   if (user === undefined) {
     return (
       <div>
-        failed to get user
+        failed to get userto
       </div>
     )
   }
 
+  const userMotivation: MotivationFormData | undefined = await getMotivationById(params.id);
+
   return(
-    <div>
-      <h1 className="font-bold text-xl py-4">
-        Detail uživatele
-      </h1>
-      <UserDetail user={user}/>
+    <div className="flex flex-row justify-center sm:flex-col">
+      <div>
+        <h1 className="font-bold text-xl py-4">
+          Detail uživatele
+        </h1>
+        <UserDetail user={user}/>
+      </div>
+      <div>
+        <h1 className="font-bold text-xl py-4">
+          Motivační formulář
+        </h1>
+        <MotivationDetail motivation={userMotivation}/>
+      </div>
 
     </div>
   )
