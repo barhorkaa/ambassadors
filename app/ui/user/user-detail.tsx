2c912d4e03dc800a097a48bdb63223d7cad5d3ef
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-20 18:27:39 +0100

    feat(ui/userDetail): created bew component to deduplicate code, displaying detail row

--- app/ui/user/user-detail.tsx
+++ app/ui/user/user-detail.tsx
@@ -3,71 +3,35 @@ import {UserModel} from "@/models/userModel";
 export default function UserDetail(params: {user: UserModel}) {
   return(
     <div className="flex flex-col ">
-      <div className="p-2 flex flex-row justify-between bg-amber-100 rounded-md">
-        <h2 className="font-bold font-lg">
-          Jméno
-        </h2>
-        <h3 className="ml-16">
-          {params.user.name}
-        </h3>
-      </div>
-      <div className="p-2 flex flex-row justify-between">
-        <h2 className="font-bold font-lg">
-          E-mail
-        </h2>
-        <h3 className="ml-16">
-          {params.user.email}
-        </h3>
-      </div>
-      <div className="p-2 flex flex-row justify-between bg-amber-100 rounded-md">
-        <h2 className="font-bold font-lg">
-          Tel. číslo
-        </h2>
-        <h3 className="ml-16">
-          {params.user.phone_number}
-        </h3>
-      </div>
-      <div className="p-2 flex flex-row justify-between">
-        <h2 className="font-bold font-lg">
-          UČO
-        </h2>
-        <h3 className="ml-16">
-          {params.user.uco}
-        </h3>
-      </div>
-      <div className="p-2 flex flex-row justify-between bg-amber-100 rounded-md">
-        <h2 className="font-bold font-lg">
-          Role
-        </h2>
-        <h3 className="ml-16">
-          {params.user.role == "ambassador" ? <div>Ambasador</div> : <div>Manažér</div>}
-        </h3>
-      </div>
-      <div className="p-2 flex flex-row justify-between">
-        <h2 className="font-bold font-lg">
-          Potvrzen
-        </h2>
-        <h3 className="ml-16">
-          {params.user.approved ? <div>Ano</div> : <div>Ne</div>}
-        </h3>
-      </div>
-      <div className="p-2 flex flex-row justify-between bg-amber-100 rounded-md">
-        <h2 className="font-bold font-lg">
-          Registrovaný
-        </h2>
-        <h3 className="ml-16">
-          {params.user.created_at.toLocaleString()}
-        </h3>
-      </div>
-      <div className="p-2 flex flex-row justify-between">
-        <h2 className="font-bold font-lg">
-          Aktivní
-        </h2>
-        <h3 className="ml-16">
-          {params.user.deleted_at ? <div>Ne</div> : <div>Ano</div>}
-        </h3>
-      </div>
+      <UserDetailRow label={"Jméno"} value={params.user.name}/>
+      <hr/>
+      <UserDetailRow label={"E-mail"} value={params.user.email}/>
+      <hr/>
+      <UserDetailRow label={"Tel. číslo"} value={params.user.phone_number}/>
+      <hr/>
+      <UserDetailRow label={"UČO"} value={params.user.uco}/>
+      <hr/>
+      <UserDetailRow label={"Role"} value={params.user.role}/>
+      <hr/>
+      <UserDetailRow label={"Potvrzen"} value={params.user.approved ? "Ano" : "Ne"}/>
+      <hr/>
+      <UserDetailRow label={"Registrovaný"} value={params.user.created_at.toLocaleString()}/>
+      <hr/>
+      <UserDetailRow label={"Aktivní"} value={params.user.deleted_at ? "Ne" : "Ano"}/>
+      <hr/>
     </div>
   )
+}
 
+function UserDetailRow(params: {label: string, value: string | number}) {
+  return (
+    <div className="p-2 flex flex-row justify-between">
+      <h2 className="font-extrabold font-lg">
+        {params.label}
+      </h2>
+      <h3 className="ml-16">
+        {params.value}
+      </h3>
+    </div>
+  )
 }
