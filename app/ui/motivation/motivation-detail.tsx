dd7ace06a5ef460f1901b3f7ccbc41611441507c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-20 13:00:22 +0100

    feat(ui/motivationDetail): created component for displaying user motivation form

---
+++ app/ui/motivation/motivation-detail.tsx
@@ -0,0 +1,56 @@
+import {MotivationFormData} from "@/app/lib/actions/motivation";
+
+export default function MotivationDetail(params : {motivation: MotivationFormData | undefined}) {
+  if (params.motivation === undefined) {
+    return (
+      <>
+        the user is not motivated
+      </>
+    )
+  }
+
+  return(
+    <>
+      <div className="p-2 bg-amber-100 rounded-md">
+        <h2 className="font-bold font-lg">
+          Proč jste se rozhodli k nám přidat?
+        </h2>
+        <div>
+          {params.motivation.why}
+        </div>
+      </div>
+      <div className="p-2">
+        <h2 className="font-bold font-lg">
+          Doporučil vám někto přidat se k programu? Pokud ano, kto?
+        </h2>
+        <div>
+          {params.motivation.who}
+        </div>
+      </div>
+      <div className="p-2 bg-amber-100 rounded-md">
+        <h2 className="font-bold font-lg">
+          Co byste chtěli jako ambasador dosáhnout?
+        </h2>
+        <div>
+          {params.motivation.goals}
+        </div>
+      </div>
+      <div className="p-2">
+        <h2 className="font-bold font-lg">
+          Jakých akcí byste se chtěli převážně zúčastňovat?
+        </h2>
+        <div>
+          {params.motivation.preferred_events}
+        </div>
+      </div>
+      <div className="p-2 bg-amber-100 rounded-md">
+        <h2 className="font-bold font-lg">
+          Kolik času budete mít na ambasadorskou činnost?
+        </h2>
+        <div>
+          {params.motivation.time}
+        </div>
+      </div>
+    </>
+  )
+}
