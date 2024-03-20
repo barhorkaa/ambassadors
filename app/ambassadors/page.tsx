d78d45c038479aaf05e7797e45aabd7b9c047d1d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-20 12:59:13 +0100

    fix(ambassadors/page): swapped user cards for user table

--- app/ambassadors/page.tsx
+++ app/ambassadors/page.tsx
@@ -2,6 +2,7 @@ import {getAllAmbassadors, getAllManagers} from "@/database/repository/user";
 import React from "react";
 import {UserModel} from "@/app/models/userModel";
 import UserCard from "@/app/ui/user/user-card";
+import UserTable from "@/app/ui/user/user-table";
 
 export default async function Ambassadors() {
   let allAmbassadors: UserModel[] | undefined = await getAllAmbassadors();
@@ -19,16 +20,18 @@ export default async function Ambassadors() {
       <div className="font-bold text-lg p-2">
         Všichni manažeři
       </div>
-      <div className="flex space-x-4 space-y-4">
-        {allManagers.map((manager) => (<UserCard user={manager}/>) )}
-      </div>
+      <UserTable users={allManagers}/>
+      {/*<div className="flex space-x-4 space-y-4">*/}
+      {/*  {allManagers.map((manager) => (<UserCard user={manager}/>) )}*/}
+      {/*</div>*/}
       <hr/>
       <div className="font-bold text-lg p-2">
         Všichni ambasadoři
       </div>
-      <div className="flex flex-wrap md:justify-between   space-y-4">
-        {allAmbassadors.map((ambassador) => (<UserCard user={ambassador}/>) )}
-      </div>
+      <UserTable users={allAmbassadors}/>
+      {/*<div className="flex flex-wrap md:justify-between   space-y-4">*/}
+      {/*  {allAmbassadors.map((ambassador) => (<UserCard user={ambassador}/>) )}*/}
+      {/*</div>*/}
     </div>
   )
 }
