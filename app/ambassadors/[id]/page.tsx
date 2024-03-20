d434a4ed2cb472608200d4df0d8034753bf6467d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-20 17:24:54 +0100

    feat(ambassador/page/id): added title of the website and approve button

--- app/ambassadors/[id]/page.tsx
+++ app/ambassadors/[id]/page.tsx
@@ -4,6 +4,7 @@ import {UserModel} from "@/app/models/userModel";
 import MotivationDetail from "@/app/ui/motivation/motivation-detail";
 import {getMotivationById} from "@/database/repository/motivation";
 import {MotivationFormData} from "@/app/lib/actions/motivation";
+import ApproveButton from "@/app/ui/button/approve-button";
 
 export default async function User({params}: { params: { id: string }}) {
   const user: UserModel | undefined = await getUserById(params.id);
@@ -18,20 +19,32 @@ export default async function User({params}: { params: { id: string }}) {
   const userMotivation: MotivationFormData | undefined = await getMotivationById(params.id);
 
   return(
-    <div className="flex flex-row justify-center sm:flex-col">
-      <div>
-        <h1 className="font-bold text-xl py-4">
-          Detail uživatele
-        </h1>
-        <UserDetail user={user}/>
+    <div className="flex flex-col">
+      <div className="flex flex-row py-4">
+        <h1 className="font-bold text-3xl">Informace o uživateli: {user.name}</h1>
       </div>
-      <div>
-        <h1 className="font-bold text-xl py-4">
-          Motivační formulář
-        </h1>
-        <MotivationDetail motivation={userMotivation}/>
-      </div>
+      <hr className=""/>
+      <div className="flex flex-row justify-start gap-10 sm:flex-col">
+        <div>
+          <div className="flex flex-row justify-between">
+            <h2 className="font-bold text-xl py-4">
+              Detail uživatele
+            </h2>
+            {
+              !user.approved &&
+              <ApproveButton id={user.id}/>
+            }
+          </div>
 
+          <UserDetail user={user}/>
+        </div>
+        <div>
+          <h2 className="font-bold text-xl py-4">
+            Motivační formulář
+          </h2>
+          <MotivationDetail motivation={userMotivation}/>
+        </div>
+      </div>
     </div>
   )
 }
