fc6b6456523d73dcda407b0cbea8a91cc1c859d7
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-22 14:31:21 +0100

    feat(actions/motivation): added using zod model to validate form data

--- app/lib/actions/motivation.ts
+++ app/lib/actions/motivation.ts
@@ -2,6 +2,8 @@
 
 import {createMotivation} from "@/database/repository/motivation";
 import {redirect} from "next/navigation";
+import {MotivationModel} from "@/models/motivation/motivation-model";
+import {MotivationFormModel} from "@/models/motivation/motivation-form-model";
 
 export type MotivationFormData = {
   why: string | null,
@@ -14,30 +16,69 @@ export type MotivationFormData = {
 
 export async function createMotivationForm(formData: FormData) {
   try {
+    // console.log("form data is", formData)
+    // const bla = {
+    //   why: formData.get("why"),
+    //   who: formData.get("who"),
+    //   goals: formData.get("goals"),
+    //   preferredEvents: formData.get("preferredEvents"),
+    //   time: formData.get("time"),
+    //   ambassadorId: formData.get("user_id"),
+    // }
+    //
+    // console.log("bla user id is: ", bla.ambassadorId)
+
+    // const res = MotivationFormModel.safeParse(bla)
+    // console.log("parsed from bla is: ", res)
+    const re = MotivationFormModel.safeParse(formData)
+
+    console.log("parsing raw form data: ", re)
+    if (re.success) {
+      console.log("res data is: ", re.data)
+      // console.log("type of data is: ", typeof (res.data.why))
+
+      // const data: MotivationFormData = {
+      //   why: bla.why as string,
+      //   who: bla.who as string,
+      //   goals: bla.goals as string,
+      //   preferred_events: bla.preferredEvents as string,
+      //   time: bla.time as string,
+      //   user_id: bla.ambassadorId as string
+      // }
+      await createMotivation({data: re.data});
+    }
     // TODO prevalidovat typy cez zod a az potom posielat
-    const why = formData.get("why");
-    const who = formData.get("who");
-    const goals = formData.get("goals");
-    const preferredEvents = formData.get("preferredEvents");
-    const time = formData.get("time");
-    const ambassadorId = formData.get("id");
 
-    if (!why || !who || !goals || !preferredEvents || !time || !ambassadorId){
-      return "All fields must be filled out"
-    }
 
-    const data: MotivationFormData = {
-      why: why as string,
-      who: who as string,
-      goals: goals as string,
-      preferred_events: preferredEvents as string,
-      time: time as string,
-      user_id: ambassadorId as string
-    }
+    // if (!why || !who || !goals || !preferredEvents || !time || !ambassadorId){
+    //   return "All fields must be filled out"
+    // }
 
-    console.log(data)
+    // const zodData = {
+    //   why,
+    //   who,
+    //   goals,
+    //   preferredEvents,
+    //   time,
+    //   ambassadorId
+    // }
 
-    await createMotivation(data);
+    // const res = MotivationFormModel.safeParse(zodData)
+    // console.log("safe parse from raw data is: ", res)
+    // console.log("\n", res, "\n")
+
+    // const data: MotivationFormData = {
+    //   why: why as string,
+    //   who: who as string,
+    //   goals: goals as string,
+    //   preferred_events: preferredEvents as string,
+    //   time: time as string,
+    //   user_id: ambassadorId as string
+    // }
+
+    // console.log(data)
+    //
+    // await createMotivation(data);
   }
   catch (error) {
     return "Something went wrong"
