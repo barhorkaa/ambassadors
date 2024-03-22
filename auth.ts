f75d95c207c7794f36f31244c72d2c65f07ea8b2
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-22 15:13:41 +0100

    refactor(models/user-login): renamed from camelCase

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
 import {getUserByEmail} from "@/database/repository/user";
-import {UserLoginData} from "@/models/userLoginData";
+import {UserLoginData} from "@/models/user-login-data";
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
