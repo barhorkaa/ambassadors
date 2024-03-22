68c316e94a57ecac9eaf2cdf46743070edc0c30d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-22 14:58:47 +0100

    fix(actions/motivation): removed unused type (replaced by new model)

--- app/lib/actions/motivation.ts
+++ app/lib/actions/motivation.ts
@@ -5,15 +5,6 @@ import {redirect} from "next/navigation";
 import {MotivationModel} from "@/models/motivation/motivation-model";
 import {MotivationFormModel} from "@/models/motivation/motivation-form-model";
 
-export type MotivationFormData = {
-  why: string | null,
-  who: string | null,
-  goals: string | null,
-  preferred_events: string | null,
-  time: string | null,
-  user_id: string
-}
-
 export async function createMotivationForm(formData: FormData) {
   try {
     const parse = MotivationFormModel.safeParse(formData)
