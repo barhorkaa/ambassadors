653a506e30f35cf3b9a7b36a7f517189c6f98968
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-25 16:46:52 +0100

    feat(ui/eventTypeDetail): created component for displaying event type details

---
+++ app/ui/event/event-type-detail.tsx
@@ -0,0 +1,32 @@
+import {EventTypeDetailModel} from "@/models/event-type/event-type-detail-model";
+import {DetailRow} from "@/app/ui/user/user-detail";
+import {auth} from "@/auth";
+
+export default async function EventTypeDetail({eventType} : {eventType: EventTypeDetailModel | undefined}) {
+  const session = await auth();
+
+  if (eventType === undefined) {
+    return (
+      <div>
+        Unable to load eventType details
+      </div>
+    )
+  }
+
+  return(
+    <div className="flex flex-col w-1/3">
+      <DetailRow label={"Typ akce"} value={eventType.name}/>
+      <DetailRow label={"Popis"} value={eventType.description}/>
+      <DetailRow label={"Instrukce"} value={eventType.instructions}/>
+      {session?.user.role == "manager" &&
+        (
+          <div>
+            <DetailRow label={"Vytvořeno"} value={eventType.created_at.toLocaleString()}/>
+            <DetailRow label={"Upraveno"} value={eventType.updated_at.toLocaleString()}/>
+            <DetailRow label={"Zmazáno"} value={eventType.deleted_at == null ? "Ne" : eventType.deleted_at.toLocaleString()}/>
+          </div>
+        )
+      }
+    </div>
+  )
+}
