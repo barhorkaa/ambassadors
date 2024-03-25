64f47f6b646e0d1ba7f33987d56ef13172a86d66
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-25 16:45:29 +0100

    feat(models/eventDetail): created model for event detail

---
+++ models/event/event-detail-model.ts
@@ -0,0 +1,15 @@
+import {z} from "zod";
+import {eventModel} from "@/models/event/event-model";
+
+export const eventDetailModel = z.object({
+  id: z.string(),
+  name: z.string(),
+  date: z.date().nullable(),
+  event_type_id: z.string(),
+  approved: z.boolean(),
+  created_at: z.date(),
+  updated_at: z.date(),
+  deleted_at: z.date().nullable(),
+})
+
+export type EventDetailModel = z.infer<typeof eventDetailModel>
