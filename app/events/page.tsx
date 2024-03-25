db812d0858cf01862cfec5a074cd5464d372d9f4
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-25 16:47:54 +0100

    feat(events/page): created basic page for displaying all events

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,5 +1,32 @@
-export default function Events(){
+import {getAllEvents} from "@/database/repository/events";
+import {EventModel} from "@/models/event/event-model";
+import EventTable from "@/app/ui/event/event-table";
+
+export default async function Events(){
+  let allEvents: EventModel[] | undefined = await getAllEvents();
+  console.log("all events are: ", allEvents)
+  if (allEvents === undefined) {
+    allEvents = []
+  }
+
   return(
-    <div>welcome</div>
+    <>
+      <div className="flex flex-row justify-between p-4">
+        <h1 className="text-2xl">
+          Akce
+        </h1>
+        <button className="btn">
+          Přidat akci
+        </button>
+      </div>
+      {/*<div>*/}
+      {/*  {allEvents.map((event) =>*/}
+      {/*    (<EventCard event={event}/>)*/}
+      {/*  )}*/}
+      {/*</div>*/}
+      <div className="py-2">
+        <EventTable events={allEvents}/>
+      </div>
+    </>
   );
 }
