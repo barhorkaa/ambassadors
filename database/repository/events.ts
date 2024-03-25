f55ad5f47bc64ea3a70234446fef108c5dbe898f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-25 16:42:57 +0100

    feat(repository/events): created function to get all events

---
+++ database/repository/events.ts
@@ -0,0 +1,12 @@
+import {db} from "@/database/database";
+
+export async function getAllEvents() {
+  try {
+    return await db.selectFrom("event")
+      .fullJoin("eventType", "eventType.id", "event_type_id")
+      .select(["event.name as name", "eventType.name as event_type_name", "event_type_id", "date", "event.id as id"])
+      .execute();
+  } catch (e) {
+    console.log(e)
+  }
+}
