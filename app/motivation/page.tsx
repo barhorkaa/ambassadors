83b6b6778f0e4b13a4538c657f07d5585d27a075
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-27 15:26:41 +0100

    fix(motivation/page): changed header class

--- app/motivation/page.tsx
+++ app/motivation/page.tsx
@@ -6,7 +6,7 @@ export default function UserMotivation() {
     <div className="hero min-h-max">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left p-4">
-          <h1 className="text-5xl font-bold">Řekněte nám proč jsete se rozhodli propagovat FI</h1>
+          <h1 className="hero-title">Řekněte nám proč jsete se rozhodli propagovat FI</h1>
           <p className="py-6">Tento dotazník slouží pro nás jako ukazatel, proč se studenti FI rozhodnou jít propagovat svojí fakultu. Zároveň vaše ochota vyplnit něco na víc dokazuje, že to chcete brát vážně :)</p>
           <p className="py-6">Zároveň je jej potrebné vyplnit, abyste se programu mohli zúčastnit a přihlašovat se na akce.</p>
         </div>
