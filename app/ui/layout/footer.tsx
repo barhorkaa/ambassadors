0ba2fa2148acd9fe5f5bdb661eaacd999ca5374b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-27 14:56:35 +0100

    feat(ui/footer): created separate component for footer

---
+++ app/ui/layout/footer.tsx
@@ -0,0 +1,14 @@
+import {auth} from "@/auth";
+
+export async function Footer() {
+  const session = await auth();
+
+  return(
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+        <p>Copyright © 2024 - All right reserved by OVVSP FI MU</p>
+      </aside>
+    </footer>
+  )
+}
