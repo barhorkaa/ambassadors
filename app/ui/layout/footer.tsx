4d890983f9381db88b7cfb9307d768a65fd756b5
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-27 14:58:08 +0100

    fix(ui/footer): changed footer color

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -4,7 +4,7 @@ export async function Footer() {
   const session = await auth();
 
   return(
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-fi-100 text-base-content">
       <aside>
         <pre>{JSON.stringify(session, null, 2)}</pre>
         <p>Copyright © 2024 - All right reserved by OVVSP FI MU</p>
