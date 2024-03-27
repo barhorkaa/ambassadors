72f047d437131367947cf236ba827f754c869dad
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-27 14:30:30 +0100

    fix(login/page): changed styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,8 +6,8 @@ export default function Login() {
     <div className="hero min-h-full">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left p-4">
-          <h1 className="text-5xl font-bold">Přihlašte se a naplánujte svou akci!</h1>
-          <p className="py-6">Po přihlášení budete míť přístup ke všem akcím a možnost se na přihlásit. Pojďte s námi představit Fakultu informatiky Masarykovy Univerzity světu!</p>
+          <h1 className="hero-title">Přihlašte se a naplánujte svou akci!</h1>
+          <p className="py-6 sm:py-2">Po přihlášení budete míť přístup ke všem akcím a možnost se na přihlásit. Pojďte s námi představit Fakultu informatiky Masarykovy Univerzity světu!</p>
         </div>
         <LoginForm/>
       </div>
