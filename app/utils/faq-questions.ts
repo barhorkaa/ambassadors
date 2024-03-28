2c57730f03db5186a613dc530735cb8571fd2c22
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-28 16:33:49 +0100

    feat(utils/faq): added faqs list

--- app/utils/faq-questions.ts
+++ app/utils/faq-questions.ts
@@ -2,3 +2,46 @@ export type FaqQuestion = {
   question: string,
   answer: string
 }
+
+export const faqQuestions: FaqQuestion[] = [
+  {
+    question: "Co je ambasadorský program?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+      "Sed sollicitudin hendrerit accumsan. Fusce eu ex odio.\n" +
+      "Quisque enim erat, dapibus in turpis nec, iaculis consectetur dui.\n" +
+      "Nunc facilisis mi non ante porttitor, sed eleifend arcu condimentum.\n" +
+      "Maecenas quis volutpat turpis. Sed blandit libero lacinia ex condimentum,\n" +
+      "a aliquam velit pulvinar. Sed eros velit, bibendum sit amet vehicula sed,\n" +
+      "scelerisque vitae dui."
+  },
+  {
+    question: "Kam můžu jako ambasador jet?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+      "Sed sollicitudin hendrerit accumsan. Fusce eu ex odio.\n" +
+      "Quisque enim erat, dapibus in turpis nec, iaculis consectetur dui.\n" +
+      "Nunc facilisis mi non ante porttitor, sed eleifend arcu condimentum.\n" +
+      "Maecenas quis volutpat turpis. Sed blandit libero lacinia ex condimentum,\n" +
+      "a aliquam velit pulvinar. Sed eros velit, bibendum sit amet vehicula sed,\n" +
+      "scelerisque vitae dui."
+  },
+  {
+    question: "A to to jako budu dělat zdarma?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+      "Sed sollicitudin hendrerit accumsan. Fusce eu ex odio.\n" +
+      "Quisque enim erat, dapibus in turpis nec, iaculis consectetur dui.\n" +
+      "Nunc facilisis mi non ante porttitor, sed eleifend arcu condimentum.\n" +
+      "Maecenas quis volutpat turpis. Sed blandit libero lacinia ex condimentum,\n" +
+      "a aliquam velit pulvinar. Sed eros velit, bibendum sit amet vehicula sed,\n" +
+      "scelerisque vitae dui."
+  },
+  {
+    question: "Koho kotaktovat jestli necomu nechapu?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" +
+      "Sed sollicitudin hendrerit accumsan. Fusce eu ex odio.\n" +
+      "Quisque enim erat, dapibus in turpis nec, iaculis consectetur dui.\n" +
+      "Nunc facilisis mi non ante porttitor, sed eleifend arcu condimentum.\n" +
+      "Maecenas quis volutpat turpis. Sed blandit libero lacinia ex condimentum,\n" +
+      "a aliquam velit pulvinar. Sed eros velit, bibendum sit amet vehicula sed,\n" +
+      "scelerisque vitae dui."
+  }
+]
