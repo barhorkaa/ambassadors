eb4927d8b9971f73039b6c4cdb58a7be94ecef86
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-28 15:20:51 +0100

    fix(register/success): restructured button

--- app/register/success/page.tsx
+++ app/register/success/page.tsx
@@ -7,7 +7,9 @@ export default function Success(){
         <div className="max-w-md">
           <h1 className="hero-title">Vaše registrace se zdařila!</h1>
           <p className="py-6">Nyní se přihlaste a v aplikaci vyplňte motivační dotazník. Poté vaši registraci zchválí někdo z našich manažerů a se budete moct začín plně věnovat svým ambasadorským povinnostem.</p>
-          <Link className="btn bg-amber-300"  href={"/login"}>Prihlásit se</Link>
+          <Link href={"/login"}>
+            <button>Přihlásit se</button>
+          </Link>
         </div>
       </div>
     </div>
