926a8cf29261568ae8c88ec63f3eb26b52bf4ae1
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-28 16:19:41 +0100

    feat(ui/navBar): replaced old sidebar with the new one

--- app/ui/layout/nav-bar-user.tsx
+++ app/ui/layout/nav-bar-user.tsx
@@ -1,32 +1,13 @@
 import Link from "next/link";
 import {signOut} from "@/auth";
+import SideBar from "@/app/ui/layout/side-bar";
 
 export default function NavBarUser() {
   return(
     <div className="navbar bg-fi-300">
       <div className="navbar-start">
         <div className="dropdown max-h-fit min-h-fit">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-          </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><Link href={""}>Můj účet</Link></li>
-            <hr/>
-            <li><Link href={""}>Moje akce</Link></li>
-            <li><Link href={"/events"}>Všechny akce</Link></li>
-            <li><Link href={""}>Historie akcií</Link></li>
-            <hr/>
-            <li><Link href={"/"}>O programu</Link></li>
-            <li><Link href={""}>Kontakty</Link></li>
-            <li><Link href={""}>Pomocník</Link></li>
-            <hr/>
-            <form action={async () => {
-              'use server';
-              await signOut();
-            }}>
-              <button>Odhlásit se</button>
-            </form>
-          </ul>
+          <SideBar/>
         </div>
       </div>
       <div className="navbar-center">
