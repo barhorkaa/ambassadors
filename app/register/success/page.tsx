ebef461f3cae19296783a237dba8b57a0b834a0c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-28 15:20:14 +0100

    fix(register/success): replaced with new class hero-title

--- app/register/success/page.tsx
+++ app/register/success/page.tsx
@@ -5,7 +5,7 @@ export default function Success(){
     <div className="hero">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Vaše registrace se zdařila!</h1>
+          <h1 className="hero-title">Vaše registrace se zdařila!</h1>
           <p className="py-6">Nyní se přihlaste a v aplikaci vyplňte motivační dotazník. Poté vaši registraci zchválí někdo z našich manažerů a se budete moct začín plně věnovat svým ambasadorským povinnostem.</p>
           <Link className="btn bg-amber-300"  href={"/login"}>Prihlásit se</Link>
         </div>
