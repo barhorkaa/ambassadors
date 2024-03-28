0b630861450ceef703a454886a7b52d58db51e57
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-03-28 15:19:16 +0100

    fix(motivation/success): replaced with new class hero-title

--- app/motivation/success/page.tsx
+++ app/motivation/success/page.tsx
@@ -6,7 +6,7 @@ export default function Success(){
     <div className="hero">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Váš motivační dotazník byl úspešně zaslán!</h1>
+          <h1 className="hero-title">Váš motivační dotazník byl úspešně zaslán!</h1>
           <p className="py-6">Nyní se odhlaste aby se změny projevili. Po přihlášení uvidíte v aplikaci další kroky.</p>
           <form action={async () => {
             'use server';
