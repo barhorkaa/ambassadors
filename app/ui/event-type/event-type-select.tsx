7f081c8ff3d3c788cec28f275d56d400b70dd776
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-04 17:41:01 +0200

    feat(ui/eventTypeSelect): created client component for selection of event type

---
+++ app/ui/event-type/event-type-select.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React, {useState} from "react";
+import {EventTypeBasicModel} from "@/models/event-type/event-type-basic";
+
+export default function EventTypeSelect({eventTypes}: {eventTypes: EventTypeBasicModel[]}) {
+  const [eventType, setEventType] = useState('');
+
+  const handleEventTypeChange = (event: any) => {
+    setEventType(event.target.value);
+    console.log("\n\n\n\n", event.target.value)
+  };
+
+  return(
+    <div className="form-control">
+      <label className="label" htmlFor="event_type">
+        <span className="label-text">Typ akce</span>
+      </label>
+      <select className="select select-bordered w-full max-w-xs" id='eventType' name="event_type_id" value={eventType} onChange={handleEventTypeChange}>
+        <option selected>Vyberte typ akce</option>
+        {eventTypes.map((event) =>
+          <option key={event.id} value={event.id}>{event.name}</option>
+        )}
+      </select>
+    </div>
+  )
+}
