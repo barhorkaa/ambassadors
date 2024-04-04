8c753295d25b99622e8a45e375ed2870182732a9
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-04 17:35:35 +0200

    feat(models/eventTypeBasic): created model for basic eventType display in select

---
+++ models/event-type/event-type-basic.ts
@@ -0,0 +1,8 @@
+import {z} from "zod";
+
+export const eventTypeBasicModel = z.object({
+  id: z.string(),
+  name: z.string(),
+})
+
+export type EventTypeBasicModel = z.infer<typeof eventTypeBasicModel>
