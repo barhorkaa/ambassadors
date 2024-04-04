b8cb43003ca41f387c95a8d015c144c535b6700f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-04 17:44:40 +0200

    feat(events/page/new): created basic page layout for creating a new event

---
+++ app/events/new/page.tsx
@@ -0,0 +1,16 @@
+import EventForm from "@/app/ui/event/event-form";
+import React from "react";
+
+export default function NewEvent() {
+  return(
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left p-4">
+          <h1>Podejte návrh na novou akci</h1>
+          <p className="py-6">Tento návrh pak zkontroluje jeden z našich manažerů a pokud bude všetko v pořádku, budete se moci na nšj přihlásit v rubrice "Akce"</p>
+        </div>
+        <EventForm/>
+      </div>
+    </div>
+  )
+}
