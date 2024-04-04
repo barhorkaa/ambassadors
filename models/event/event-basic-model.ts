a38339c9473e128015c9e0bd9f78a8d307e95638
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-04 18:03:34 +0200

    feat(models/eventBasic): added field for approved with default value

--- models/event/event-basic-model.ts
+++ models/event/event-basic-model.ts
@@ -4,6 +4,7 @@ export const eventBasicModel = z.object({
   name: z.string(),
   date: z.string().pipe( z.coerce.date() ).nullable(),
   event_type_id: z.string(),
+  approved: z.string().pipe(z.coerce.boolean()).default("")
 })
 
 export type EventBasicModel = z.infer<typeof eventBasicModel>
