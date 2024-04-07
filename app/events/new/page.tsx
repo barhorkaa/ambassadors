300f09718b5c4cee2fad513d5c3c78f36b6e371d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-07 14:20:02 +0200

    fix(events/page/new): replaced quotation marks

--- app/events/new/page.tsx
+++ app/events/new/page.tsx
@@ -7,7 +7,7 @@ export default function NewEvent() {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left p-4">
           <h1>Podejte návrh na novou akci</h1>
-          <p className="py-6">Tento návrh pak zkontroluje jeden z našich manažerů a pokud bude všetko v pořádku, budete se moci na nšj přihlásit v rubrice "Akce"</p>
+          <p className="py-6">Tento návrh pak zkontroluje jeden z našich manažerů a pokud bude všetko v pořádku, budete se moci na nšj přihlásit v rubrice &quot;Akce&quot;</p>
         </div>
         <EventForm/>
       </div>
