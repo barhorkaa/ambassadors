de5c0ee27803d088d9449ef7d621565bab189c0a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-08 15:09:02 +0200

    feat(ui/editEventModal): created modal for editing event

---
+++ app/ui/modals/edit-event-modal.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import EventForm from "@/app/ui/event/event-form";
+import {EventTypeBasicModel} from "@/models/event-type/event-type-basic";
+import {EventDetailModel} from "@/models/event/event-detail-model";
+
+export default function EditEventModal(data: {eventTypes: EventTypeBasicModel[], event: EventDetailModel}) {
+  return(
+    <>
+      <button className="btn" onClick={() => {
+        if (document) {
+          (document.getElementById('my_modal_2') as HTMLFormElement).showModal();
+        }
+      }}>open modal</button>
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <h3>Upravit akci</h3>
+          <EventForm event={data.event} eventTypes={data.eventTypes}/>
+        </div>
+      </dialog>
+    </>
+  )
+}
