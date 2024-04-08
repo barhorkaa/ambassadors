7f85c2f73eddcbd88036ad7dc49e8b281d72b4ab
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-08 15:01:36 +0200

    refactor(events/page/new): extracted card and moved function

--- app/events/new/page.tsx
+++ app/events/new/page.tsx
@@ -1,7 +1,14 @@
 import EventForm from "@/app/ui/event/event-form";
 import React from "react";
+import {EventTypeBasicModel} from "@/models/event-type/event-type-basic";
+import {getAllEventTypesBasics} from "@/database/repository/event-type";
 
-export default function NewEvent() {
+export default async function NewEvent() {
+  let eventTypes: EventTypeBasicModel[] | undefined = await getAllEventTypesBasics();
+  if (eventTypes === undefined) {
+    eventTypes = [];
+  }
+
   return(
     <div className="hero">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,7 +16,9 @@ export default function NewEvent() {
           <h1>Podejte návrh na novou akci</h1>
           <p className="py-6">Tento návrh pak zkontroluje jeden z našich manažerů a pokud bude všetko v pořádku, budete se moci na nšj přihlásit v rubrice &quot;Akce&quot;</p>
         </div>
-        <EventForm/>
+        <div className="card">
+          <EventForm event={null} eventTypes={eventTypes}/>
+        </div>
       </div>
     </div>
   )
