4269c7f47ff606a60b560b60d629925d33213a41
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-08 14:52:50 +0200

    feat(ui/eventForm): conditionally updating or creating

--- app/ui/event/event-form.tsx
+++ app/ui/event/event-form.tsx
@@ -9,19 +9,25 @@ import {EventDetailModel} from "@/models/event/event-detail-model";
 export default async function EventForm(data: {eventTypes: EventTypeBasicModel[], event: EventDetailModel | null}) {
 
   return(
-    <form action={createNewEvent} className="card-body">
+    <form action={data.event === null ? createNewEvent : updateEventWithId} className="card-body">
       <div className="form-control">
         <label className="label"
                htmlFor="name">
           <span className="label-text">Název akce</span>
         </label>
-        <input id="name" type="text" name="name" placeholder="Gymnázium Třídy Kapitána Jaroše" className="input input-bordered" required />
+        <input id="name" value={data.event?.name} type="text" name="name" placeholder="Gymnázium Třídy Kapitána Jaroše" className="input input-bordered" required />
       </div>
       <div className="form-control">
         <label className="label" htmlFor="who">
           <span className="label-text">Datum akce (může zůstat nevyplněno)</span>
         </label>
-        <input id="date" type="date" name="date" placeholder="" className="input input-bordered" />
+        <input id="date"  type="date" name="date" placeholder="" className="input input-bordered" />
+      </div>
+      <div className="form-control">
+        <label className="label" htmlFor="who">
+          <span className="label-text">Datum akce (může zůstat nevyplněno)</span>
+        </label>
+        <input id="id" value={data.event?.id} type="hidden" name="id" className="input input-bordered" />
       </div>
       <EventTypeSelect eventTypes={data.eventTypes}/>
       <SubmitButton/>
