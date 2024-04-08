3e91edbfdbdc1ebb893a0eeeccf01af0d8442d2c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-08 15:11:21 +0200

    fix(models/eventBasic): added id to model

--- models/event/event-basic-model.ts
+++ models/event/event-basic-model.ts
@@ -1,6 +1,7 @@
 import {z} from "zod";
 
 export const eventBasicModel = z.object({
+  id: z.string(),
   name: z.string(),
   date: z.string().pipe( z.coerce.date() ).nullable(),
   event_type_id: z.string(),
