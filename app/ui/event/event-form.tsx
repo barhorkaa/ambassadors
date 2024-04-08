a538819fc413c64690564d438d6229e34ac0e3ce
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-08 14:52:12 +0200

    feat(ui/eventForm): removed card element to have plain form

--- app/ui/event/event-form.tsx
+++ app/ui/event/event-form.tsx
@@ -2,31 +2,29 @@ import React from "react";
 import SubmitButton from "@/app/ui/button/submit-button";
 import EventTypeSelect from "@/app/ui/event-type/event-type-select";
 import {EventTypeBasicModel} from "@/models/event-type/event-type-basic";
-import {getAllEventTypesBasics} from "@/database/repository/event-type";
-import {createNewEvent} from "@/app/lib/actions/event";
+import {createNewEvent, updateEventWithId} from "@/app/lib/actions/event";
+import {EventModel} from "@/models/event/event-model";
+import {EventDetailModel} from "@/models/event/event-detail-model";
 
 export default async function EventForm(data: {eventTypes: EventTypeBasicModel[], event: EventDetailModel | null}) {
 
   return(
-    <div className="card">
-      <form action={createNewEvent} className="card-body">
-        <div className="form-control">
-          <label className="label"
-                 htmlFor="name">
-            <span className="label-text">Název akce</span>
-          </label>
-          <input id="name" type="text" name="name" placeholder="Gymnázium Třídy Kapitána Jaroše" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label" htmlFor="who">
-            <span className="label-text">Datum akce (může zůstat nevyplněno)</span>
-          </label>
-          <input id="date" type="date" name="date" placeholder="" className="input input-bordered" />
-        </div>
-        <EventTypeSelect eventTypes={eventTypes}/>
-        <SubmitButton/>
-      </form>
-    </div>
+    <form action={createNewEvent} className="card-body">
+      <div className="form-control">
+        <label className="label"
+               htmlFor="name">
+          <span className="label-text">Název akce</span>
+        </label>
+        <input id="name" type="text" name="name" placeholder="Gymnázium Třídy Kapitána Jaroše" className="input input-bordered" required />
+      </div>
+      <div className="form-control">
+        <label className="label" htmlFor="who">
+          <span className="label-text">Datum akce (může zůstat nevyplněno)</span>
+        </label>
+        <input id="date" type="date" name="date" placeholder="" className="input input-bordered" />
+      </div>
       <EventTypeSelect eventTypes={data.eventTypes}/>
+      <SubmitButton/>
+    </form>
   )
 }
