3c5d47898b68c59efae0d96078e73257c9ccdd81
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-08 17:25:57 +0200

    feat(ui/navBarUser): added displaying of user's name to navBar

--- app/ui/layout/nav-bar-user.tsx
+++ app/ui/layout/nav-bar-user.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import SideBar from "@/app/ui/layout/side-bar";
+import {auth} from "@/auth";
 
-export default function NavBarUser() {
+export default async function NavBarUser() {
+  const session = await auth();
+
   return(
     <div className="navbar bg-fi-300">
       <div className="navbar-start">
@@ -13,9 +16,10 @@ export default function NavBarUser() {
         <Link className="btn btn-ghost text-xl" href={"/"}>AmbassadorsFIMU</Link>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-        </button>
+        {/*<button className="btn btn-ghost btn-circle">*/}
+        {/*  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>*/}
+        {/*</button>*/}
+        <Link className="flex flex-row gap-4" href={"/me"}>Vítej: <h3>{session?.user.name}</h3></Link>
       </div>
     </div>
   );
