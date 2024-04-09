c1d3d912dafd46a975581b090faf57a39d1a76f1
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-09 14:56:19 +0200

    feat(models/eventUser): created basic model for user event

---
+++ models/event-user/event-user-basic-model.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const eventUserBasicModel = z.object({
+  id: z.string().nullable(),
+  user_name: z.string().nullable(),
+  user_id: z.string().nullable(),
+  event_id: z.string().nullable(),
+  event_name: z.string().nullable(),
+})
+
+export type EventUserBasicModel = z.infer<typeof eventUserBasicModel>
