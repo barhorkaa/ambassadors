e792617ad08086b7a1aa7dbfb2c330cb82b56695
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-09 16:53:04 +0200

    refactor(ui/eventUserTable): moved table to new directory

--- app/events/signups/page.tsx
+++ app/events/signups/page.tsx
@@ -1,4 +1,4 @@
-import EventUserTable from '@/app/ui/event/event-user-table';
+import EventUserTable from '@/app/ui/event-user/event-user-table';
 import { getAllUnapprovedSignUps } from '@/database/repository/event-user';
 import { EventUserBasicModel } from '@/models/event-user/event-user-basic-model';
 
