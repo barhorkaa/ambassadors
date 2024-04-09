68de21e6f66a945e4870204e550d987023da9649
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-09 16:20:30 +0200

    fix(ui/approveButton): changed props to exclude id from function

--- app/ui/button/approve-button.tsx
+++ app/ui/button/approve-button.tsx
@@ -5,7 +5,7 @@ import {approveUserById} from "@/app/lib/actions/users";
 import {CheckIcon} from "@heroicons/react/24/outline";
 
 
-export default function ApproveButton(props : {fun: any}) {
+export default function ApproveButton(props : {fun: any, id: string}) {
   const [show, setShow] = useState(true);
 
   function changeState() {
@@ -16,7 +16,7 @@ export default function ApproveButton(props : {fun: any}) {
     <>
       {show &&
         <button onClick={async () => {
-          await props.fun;
+          await props.fun(props.id);
           changeState()
         }}>
           {/*<CheckIcon className="h-5 w-5"/>*/}
