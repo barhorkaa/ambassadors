cd1df7e27bf15ec4cee0240aad8a9eadbfc06bcf
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-10 21:09:33 +0200

    feat(ui/eventDetail): changed event detail displaying

--- app/ui/event/event-detail.tsx
+++ app/ui/event/event-detail.tsx
@@ -1,21 +1,41 @@
-import { DetailRow } from '@/app/ui/user/user-detail';
 import { auth } from '@/auth';
 import { EventDetailModel } from '@/models/event/event-detail-model';
+import { CalendarDaysIcon, CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/outline';
 
 export default async function EventDetail({ event }: { event: EventDetailModel }) {
   const session = await auth();
   return (
-    <div className="flex flex-col w-1/3">
-      <DetailRow label={'Název akce'} value={event.name} />
-      <DetailRow label={'Datum'} value={event.date === null ? 'Nezadáno' : event.date.toLocaleString()} />
-      <DetailRow label={'Potvrzeno'} value={event.approved ? 'Ano' : 'Ne'} />
-      {session?.user.role == 'manager' && (
-        <div>
-          <DetailRow label={'Vytvořeno'} value={event.created_at.toLocaleString()} />
-          <DetailRow label={'Upraveno'} value={event.updated_at.toLocaleString()} />
-          <DetailRow label={'Zmazáno'} value={event.deleted_at == null ? 'Ne' : event.deleted_at.toLocaleString()} />
+    <div className="w-full">
+      <h2 className="text-5xl">{event.name}</h2>
+      <hr className="divider w-full" />
+      <div className="flex flex-row gap-10 sm:flex-col sm:gap-4">
+        <div className="flex flex-row gap-2">
+          <CalendarDaysIcon className="h-7" />
+          <p className="text-lg">{event.date?.toLocaleDateString()}</p>
         </div>
-      )}
+        <div className="flex flex-row gap-2">
+          {event.approved ? (
+            <div className="flex flex-row gap-2">
+              <CheckCircleIcon className="h-7" />
+              <p className="text-lg">Potvrzeno</p>
+            </div>
+          ) : (
+            <div className="flex flex-row gap-2">
+              <XCircleIcon className="h-7" />
+              <p className="text-lg">Nepotvrzeno</p>
+            </div>
+          )}
+        </div>
+        {session?.user.role === 'manager' && (
+          <div className="flex flex-row gap-10 sm:flex-col sm:gap-4">
+            <p className="text-lg">Vytvořeno: {event.created_at.toLocaleDateString()}</p>
+            <p className="text-lg">Upraveno: {event.updated_at.toLocaleDateString()}</p>
+            <p className="text-lg">
+              Smazáno: {event.deleted_at === null ? 'Ne' : event.deleted_at.toLocaleDateString()}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
