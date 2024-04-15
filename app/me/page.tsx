d174d712ab0237ccd768647a2b5bdce4064b6525
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-15 17:03:15 +0200

    feat(me/page): created page to display user details

---
+++ app/me/page.tsx
@@ -0,0 +1,29 @@
+import MotivationDetail from '@/app/ui/motivation/motivation-detail';
+import UserDetail from '@/app/ui/user/user-detail';
+import { auth } from '@/auth';
+import { getUserMotivation } from '@/database/repository/motivation';
+import { getUserById } from '@/database/repository/user';
+import { MotivationModel } from '@/models/motivation/motivation-model';
+import { UserModel } from '@/models/userModel';
+
+export default async function Me() {
+  const session = await auth();
+
+  const user: UserModel | undefined = await getUserById(session?.user.id!);
+  if (user === undefined) {
+    return <div>no details</div>;
+  }
+
+  const motivation: MotivationModel | undefined = await getUserMotivation(session?.user.id!);
+
+  return (
+    <div className="page">
+      <h1>Moje informace</h1>
+      <hr className="divider w-full" />
+      <div className="flex flex-row gap-20 sm:flex-col sm:gap-2">
+        <UserDetail user={user} />
+        <MotivationDetail motivation={motivation} />
+      </div>
+    </div>
+  );
+}
