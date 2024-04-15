35b31f15511480da3ddcbf5ecf05e8d82059ee3a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-15 18:07:25 +0200

    feat(actions/users): added action for editing user info

--- app/lib/actions/users.ts
+++ app/lib/actions/users.ts
@@ -1,6 +1,7 @@
 'use server';
 
-import { approveUser } from '@/database/repository/user';
+import { approveUser, editUser } from '@/database/repository/user';
+import { userEditSelfModel } from '@/models/user/user-edit-self-model';
 
 export async function approveUserWithId(formData: FormData) {
   try {
@@ -16,6 +17,27 @@ export async function approveUserWithId(formData: FormData) {
 export async function approveUserById(id: string) {
   try {
     await approveUser(id);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function editUserAction(formData: FormData) {
+  try {
+    console.log('got to edit user action');
+    console.log('form data is: ', formData);
+    const user = {
+      id: formData.get('id'),
+      name: formData.get('name'),
+      phone_number: formData.get('phoneNumber'),
+    };
+
+    const parse = userEditSelfModel.safeParse(user);
+    console.log('parse is: ', parse);
+
+    if (parse.success) {
+      await editUser(parse.data);
+    }
   } catch (e) {
     console.log(e);
   }
