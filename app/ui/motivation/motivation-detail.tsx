198f7baea76ac0f6b3366c4b024028956d91a7fe
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-16 17:06:48 +0200

    fix(ui/motivationDetail): changed data displaying

--- app/ui/motivation/motivation-detail.tsx
+++ app/ui/motivation/motivation-detail.tsx
@@ -1,3 +1,4 @@
+import DetailRow from '@/app/ui/utils/detail-row';
 import { MotivationModel } from '@/models/motivation/motivation-model';
 
 export default function MotivationDetail(params: { motivation: MotivationModel | undefined }) {
@@ -7,25 +8,23 @@ export default function MotivationDetail(params: { motivation: MotivationModel |
 
   return (
     <div className="flex flex-col">
-      <div className="p-2 bg-amber-100 rounded-md">
-        <h3 className="">Proč jste se rozhodli k nám přidat?</h3>
-        <div>{params.motivation.why}</div>
+      <div className="pb-4">
+        <DetailRow label={'Proč jste se rozhodli k nám přidat?'} value={params.motivation.why} />
       </div>
-      <div className="p-2">
-        <h3 className="">Doporučil vám někto přidat se k programu? Pokud ano, kto?</h3>
-        <div>{params.motivation.who}</div>
+      <div className="pb-4">
+        <DetailRow label={'Doporučil vám někto přidat se k programu? Pokud ano, kto?'} value={params.motivation.who} />
       </div>
-      <div className="p-2 bg-amber-100 rounded-md">
-        <h3 className="">Co byste chtěli jako ambasador dosáhnout?</h3>
-        <div>{params.motivation.goals}</div>
+      <div className="pb-4">
+        <DetailRow label={'Co byste chtěli jako ambasador dosáhnout?'} value={params.motivation.goals} />
       </div>
-      <div className="p-2">
-        <h3 className="">Jakých akcí byste se chtěli převážně zúčastňovat?</h3>
-        <div>{params.motivation.preferred_events}</div>
+      <div className="pb-4">
+        <DetailRow
+          label={'Jakých akcí byste se chtěli převážně zúčastňovat?'}
+          value={params.motivation.preferred_events}
+        />
       </div>
-      <div className="p-2 bg-amber-100 rounded-md">
-        <h3 className="">Kolik času budete mít na ambasadorskou činnost?</h3>
-        <div>{params.motivation.time}</div>
+      <div className="pb-4">
+        <DetailRow label={'Kolik času budete mít na ambasadorskou činnost?'} value={params.motivation.time} />
       </div>
     </div>
   );
