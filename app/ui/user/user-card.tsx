6a58840d6360c61661761dd44ae03f92eacef493
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-18 12:03:33 +0200

    fix(ui/userCard): changed user card layout

--- app/ui/user/user-card.tsx
+++ app/ui/user/user-card.tsx
@@ -1,19 +1,11 @@
-import { approveUserWithId } from '@/app/lib/actions/users';
 import { UserModel } from '@/models/userModel';
 import Link from 'next/link';
 
 export default async function UserCard(props: { user: UserModel }) {
   console.log('user in card is', props.user);
   return (
-    <Link
-      href={`ambassadors/${props.user.id}`}
-      // href={{
-      // pathname: `/ambassadors/[id]`,
-      // query: { id: props.user.id },
-      // }}
-    >
-      {/*<button onClick={redirect(`ambassadors/${props.user.id}`)}>*/}
-      <div className="card md:w-72 sm:w-full bg-base-200 shadow-xl">
+    <div className="card shadow-lg w-5/6">
+      <Link href={`ambassadors/${props.user.id}`}>
         <div className="card-body">
           <h2 className="card-title">{props.user.name}</h2>
           <div className="flex flex-row justify-between">
@@ -21,21 +13,9 @@ export default async function UserCard(props: { user: UserModel }) {
               <p>E-mail: {props.user.email}</p>
               <p>Tel. číslo: {props.user.phone_number}</p>
             </div>
-            {!props.user.approved && (
-              <div className="card-actions justify-end">
-                <form action={approveUserWithId}>
-                  <input name="id" type="hidden" value={props.user.id} />
-                  {/*<ApproveButton/>*/}
-                  {/*<button type="submit" className="btn btn-circle btn-outline">*/}
-                  {/*    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4.5 12.75 6 6 9-13.5" /></svg>*/}
-                  {/*</button>*/}
-                </form>
-              </div>
-            )}
           </div>
         </div>
-      </div>
-      {/*</button>*/}
-    </Link>
+      </Link>
+    </div>
   );
 }
