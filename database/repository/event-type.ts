655f3ee4c444346d44c42ed0c992a2e2d91c96dc
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-20 18:50:58 +0200

    feat(repository/eventType): added functions to create and edit event types

--- database/repository/event-type.ts
+++ database/repository/event-type.ts
@@ -1,4 +1,5 @@
 import { db } from '@/database/database';
+import { EventTypeCreateModel } from '@/models/event-type/event-type-create-model';
 
 export async function getEventTypeById(id: string) {
   try {
@@ -11,6 +12,39 @@ export async function getEventTypeById(id: string) {
 export async function getAllEventTypesBasics() {
   try {
     return await db.selectFrom('eventType').select(['name', 'id']).execute();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function getAllEventTypes() {
+  try {
+    return await db.selectFrom('eventType').selectAll().execute();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function createEventType(eventType: EventTypeCreateModel) {
+  try {
+    await db.insertInto('eventType').values(eventType).execute();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function editEventType(eventType: EventTypeCreateModel) {
+  try {
+    await db
+      .updateTable('eventType')
+      .where('id', '=', eventType.id)
+      .set({
+        name: eventType.name,
+        description: eventType.description,
+        instructions: eventType.instructions,
+        updated_at: new Date(),
+      })
+      .execute();
   } catch (e) {
     console.log(e);
   }
