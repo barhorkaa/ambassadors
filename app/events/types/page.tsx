91f758b05001747c8b738030dba5f3fd20add02d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-20 19:20:41 +0200

    feat(events/page/types): created a page for displaying event types

---
+++ app/events/types/page.tsx
@@ -0,0 +1,47 @@
+import CreateEventTypeModal from '@/app/ui/modals/create-event-type-modal';
+import EditEventTypeModal from '@/app/ui/modals/edit-event-type-modal';
+import DetailRow from '@/app/ui/utils/detail-row';
+import { auth } from '@/auth';
+import { getAllEventTypes } from '@/database/repository/event-type';
+import { EventTypeDetailModel } from '@/models/event-type/event-type-detail-model';
+
+export default async function EventsTypes() {
+  const allEventTypes: EventTypeDetailModel[] | undefined = await getAllEventTypes();
+  if (allEventTypes === undefined) {
+    return <div>unable to get events</div>;
+  }
+
+  const session = await auth();
+
+  return (
+    <div className="page">
+      <div className="flex flex-row justify-between">
+        <h1>Typy akcii</h1>
+        {session?.user.role === 'manager' && <CreateEventTypeModal />}
+      </div>
+
+      <hr className="divider w-full" />
+      {allEventTypes.map((eventType) => (
+        <Detail key={eventType.id} eventType={eventType} />
+      ))}
+    </div>
+  );
+}
+
+async function Detail(params: { eventType: EventTypeDetailModel }) {
+  const session = await auth();
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between">
+        <h2>{params.eventType.name}</h2>
+        {session?.user.role === 'manager' && <EditEventTypeModal eventType={params.eventType} />}
+      </div>
+      <div>
+        <DetailRow label={'Popis'} value={params.eventType.description} />
+        <DetailRow label={'Instrukce'} value={params.eventType.instructions} />
+      </div>
+      <hr className="divider" />
+    </div>
+  );
+}
