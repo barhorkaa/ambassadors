4e2a2d043c15fa940c4a5507fdee444edda5b283
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-20 09:30:32 +0200

    fix(me/page): changed to use an existing component

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,35 +1,12 @@
-import EditUserModal from '@/app/ui/modals/edit-user-modal';
-import MotivationDetail from '@/app/ui/motivation/motivation-detail';
-import UserDetail from '@/app/ui/user/user-detail';
+import User from '@/app/ambassadors/[id]/page';
 import { auth } from '@/auth';
-import { getUserMotivation } from '@/database/repository/motivation';
-import { getUserById } from '@/database/repository/user';
-import { MotivationModel } from '@/models/motivation/motivation-model';
-import { UserModel } from '@/models/userModel';
 
 export default async function Me() {
   const session = await auth();
 
-  const user: UserModel | undefined = await getUserById(session?.user.id!);
-  if (user === undefined) {
-    return <div>no details</div>;
-  }
-
-  const motivation: MotivationModel | undefined = await getUserMotivation(session?.user.id!);
-
   return (
     <div className="page">
-      <div className="flex flex-row justify-between">
-        <h1>Moje informace</h1>
-        <EditUserModal user={user} />
-      </div>
-
-      <hr className="divider w-full" />
-      <div className="flex flex-col md:flex-row gap-2 md:gap-20">
-        <UserDetail user={user} />
-        <hr className="divider md:hidden" />
-        <MotivationDetail motivation={motivation} />
-      </div>
+      <User params={{ id: session?.user.id! }} />
     </div>
   );
 }
