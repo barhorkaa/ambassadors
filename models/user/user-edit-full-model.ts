b167b77cc0543649248c3d32da2fc802733c9dfe
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-20 09:28:14 +0200

    feat(models/editUserFull): created a model for full user edit

---
+++ models/user/user-edit-full-model.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const userEditFullModel = z.object({
+  id: z.string(),
+  name: z.string(),
+  phone_number: z.string(),
+  uco: z.string().pipe(z.coerce.number()),
+  email: z.string(),
+});
+
+export type UserEditFullModel = z.infer<typeof userEditFullModel>;
