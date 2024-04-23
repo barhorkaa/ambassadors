2e03f605bf779d31bc2c0e10815cde7e7bb33553
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-23 15:08:00 +0200

    fix(ui/eventUserDetail): added keys to iterators

--- app/ui/event-user/event-user-detail.tsx
+++ app/ui/event-user/event-user-detail.tsx
@@ -20,20 +20,20 @@ export async function EventUserDetail({ event_id }: { event_id: string }) {
         <h2>
           Na akci {signedUpForEvent.length > 1 ? 'jsou' : 'je'} přihlášen{signedUpForEvent.length > 1 ? 'i' : ''}
         </h2>
-        {signedUpForEvent.map((user_name) => (
-          <div className="flex flex-row gap-4 pl-16 py-2">
+        {signedUpForEvent.map((user, iterator) => (
+          <div key={iterator} className="flex flex-row gap-4 pl-16 py-2">
             <UserIcon className="h-6" />
-            <p className="text-lg">{user_name.user_name}</p>
+            <p className="text-lg">{user.user_name}</p>
           </div>
         ))}
       </div>
       <div>
         <h2>
           Náhradní{signedUpForEvent.length > 1 ? 'ci' : 'k'}
-          {substitutesForEvent.map((user_name) => (
-            <div className="flex flex-row gap-4 pl-16 py-2">
+          {substitutesForEvent.map((user, iterator) => (
+            <div key={iterator} className="flex flex-row gap-4 pl-16 py-2">
               <UserIcon className="h-6" />
-              <p className="text-lg">{user_name.user_name}</p>
+              <p className="text-lg">{user.user_name}</p>
             </div>
           ))}
         </h2>
