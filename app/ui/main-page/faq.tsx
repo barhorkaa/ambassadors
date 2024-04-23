aa453e83c6e4fa2b23ea0ddf2f3bbf5cb19ad31b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-23 16:45:01 +0200

    fix(ui/faq): fixed styling

--- app/ui/main-page/faq.tsx
+++ app/ui/main-page/faq.tsx
@@ -2,7 +2,7 @@ import { FaqQuestion, faqQuestions } from '@/app/utils/faq-questions';
 
 export default function FAQ() {
   return (
-    <div className="w-1/2 sm:w-full">
+    <div className="w-full md:w-1/2">
       {faqQuestions.map((question, index) => (
         <FAQDrawer key={index} faq={question} />
       ))}
