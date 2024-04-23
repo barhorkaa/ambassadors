a653fd422b68390617faccbfd5b1bd343515b93b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-23 15:52:40 +0200

    feat(events/page): added displaying of unapproved events

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,6 +1,6 @@
 import EventCard from '@/app/ui/event/event-card';
 import EventTable from '@/app/ui/event/event-table';
-import { getAllEvents } from '@/database/repository/events';
+import { getAllEvents, getAllUnapprovedEvents } from '@/database/repository/events';
 import { EventModel } from '@/models/event/event-model';
 import Link from 'next/link';
 
@@ -11,6 +11,12 @@ export default async function Events() {
     allEvents = [];
   }
 
+  let allUnapprovedEvents: EventModel[] | undefined = await getAllUnapprovedEvents();
+  console.log('all Unapproved Events are: ', allUnapprovedEvents);
+  if (allUnapprovedEvents === undefined) {
+    allUnapprovedEvents = [];
+  }
+
   return (
     <div className="page">
       <div className="flex flex-row justify-between p-4">
@@ -19,6 +25,18 @@ export default async function Events() {
           <Link href={'/events/new'}>Přidat akci</Link>
         </button>
       </div>
+      <hr className="divider w-full" />
+      <h2>Nepotvrzené akce</h2>
+      <div className="flex md:hidden flex-wrap justify-center gap-4 ">
+        {allUnapprovedEvents.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </div>
+      <div className="py-2 hidden md:block">
+        <EventTable events={allUnapprovedEvents} />
+      </div>
+      <hr className="divider" />
+      <h2>Všechny akce</h2>
       <div className="flex md:hidden flex-wrap justify-center gap-4 ">
         {allEvents.map((event) => (
           <EventCard key={event.id} event={event} />
