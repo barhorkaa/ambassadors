8909b3e882eab3dfe094de16c0388e996c92ef95
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-23 15:03:12 +0200

    feat(ui/eventUserDetail): added displaying of substitutes in event user detail

--- app/ui/event-user/event-user-detail.tsx
+++ app/ui/event-user/event-user-detail.tsx
@@ -1,4 +1,4 @@
-import { getSignUpsForEvent } from '@/database/repository/event-user';
+import { getSignUpsForEvent, getSubstitutesForEvent } from '@/database/repository/event-user';
 import { UserIcon } from '@heroicons/react/24/outline';
 
 export async function EventUserDetail({ event_id }: { event_id: string }) {
@@ -8,17 +8,36 @@ export async function EventUserDetail({ event_id }: { event_id: string }) {
     return <div>Na akci není přhlášen nikto</div>;
   }
 
+  let substitutesForEvent = await getSubstitutesForEvent(event_id);
+  console.log('these users are substitutes for this event', substitutesForEvent);
+  if (substitutesForEvent === undefined) {
+    return <div>Na akci není přhlášen žádný náhradník</div>;
+  }
+
   return (
-    <div>
-      <h2>
-        Na akci {signedUpForEvent.length > 1 ? 'jsou' : 'je'} přihlášen{signedUpForEvent.length > 1 ? 'i' : ''}
-      </h2>
-      {signedUpForEvent.map((user_name) => (
-        <div className="flex flex-row gap-4 pl-16 py-2">
-          <UserIcon className="h-6" />
-          <p className="text-lg">{user_name.user_name}</p>
-        </div>
-      ))}
+    <div className="flex flex-col md:flex-row md:gap-16">
+      <div>
+        <h2>
+          Na akci {signedUpForEvent.length > 1 ? 'jsou' : 'je'} přihlášen{signedUpForEvent.length > 1 ? 'i' : ''}
+        </h2>
+        {signedUpForEvent.map((user_name) => (
+          <div className="flex flex-row gap-4 pl-16 py-2">
+            <UserIcon className="h-6" />
+            <p className="text-lg">{user_name.user_name}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2>
+          Náhradní{signedUpForEvent.length > 1 ? 'ci' : 'k'}
+          {substitutesForEvent.map((user_name) => (
+            <div className="flex flex-row gap-4 pl-16 py-2">
+              <UserIcon className="h-6" />
+              <p className="text-lg">{user_name.user_name}</p>
+            </div>
+          ))}
+        </h2>
+      </div>
     </div>
   );
 }
