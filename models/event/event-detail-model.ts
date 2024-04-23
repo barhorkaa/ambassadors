b92418c9d890129638c4cbd09321cb92ac11f6fc
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-23 15:37:34 +0200

    fix(models/eventDetail): added limit to model

--- models/event/event-detail-model.ts
+++ models/event/event-detail-model.ts
@@ -6,6 +6,7 @@ export const eventDetailModel = z.object({
   date: z.date().nullable(),
   event_type_id: z.string(),
   approved: z.boolean(),
+  limit: z.string(),
   created_at: z.date(),
   updated_at: z.date(),
   deleted_at: z.date().nullable(),
