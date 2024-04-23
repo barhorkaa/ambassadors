014dfbddab2b9d589fca8160391c265d6ce35632
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-23 12:44:39 +0200

    feat(actions/eventUser): added an action for creating signups with substitute logic

--- app/lib/actions/event-user.ts
+++ app/lib/actions/event-user.ts
@@ -13,6 +13,26 @@ export async function approveSignUpAction(id: string) {
 export async function createSignUpAction(event_id: string, user_id: string) {
   try {
     await createSignUp(event_id, user_id);
+    const event = await getEventById(event_id);
+    if (event === undefined) {
+      return 'Invalid event';
+    }
+    const eventLimit = Number(event.limit!);
+
+    const signedForEvent = await getSignUpsForEvent(event_id);
+    if (signedForEvent === undefined) {
+      return 'something went wrong';
+    }
+
+    if (eventLimit < signedForEvent.length) {
+      await createSignUp(event_id, user_id, false);
+    } else {
+      await createSignUp(event_id, user_id, true);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
   } catch (e) {
     console.log(e);
   }
