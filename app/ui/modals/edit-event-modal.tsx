fd844fb8be1e26b9a670528e86d6cc85c070e39b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-24 18:28:52 +0200

    fix(ui/editEventModal): removed unnecessary class

--- app/ui/modals/edit-event-modal.tsx
+++ app/ui/modals/edit-event-modal.tsx
@@ -8,7 +8,6 @@ export default function EditEventModal(data: { eventTypes: EventTypeBasicModel[]
   return (
     <>
       <button
-        className="btn"
         onClick={() => {
           if (document) {
             (document.getElementById('my_modal_2') as HTMLFormElement).showModal();
