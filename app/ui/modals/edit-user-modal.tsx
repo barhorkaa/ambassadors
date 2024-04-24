9832755d084ff24232e07f581ae1fe9c878918e4
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-24 18:29:17 +0200

    fix(ui/editUserModal): removed unnecessary class

--- app/ui/modals/edit-user-modal.tsx
+++ app/ui/modals/edit-user-modal.tsx
@@ -7,7 +7,6 @@ export default function EditUserModal(data: { user: UserModel }) {
   return (
     <>
       <button
-        className="btn"
         onClick={() => {
           if (document) {
             (document.getElementById('edit_user_modal') as HTMLFormElement).showModal();
@@ -19,7 +18,6 @@ export default function EditUserModal(data: { user: UserModel }) {
       <dialog id="edit_user_modal" className="modal">
         <div className="modal-box">
           <h3>Upravit informace</h3>
-          {/*<EventForm event={data.event} eventTypes={data.eventTypes} />*/}
           <UserEditForm user={data.user} />
         </div>
       </dialog>
