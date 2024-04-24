ba0e9aaa63bc5153be26c52de438dbcdc363534c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-24 18:02:53 +0200

    fix(ui/submitButton): added title to button to make it more custom

--- app/ui/button/submit-button.tsx
+++ app/ui/button/submit-button.tsx
@@ -2,12 +2,13 @@
 
 import { useFormStatus } from 'react-dom';
 
-export default function SubmitButton() {
+export default function SubmitButton(props: { title: string }) {
   const { pending } = useFormStatus();
 
   return (
     <button className="mt-4 w-full bg-amber-200 p-2" aria-disabled={pending}>
       Odeslat
+      {props.title}
     </button>
   );
 }
