34021c5674b580db7337234728f20c962edbd921
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-25 16:45:04 +0200

    feat(ui/createEventTypeModal): added closing options for modal

--- app/ui/modals/create-event-type-modal.tsx
+++ app/ui/modals/create-event-type-modal.tsx
@@ -17,10 +17,15 @@ export default function CreateEventTypeModal() {
       </button>
       <dialog id="create_event_type_modal" className="modal">
         <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-base-100">✕</button>
+          </form>
           <h3>Nový typ akce</h3>
-          {/*<EventForm event={data.event} eventTypes={data.eventTypes} />*/}
           <EventTypeForm eventType={null} />
         </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
       </dialog>
     </>
   );
