00d9318529127380539b6a23fb1b3e6f5dcc2006
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-25 16:44:03 +0200

    feat(ui/editUserFullModal): added closing options for modal

--- app/ui/modals/edit-user-full-modal.tsx
+++ app/ui/modals/edit-user-full-modal.tsx
@@ -20,9 +20,15 @@ export default function EditUserFullModal(data: { user: UserModel }) {
       </button>
       <dialog id="edit_user_modal_manager" className="modal">
         <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-base-100">✕</button>
+          </form>
           <h3>Upravit informace</h3>
           <UserEditFullForm user={data.user} />
         </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
       </dialog>
     </>
   );
