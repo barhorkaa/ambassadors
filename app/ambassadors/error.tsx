d1efcf9dbb463745b9fb81bb2f51a66813c5e1e1
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-30 14:49:59 +0200

    feat(ambassadors/error): created error

---
+++ app/ambassadors/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  return (
+    <div className="page flex flex-col gap-6 items-center">
+      <h1>Nepodařilo se získat uživatele</h1>
+      <button className="btn" onClick={() => reset()}>
+        Zkusit znovu
+      </button>
+    </div>
+  );
+}
