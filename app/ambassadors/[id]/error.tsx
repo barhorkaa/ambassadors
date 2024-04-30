1ca92fc583de3033398782d1e633a831302872ae
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-30 14:49:30 +0200

    fix(ambassadors/error/id): changed error title

--- app/ambassadors/[id]/error.tsx
+++ app/ambassadors/[id]/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   return (
     <div className="page flex flex-col gap-6 items-center">
-      <h1>Něco se pokazilo!</h1>
+      <h1>Nepodařilo se získat informace o uživateli</h1>
       <button className="btn" onClick={() => reset()}>
         Zkusit znovu
       </button>
