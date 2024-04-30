2b92a980f094d1c3efdb7dff2b28bcda250329a8
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-30 14:25:43 +0200

    feat(ambassadors/page/id): added grouping to buttons

--- app/ambassadors/[id]/page.tsx
+++ app/ambassadors/[id]/page.tsx
@@ -20,14 +20,12 @@ export default async function User({ params }: { params: { id: string } }) {
   return (
     <div className="page flex flex-col">
       <div className="flex flex-row justify-between">
-        <h1>Informace o uživateli: {user.name}</h1>
-        {!user.approved && <ApproveButton fun={approveUserById} id={user.id} />}
-        {session?.user.id === params.id && session.user.role !== 'manager' && <EditUserModal user={user} />}
-        {session?.user.role === 'manager' && <EditUserFullModal user={user} />}
         <h1>Informace o uživateli: {user!.name}</h1>
+        <div className="flex flex-row gap-2">
           {!user!.approved && <ApproveButton fun={approveUserById} id={user!.id} />}
           {session?.user.id === params.id && session.user.role !== 'manager' && <EditUserModal user={user!} />}
           {session?.user.role === 'manager' && <EditUserFullModal user={user!} />}
+        </div>
       </div>
       <hr className="w-full" />
       <div className="flex flex-col md:flex-row justify-start gap-20 py-4 ">
