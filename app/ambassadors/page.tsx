9359e9d5de415ac4ee60bd045c1308d51655d8d7
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-30 15:29:21 +0200

    feat(ambassadors/page): replaced repeated code with new component

--- app/ambassadors/page.tsx
+++ app/ambassadors/page.tsx
@@ -12,35 +12,9 @@ export default async function Ambassadors() {
     <>
       <h1>Uživatelé</h1>
       <hr className="w-full" />
-      <h2>Nepotvrzení uživatelé</h2>
-      <div className="hidden md:block">
-        <UserTable users={unapprovedAmbassadors} />
-      </div>
-      <div className="flex md:hidden flex-wrap justify-center gap-4">
-        {unapprovedAmbassadors.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
-      </div>
-      <hr />
-      <h2>Manažeři</h2>
-      <div className="hidden md:block">
-        <UserTable users={allManagers} />
-      </div>
-      <div className="flex md:hidden flex-wrap justify-center gap-4">
-        {allManagers.map((manager) => (
-          <UserCard key={manager.id} user={manager} />
-        ))}
-      </div>
-      <hr />
-      <h2>Ambasadoři</h2>
-      <div className="hidden md:block">
-        <UserTable users={allAmbassadors} />
-      </div>
-      <div className="flex md:hidden flex-wrap justify-center gap-4">
-        {allAmbassadors.map((ambassador) => (
-          <UserCard key={ambassador.id} user={ambassador} />
-        ))}
-      </div>
+      <UserList title={'Nepotvrzení uživatelé'} list={unapprovedAmbassadors} />
+      <UserList title={'Manažeři'} list={allManagers} />
+      <UserList title={'Ambasadoři'} list={allAmbassadors} />
     </>
   );
 }
