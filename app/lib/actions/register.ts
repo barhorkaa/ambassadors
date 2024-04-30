d8e3dd734ca6b4dc19a4d1de55a638bf9cb27db8
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-04-30 15:46:35 +0200

    fix(actions/register): changed parse logic

--- app/lib/actions/register.ts
+++ app/lib/actions/register.ts
@@ -16,15 +16,11 @@ export async function createUserAction(prevState: string | undefined, formData: 
       phone_number: formData.get('phoneNumber'),
     };
 
-    const parse = registrationModel.safeParse(newUserForm);
-    console.log('parse is: ', parse);
+    const newUser = registrationModel.parse(newUserForm);
+    newUser.password = await bcrypt.hash(newUser.password, 10); // TODO change salt to random
 
-    if (parse.success) {
-      let newUser: RegistrationModel = parse.data;
-      newUser.password = await bcrypt.hash(newUser.password, 10); // TODO change salt to random
-      console.log('new user post hash is: ', newUser);
-      result = await createUser(newUser);
-    }
+    console.log('new user post hash is: ', newUser);
+    await createUser(newUser);
   } catch (error) {
     console.log(error);
     return 'Something went wrong, error';
