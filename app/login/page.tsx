bf0570f238316070a7eea21660476f3844c2e34d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-01 17:15:39 +0200

    fix(login/layout): removed page styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/app/ui/auth/login-form';
 
 export default function Login() {
   return (
-    <div className="page hero">
+    <div className="hero">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left p-4">
           <h1 className="hero-title">Přihlašte se a naplánujte svou akci!</h1>
