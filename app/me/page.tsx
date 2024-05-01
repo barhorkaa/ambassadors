93be9e8d9b057593dac97f3cd1214eb088af66f8
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-01 16:51:09 +0200

    fix(me/page): removed page styling

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -4,9 +4,5 @@ import { auth } from '@/auth';
 export default async function Me() {
   const session = await auth();
 
-  return (
-    <div className="page">
-      <User params={{ id: session?.user.id! }} />
-    </div>
-  );
+  return <User params={{ id: session?.user.id! }} />;
 }
