38de5170e94ad976635aa4f4dff94e9504ded6c1
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-01 16:22:26 +0200

    fix(actions/motivation): removed return based redirect

--- app/lib/actions/motivation.ts
+++ app/lib/actions/motivation.ts
@@ -5,15 +5,11 @@ import { MotivationFormModel } from '@/models/motivation/motivation-form-model';
 import { redirect } from 'next/navigation';
 
 export async function createMotivationAction(formData: FormData) {
-  let result: boolean | undefined = false;
   try {
     const parsedData = MotivationFormModel.parse(formData);
-
-    result = await createMotivation(parsedData);
+    await createMotivation(parsedData);
   } catch (error) {
     return 'Something went wrong';
   }
-  if (result) {
-    redirect('/motivation/success');
-  }
+  redirect('/motivation/success');
 }
