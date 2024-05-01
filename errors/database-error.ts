5240abe07c998b2bc995476f2c6e22faaf61b9e9
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-01 11:50:27 +0200

    fix(error/databaseError): added delete to error names

--- errors/database-error.ts
+++ errors/database-error.ts
@@ -1,5 +1,5 @@
 import { ErrorBase } from '@/errors/base-error';
 
-type ErrorName = 'DATABASE_GET_ERROR' | 'DATABASE_CREATE_ERROR' | 'DATABASE_UPDATE_ERROR';
+type ErrorName = 'DATABASE_GET_ERROR' | 'DATABASE_CREATE_ERROR' | 'DATABASE_UPDATE_ERROR' | 'DATABASE_DELETE_ERROR';
 
 export class DatabaseError extends ErrorBase<ErrorName> {}
