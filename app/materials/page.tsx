9dd74411f4a521004c025efb4a9cff894a787918
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-02 17:54:20 +0200

    feat(materials/page): created basic page for material

---
+++ app/materials/page.tsx
@@ -0,0 +1,21 @@
+import CreateMaterialModal from '@/app/ui/modals/create-material-modal';
+import { auth } from '@/auth';
+import { getAllMaterials } from '@/database/repository/material';
+
+export default async function Material() {
+  const session = await auth();
+  const isManager = session?.user.role === 'manager';
+
+  const allMaterials = await getAllMaterials();
+
+  return (
+    <>
+      <div className="flex flex-col md:flex-row md:justify-between">
+        <h1>Materiály</h1>
+        {isManager && <CreateMaterialModal />}
+      </div>
+
+      <hr className="w-full" />
+    </>
+  );
+}
