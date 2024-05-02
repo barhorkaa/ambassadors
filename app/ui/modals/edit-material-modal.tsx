c6ab6fb8b4a12b5466cbda33650bccfd0511b1e3
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-02 18:31:20 +0200

    feat(modals/editMaterial): created modal for editing

---
+++ app/ui/modals/edit-material-modal.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { MaterialForm } from '@/app/ui/material/material-form';
+import { MaterialFullModel } from '@/models/material/material-full-model';
+import { PencilIcon } from '@heroicons/react/24/outline';
+
+export default function EditMaterialModal(data: { material: MaterialFullModel }) {
+  return (
+    <>
+      <button
+        className="btn"
+        onClick={() => {
+          if (document) {
+            (document.getElementById(data.material.id) as HTMLFormElement).showModal();
+          }
+        }}
+      >
+        <PencilIcon className="h-5" />
+        <p className="hidden md:block">Upravit</p>
+      </button>
+      <dialog id={data.material.id} className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-base-100">✕</button>
+          </form>
+          <h3>Upravit informace</h3>
+          <MaterialForm material={data.material} />
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
+  );
+}
