72584be051c53b7139f4d6fd69481530413f6529
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-02 18:32:02 +0200

    feat(actions/material): added action for editing

--- app/lib/actions/material.ts
+++ app/lib/actions/material.ts
@@ -1,7 +1,8 @@
 'use server';
 
-import { createMaterial } from '@/database/repository/material';
+import { createMaterial, editMaterial } from '@/database/repository/material';
 import { materialCreateModel } from '@/models/material/material-create-model';
+import { materialEditModel } from '@/models/material/material-edit-model';
 
 export async function createMaterialAction(formData: FormData) {
   try {
@@ -12,6 +13,22 @@ export async function createMaterialAction(formData: FormData) {
 
     const parsedData = materialCreateModel.parse(materialForm);
     await createMaterial(parsedData);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+export async function editMaterialAction(formData: FormData) {
+  try {
+    const materialForm = {
+      id: formData.get('id'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+    };
+
+    const parsedData = materialEditModel.parse(materialForm);
+    await editMaterial(parsedData);
   } catch (e) {
     console.error(e);
     throw e;
