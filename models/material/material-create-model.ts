decfef47cdcaa89b569653bc51c85ee5edbb49e6
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-02 18:26:27 +0200

    fix(models/materialCreate): made crate dependent on edit

--- models/material/material-create-model.ts
+++ models/material/material-create-model.ts
@@ -1,11 +1,8 @@
-import { materialFullModel } from '@/models/material/material-full-model';
+import { materialEditModel } from '@/models/material/material-edit-model';
 import { z } from 'zod';
 
-export const materialCreateModel = materialFullModel.omit({
+export const materialCreateModel = materialEditModel.omit({
   id: true,
-  created_at: true,
-  updated_at: true,
-  deleted_at: true,
 });
 
 export type MaterialCreateModel = z.infer<typeof materialCreateModel>;
