732b83d9416ea083a0c83ebed62cc3b8bb56a874
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-02 15:30:56 +0200

    fix(ui/footer): removed session info

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -1,12 +1,7 @@
-import { auth } from '@/auth';
-
 export async function Footer() {
-  const session = await auth();
-
   return (
     <footer className="footer footer-center p-4 bg-fi-100 text-base-content">
       <aside>
-        <pre>{JSON.stringify(session, null, 2)}</pre>
         <p>Copyright © 2024 - All right reserved by OVVSP FI MU</p>
       </aside>
     </footer>
