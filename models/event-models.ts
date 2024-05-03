a0a7da551ba2bd71e8c0178c99293012e2953bee
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-03 17:34:56 +0200

    fix(models/eventBasic): moved to event-models

--- models/event-models.ts
+++ models/event-models.ts
@@ -1,3 +1,16 @@
+import { z } from 'zod';
+
+export const eventBasicModel = z.object({
+  id: z.string().optional(),
+  name: z.string(),
+  date: z.string().pipe(z.coerce.date()).nullable(),
+  limit: z.string().pipe(z.coerce.number()),
+  event_type_id: z.string(),
+  approved: z.string().pipe(z.coerce.boolean()).default(''),
+});
+
+export type EventBasicModel = z.infer<typeof eventBasicModel>;
+
 export type EventModel = {
   id: string;
   name: string;
