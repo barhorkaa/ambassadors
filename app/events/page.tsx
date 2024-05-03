32660c7e49b84e2271b18db88bef84e963994401
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-03 17:02:44 +0200

    fix(events/page): changed to parameterized

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,13 +1,12 @@
 import { EventList } from '@/app/ui/utils/event-list';
-import { getAllEvents, getAllUnapprovedEvents } from '@/database/repository/events';
-import { EventModel } from '@/models/event/event-model';
+import { getAllEvents } from '@/database/repository/events';
 import { EventModel } from '@/models/event-models';
 import { PlusIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 export default async function Events() {
-  const allEvents: EventModel[] = await getAllEvents();
-  const allUnapprovedEvents: EventModel[] = await getAllUnapprovedEvents();
+  const allEvents: EventModel[] = await getAllEvents(true);
+  const allUnapprovedEvents: EventModel[] = await getAllEvents(false);
 
   return (
     <>
