d8e952fb9d3bccf78c28216709472bb6098dd956
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 11:26:52 +0200

    refactor(models/userEditSelf): removed unused

--- models/user/user-edit-self-model.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const userEditSelfModel = z.object({
-  id: z.string(),
-  name: z.string(),
-  phone_number: z.string(),
-});
-
-export type UserEditSelfModel = z.infer<typeof userEditSelfModel>;
