2b18ae158c51cd617276b7acc8552d96373da1cc
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 15:23:15 +0200

    refactor(models/materialCreate): removed unused

--- models/material/material-create-model.ts
+++
@@ -1,8 +0,0 @@
-import { materialEditModel } from '@/models/material/material-edit-model';
-import { z } from 'zod';
-
-export const materialCreateModel = materialEditModel.omit({
-  id: true,
-});
-
-export type MaterialCreateModel = z.infer<typeof materialCreateModel>;
