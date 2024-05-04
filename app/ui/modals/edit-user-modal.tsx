0045d288f96356a64588e3f7a74dbfd81736468c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 18:29:32 +0200

    feat(modals/editUser): applied modal layout

--- app/ui/modals/edit-user-modal.tsx
+++ app/ui/modals/edit-user-modal.tsx
@@ -1,35 +1,14 @@
 'use client';
 
+import ModalLayout from '@/app/ui/modals/modal-layout';
 import UserEditForm from '@/app/ui/user/user-edit-form';
+import UserEditFullForm from '@/app/ui/user/user-edit-full-form';
 import { UserModel } from '@/models/user-models';
-import { PencilIcon } from '@heroicons/react/24/outline';
 
 export default function EditUserModal(data: { user: UserModel; full: boolean }) {
   return (
-    <>
-      <button
-        className="btn"
-        onClick={() => {
-          if (document) {
-            (document.getElementById('edit_user_modal') as HTMLFormElement).showModal();
-          }
-        }}
-      >
-        <PencilIcon className="h-5" />
-        <p className="hidden md:block">Upravit</p>
-      </button>
-      <dialog id="edit_user_modal" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-base-100">✕</button>
-          </form>
-          <h3>Upravit informace</h3>
-          <UserEditForm user={data.user} />
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
-    </>
+    <ModalLayout id={data.user.id} title={'Upravit informace o uživateli'} create={false}>
+      {data.full ? <UserEditFullForm user={data.user} /> : <UserEditForm user={data.user} />}
+    </ModalLayout>
   );
 }
