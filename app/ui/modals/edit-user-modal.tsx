46b7fdc2e667d665d24a02262b918ef098804daf
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 18:40:53 +0200

    fix(modals/editUser): using parametrized form

--- app/ui/modals/edit-user-modal.tsx
+++ app/ui/modals/edit-user-modal.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import ModalLayout from '@/app/ui/modals/modal-layout';
-import UserEditForm from '@/app/ui/user/user-edit-form';
 import UserEditFullForm from '@/app/ui/user/user-edit-full-form';
 import { UserModel } from '@/models/user-models';
 
 export default function EditUserModal(data: { user: UserModel; full: boolean }) {
   return (
     <ModalLayout id={data.user.id} title={'Upravit informace o uživateli'} create={false}>
-      {data.full ? <UserEditFullForm user={data.user} /> : <UserEditForm user={data.user} />}
+      <UserEditFullForm user={data.user} full={data.full} />
     </ModalLayout>
   );
 }
