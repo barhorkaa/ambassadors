8752152d4eb3bafceaaefd04b44208501e7227dc
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 10:36:42 +0200

    refactor(models/eTCreate): removed unused

--- models/event-type/event-type-create-model.ts
+++
@@ -1,6 +0,0 @@
-import { eventTypeDefaultModel } from '@/models/event-type-models';
-import { z } from 'zod';
-
-export const eventTypeCreateModel = eventTypeDefaultModel.omit({ id: true });
-
-export type EventTypeCreateModel = z.infer<typeof eventTypeCreateModel>;
