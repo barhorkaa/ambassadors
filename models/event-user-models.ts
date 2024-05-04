f1ee86e7f26215ad7dd5cbb2cb62c26622d79e7b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 15:28:16 +0200

    fix(models/eventUser): removed zod

--- models/event-user-models.ts
+++ models/event-user-models.ts
@@ -1,13 +1,9 @@
-import { z } from 'zod';
-
-export const eventUserBasicModel = z.object({
-  id: z.string().nullable(),
-  user_name: z.string().nullable(),
-  substitute: z.boolean().nullable(),
-  user_id: z.string().nullable(),
-  event_id: z.string().nullable(),
-  event_name: z.string().nullable(),
-  approved: z.boolean(),
-});
-
-export type EventUserBasicModel = z.infer<typeof eventUserBasicModel>;
+export type EventUserBasicModel = {
+  id: string;
+  user_name: string;
+  substitute: boolean;
+  user_id: string;
+  event_id: string;
+  event_name: string;
+  approved: boolean;
+};
