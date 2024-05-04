9b6a35eb5b1bd74a5e1bb8f6b27ee907d44ca4b8
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 18:15:39 +0200

    feat(modals/editEvent): applied modal layout

--- app/ui/modals/edit-event-modal.tsx
+++ app/ui/modals/edit-event-modal.tsx
@@ -1,36 +1,14 @@
 'use client';
 
 import EventForm from '@/app/ui/event/event-form';
+import ModalLayout from '@/app/ui/modals/modal-layout';
 import { EventDetailModel } from '@/models/event-models';
 import { EventTypeMinModel } from '@/models/event-type-models';
-import { PencilIcon } from '@heroicons/react/24/outline';
 
 export default function EditEventModal(data: { eventTypes: EventTypeMinModel[]; event: EventDetailModel }) {
   return (
-    <>
-      <button
-        className="btn"
-        onClick={() => {
-          if (document) {
-            (document.getElementById('my_modal_2') as HTMLFormElement).showModal();
-          }
-        }}
-      >
-        <PencilIcon className="h-5" />
-        <p className="hidden md:block">Upravit</p>
-      </button>
-      <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-base-100">✕</button>
-          </form>
-          <h3>Upravit akci</h3>
-          <EventForm event={data.event} eventTypes={data.eventTypes} />
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
-    </>
+    <ModalLayout id={data.event.id} title={'Upravit akci'} create={false}>
+      <EventForm event={data.event} eventTypes={data.eventTypes} />
+    </ModalLayout>
   );
 }
