17d2ee7b524151ff54770a323861048f9bc334e0
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 10:29:06 +0200

    refactor(models/eventTypeBasic): removed unused

--- models/event-type/event-type-basic.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const eventTypeBasicModel = z.object({
-  id: z.string(),
-  name: z.string(),
-});
-
-export type EventTypeBasicModel = z.infer<typeof eventTypeBasicModel>;
