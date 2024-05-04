b9e47e106c4550d82a9a44663af7303fe21f4616
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 11:51:10 +0200

    fix(models/motivation): returned zod

--- models/motivation-models.ts
+++ models/motivation-models.ts
@@ -1,8 +1,12 @@
-export type MotivationModel = {
-  why: string;
-  who: string;
-  goals: string;
-  preferredEvents: string;
-  time: string;
-  userId: string;
-};
+import { z } from 'zod';
+
+export const motivationModel = z.object({
+  why: z.string(),
+  who: z.string(),
+  goals: z.string(),
+  preferredEvents: z.string(),
+  time: z.string(),
+  userId: z.string(),
+});
+
+export type MotivationModel = z.infer<typeof motivationModel>;
