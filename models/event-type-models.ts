cc9fc4e9762fcb0ef9a0e71611a13075f3e60acc
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 10:31:48 +0200

    refactor(models/eventType): moved default model, type

--- models/event-type-models.ts
+++ models/event-type-models.ts
@@ -2,3 +2,14 @@ export type EventTypeMinModel = {
   id: string;
   name: string;
 };
+
+import { z } from 'zod';
+
+export const eventTypeDefaultModel = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  instructions: z.string(),
+});
+
+export type EventTypeDefaultModel = z.infer<typeof eventTypeDefaultModel>;
