5f9db07eb1975aaf40d39e21690a4e3f0ada89c6
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 18:15:31 +0200

    feat(modals/createMaterial): applied modal layout

--- app/ui/modals/create-material-modal.tsx
+++ app/ui/modals/create-material-modal.tsx
@@ -1,34 +1,12 @@
 'use client';
 
 import { MaterialForm } from '@/app/ui/material/material-form';
-import { PlusIcon } from '@heroicons/react/24/outline';
+import ModalLayout from '@/app/ui/modals/modal-layout';
 
 export default function CreateMaterialModal() {
   return (
-    <>
-      <button
-        className="btn"
-        onClick={() => {
-          if (document) {
-            (document.getElementById('create_material_modal') as HTMLFormElement).showModal();
-          }
-        }}
-      >
-        <PlusIcon className="h-5" />
-        <p className="hidden md:block">Přidat</p>
-      </button>
-      <dialog id="create_material_modal" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-base-100">✕</button>
-          </form>
-          <h3>Nový materiál</h3>
-          <MaterialForm material={null} />
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
-    </>
+    <ModalLayout id={'create_material_modal'} title={'Nový materiál'} create={true}>
+      <MaterialForm material={null} />
+    </ModalLayout>
   );
 }
