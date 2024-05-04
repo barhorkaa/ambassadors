274a8e422759519e8c7d3ff3e6fab3f712d81326
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 15:20:03 +0200

    feat(models/material): added schema and model for manipulation

--- models/material-models.ts
+++ models/material-models.ts
@@ -1,3 +1,12 @@
+import { z } from 'zod';
+
+export const materialSchema = z.object({
+  id: z.string().optional(),
+  name: z.string(),
+  description: z.string(),
+});
+export type MaterialManipulationModel = z.infer<typeof materialSchema>;
+
 export type MaterialMinModel = {
   id: string;
   name: string;
