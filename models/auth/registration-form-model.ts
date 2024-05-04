d1726a9714b5fa46e8554524cf7c87fe95945b04
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 15:47:00 +0200

    refactor(models/registrationFrom): removed unused

--- models/auth/registration-form-model.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const RegistrationFormModel = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
-  uco: z.number().min(6).max(6),
-  phoneNumber: z.string().min(9).max(16),
-});
