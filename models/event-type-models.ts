23bd1b6af562d3bb55f365b4b51839e421bed498
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 10:47:54 +0200

    refactor(models/eTDetail): moved to common file

--- models/event-type-models.ts
+++ models/event-type-models.ts
@@ -17,3 +17,15 @@ export type EventTypeDefaultModel = z.infer<typeof eventTypeDefaultModel>;
 export const eventTypeCreateModel = eventTypeDefaultModel.omit({ id: true });
 
 export type EventTypeCreateModel = z.infer<typeof eventTypeCreateModel>;
+
+export const eventTypeDetailModel = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  instructions: z.string(),
+  created_at: z.date(),
+  updated_at: z.date(),
+  deleted_at: z.date().nullable(),
+});
+
+export type EventTypeDetailModel = z.infer<typeof eventTypeDetailModel>;
