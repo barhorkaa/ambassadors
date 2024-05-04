626c7f79ef2662a6c4b29dad0a0f3e53a516a126
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 18:19:05 +0200

    feat(modals/editMaterial): applied modal layout

--- app/ui/modals/edit-material-modal.tsx
+++ app/ui/modals/edit-material-modal.tsx
@@ -1,35 +1,13 @@
 'use client';
 
 import { MaterialForm } from '@/app/ui/material/material-form';
+import ModalLayout from '@/app/ui/modals/modal-layout';
 import { MaterialDetailModel } from '@/models/material-models';
-import { PencilIcon } from '@heroicons/react/24/outline';
 
 export default function EditMaterialModal(data: { material: MaterialDetailModel }) {
   return (
-    <>
-      <button
-        className="btn"
-        onClick={() => {
-          if (document) {
-            (document.getElementById(data.material.id) as HTMLFormElement).showModal();
-          }
-        }}
-      >
-        <PencilIcon className="h-5" />
-        <p className="hidden md:block">Upravit</p>
-      </button>
-      <dialog id={data.material.id} className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-base-100">✕</button>
-          </form>
-          <h3>Upravit informace</h3>
-          <MaterialForm material={data.material} />
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
-    </>
+    <ModalLayout id={data.material.id} title={'Upravit informace o materiálu'} create={false}>
+      <MaterialForm material={data.material} />
+    </ModalLayout>
   );
 }
