b5a2d6b30fe8d6e914d5d12b0f384c2c7d81c470
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 11:08:20 +0200

    fix(models/eventType): removed zod for detail model

--- models/event-type-models.ts
+++ models/event-type-models.ts
@@ -13,14 +13,10 @@ export const eventTypeSchema = z.object({
 });
 export type EventTypeManipulationModel = z.infer<typeof eventTypeSchema>;
 
-export const eventTypeDetailModel = z.object({
-  id: z.string(),
-  name: z.string(),
-  description: z.string(),
-  instructions: z.string(),
-  created_at: z.date(),
-  updated_at: z.date(),
-  deleted_at: z.date().nullable(),
-});
-
-export type EventTypeDetailModel = z.infer<typeof eventTypeDetailModel>;
+export type EventTypeDetailModel = EventTypeMinModel & {
+  description: string;
+  instructions: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+};
