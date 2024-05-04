26f7572585aa29e3c7a31c358baf5182e8ce2682
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 11:24:29 +0200

    refactor(models/userEditFull): removed unused

--- models/user/user-edit-full-model.ts
+++
@@ -1,9 +0,0 @@
-// export const userEditFullModel = z.object({
-//   id: z.string(),
-//   name: z.string(),
-//   phone_number: z.string(),
-//   uco: z.string().pipe(z.coerce.number()),
-//   email: z.string(),
-// });
-
-// export type UserEditFullModel = z.infer<typeof userEditFullModel>;
