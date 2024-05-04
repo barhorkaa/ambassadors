b5d034850fdf3850c9fe4293bbde97a03a522ad6
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 11:50:15 +0200

    fix(actions/motivation): changed the logic of getting motivation data from form

--- app/lib/actions/motivation.ts
+++ app/lib/actions/motivation.ts
@@ -1,12 +1,21 @@
 'use server';
 
 import { createMotivation } from '@/database/repository/motivation';
-import { MotivationFormModel } from '@/models/motivation/motivation-form-model';
+import { motivationModel } from '@/models/motivation-models';
 import { redirect } from 'next/navigation';
 
 export async function createMotivationAction(formData: FormData) {
   try {
-    const parsedData = MotivationFormModel.parse(formData);
+    const motivationForm = {
+      why: formData.get('why'),
+      who: formData.get('who'),
+      goals: formData.get('goals'),
+      preferredEvents: formData.get('preferredEvents'),
+      time: formData.get('time'),
+      userId: formData.get('userId'),
+    };
+
+    const parsedData = motivationModel.parse(motivationForm);
     await createMotivation(parsedData);
   } catch (error) {
     return 'Something went wrong';
