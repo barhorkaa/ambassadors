d62b6d82a563a355ccf2fce6cb734b6674cb3777
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-04 15:26:37 +0200

    feat(models/eventUser): moved model to new file

---
+++ models/event-user-models.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const eventUserBasicModel = z.object({
+  id: z.string().nullable(),
+  user_name: z.string().nullable(),
+  substitute: z.boolean().nullable(),
+  user_id: z.string().nullable(),
+  event_id: z.string().nullable(),
+  event_name: z.string().nullable(),
+  approved: z.boolean(),
+});
+
+export type EventUserBasicModel = z.infer<typeof eventUserBasicModel>;
