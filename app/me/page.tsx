075fd92bdd8414f8b07d40fc8730462a369a0b93
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-05 16:11:56 +0200

    fix(me/page): replaced with redirect to new personal info displaying

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,8 +1,8 @@
-import User from '@/app/ambassadors/[id]/page';
 import { auth } from '@/auth';
+import { permanentRedirect } from 'next/navigation';
 
 export default async function Me() {
   const session = await auth();
 
-  return <User params={{ id: session?.user.id! }} />;
+  permanentRedirect(`/ambassadors/${session?.user.id!}`);
 }
