04e61dcaa988ef1625d16651cdc66fe4d7b222a2
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-05 10:46:33 +0200

    feat(denied/page): added handling of session

--- app/denied/page.tsx
+++ app/denied/page.tsx
@@ -1,4 +1,15 @@
-export default function Unsuccessful() {
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+
+export default async function Unsuccessful() {
+  const session = await auth();
+  if (!session) {
+    redirect('/');
+  }
+
+  if (session.user.approved) {
+    redirect('/events');
+  }
   return (
     <div>Your account has not been verified yet, please wait until one of our managers verifies your registration</div>
   );
