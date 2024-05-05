72875d2d28bd7d5c119ffec2860e34daa3e04229
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-05 10:46:41 +0200

    feat(denied/page/role): added handling of session

--- app/denied/role/page.tsx
+++ app/denied/role/page.tsx
@@ -1,3 +1,10 @@
-export default function DeniedRole() {
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+
+export default async function DeniedRole() {
+  const session = await auth();
+  if (!session) {
+    redirect('/');
+  }
   return <div>You dont have access to this page since you are not a manager</div>;
 }
