32e07a9e5a3983f9a9947d30144f4e962f23def2
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-05 15:42:28 +0200

    feat(ui/detailRow): changed detail row style

--- app/ui/utils/detail-row.tsx
+++ app/ui/utils/detail-row.tsx
@@ -1,8 +1,11 @@
 export default function DetailRow(detail: { label: string; value: string | number }) {
   return (
     <div>
-      <p className="font-light">{detail.label}</p>
-      <p className="pl-16 text-xl">{detail.value}</p>
+      <div className="grid grid-rows-[min-content_min-content] items-end">
+        <p className="font-light text-sm">{detail.label}</p>
+        <p className="text-xl">{detail.value}</p>
+      </div>
+      <hr className="w-full h-0.5 text-black my-1 bg-base-300" />
     </div>
   );
 }
