87b12cf6bb1b8e97a5e9e6c7dae7ad094af67133
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 16:18:13 +0200

    feat(modals/createReport): added materials passing

--- app/ui/modals/create/create-report-modal.tsx
+++ app/ui/modals/create/create-report-modal.tsx
@@ -2,11 +2,12 @@
 
 import ModalLayout from '@/app/ui/modals/modal-layout';
 import ReportForm from '@/app/ui/report/report-form';
+import { MaterialMinModel } from '@/models/material-models';
 
-export default function CreateReportModal({ eventId }: { eventId: string }) {
+export default function CreateReportModal({ eventId, materials }: { eventId: string; materials: MaterialMinModel[] }) {
   return (
     <ModalLayout id={'create_report_modal'} title={'Vytvořit zprávu'} create={true}>
-      <ReportForm eventId={eventId} />
+      <ReportForm eventId={eventId} materials={materials} />
     </ModalLayout>
   );
 }
