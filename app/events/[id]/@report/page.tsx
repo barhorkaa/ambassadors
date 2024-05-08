7e6a086d1af2d0fadfb561d744b7f63ce9fdc2ee
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 20:43:08 +0200

    feat(events/id/@report/page): added page contents when there is no report

--- app/events/[id]/@report/page.tsx
+++ app/events/[id]/@report/page.tsx
@@ -1,7 +1,26 @@
-export default function ReportPage() {
+import CreateReportModal from '@/app/ui/modals/create/create-report-modal';
+import { getAllMaterials } from '@/database/repository/material';
+import { MaterialMinModel } from '@/models/material-models';
+
+export default async function ReportPage({ params }: { params: { id: string } }) {
+  const eventReport = false;
+
+  const materials: MaterialMinModel[] = await getAllMaterials();
+
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="card shadow-md rounded-none">
+      <div className="card-body">
+        <h2 className="card-title">Zpráva z akce</h2>
+
+        {!eventReport ? (
+          <div className="w-full h-full flex items-center justify-center">
+            <CreateReportModal eventId={params.id} materials={materials} />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+      {/*<h1>hello</h1>*/}
     </div>
   );
 }
