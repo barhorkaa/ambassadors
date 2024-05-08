2981e4cfc5bd4bdff6a54bb9c6b35e9d83538177
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 20:43:44 +0200

    feat(events/id/@type/page): redid the layout of type component, only necessary

--- app/events/[id]/@type/page.tsx
+++ app/events/[id]/@type/page.tsx
@@ -1,7 +1,22 @@
-export default function TypePage() {
+import { getEventTypeById } from '@/database/repository/event-type';
+import { getEventById } from '@/database/repository/events';
+import { EventDetailModel } from '@/models/event-models';
+import { EventTypeDetailModel } from '@/models/event-type-models';
+
+export default async function TypePage({ params }: { params: { id: string } }) {
+  const event: EventDetailModel = await getEventById(params.id);
+  const eventType: EventTypeDetailModel = await getEventTypeById(event.eventTypeId);
+
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="card shadow-md rounded-none">
+      <div className="card-body">
+        <h2 className="card-title font-light text-sm">Typ akce</h2>
+        <h3 className="text-xl">{eventType.name}</h3>
+        <div>
+          <p className="text-sm font-light">Instrukce</p>
+          <p className="text-lg">{eventType.instructions}</p>
+        </div>
+      </div>
     </div>
   );
 }
