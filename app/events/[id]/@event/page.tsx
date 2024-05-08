dadf19704311593704e68b26797cebc7f5e0da3c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 20:44:13 +0200

    feat(events/id/@event/page): filled the event page

--- app/events/[id]/@event/page.tsx
+++ app/events/[id]/@event/page.tsx
@@ -1,7 +1,13 @@
-export default function EventPage() {
+import EventDetail from '@/app/ui/event/event-detail';
+import { getEventById } from '@/database/repository/events';
+import { EventDetailModel } from '@/models/event-models';
+
+export default async function EventPage({ params }: { params: { id: string } }) {
+  const event: EventDetailModel = await getEventById(params.id);
+
   return (
     <div>
-      <h1>hello</h1>
+      <EventDetail event={event} />
     </div>
   );
 }
