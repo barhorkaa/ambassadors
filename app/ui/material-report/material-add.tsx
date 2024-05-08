4837d727ebe60e338c402a105aa6cbb51d0e1740
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 16:11:34 +0200

    feat(ui/materialAdd): created component add multiple materials

---
+++ app/ui/material-report/material-add.tsx
@@ -0,0 +1,46 @@
+import MaterialReportSelect from '@/app/ui/material-report/material-report-select';
+import { MaterialMinModel } from '@/models/material-models';
+import { ChangeEvent, useState } from 'react';
+
+function DynamicForm({ materials }: { materials: MaterialMinModel[] }) {
+  const [inputs, setInputs] = useState([{ materialId: '', amount: 0 }]);
+
+  console.log('inpusts are: ', inputs);
+  // Function to handle change in input field
+  const handleInputChange = (index: number, event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const list = [...inputs];
+    // list[index][name] = value;
+    setInputs(list);
+  };
+
+  const handleAddClick = () => {
+    setInputs([...inputs, { materialId: '', amount: 0 }]);
+  };
+
+  const handleRemoveClick = (index: number) => {
+    const list = [...inputs];
+    list.splice(index, 1);
+    setInputs(list);
+  };
+
+  return (
+    <div>
+      <div className="form-control">
+        <label className="label" htmlFor="numerOfAttendees">
+          <span className="label-text">materiály</span>
+        </label>
+        {inputs.map((input, i) => (
+          <div key={i}>
+            <MaterialReportSelect materials={materials} selectedEvent={undefined} />
+            {inputs.length !== 1 && <button onClick={() => handleRemoveClick(i)}>Remove</button>}
+            {inputs.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+          </div>
+        ))}
+        {/*<button type="submit">Submit</button>*/}
+      </div>
+    </div>
+  );
+}
+
+export default DynamicForm;
