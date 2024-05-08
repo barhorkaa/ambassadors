6f833c912c819641d04eaaa903a3e59b87c11f07
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 20:49:15 +0200

    fix(ui/eventUserDetail): removed unnecessary undefined handling

--- app/ui/event-user/event-user-detail.tsx
+++ app/ui/event-user/event-user-detail.tsx
@@ -3,16 +3,7 @@ import { UserIcon } from '@heroicons/react/24/outline';
 
 export async function EventUserDetail({ event_id }: { event_id: string }) {
   let signedUpForEvent = await getSignUpsForEvent(event_id);
-  console.log('these users are signed for this event', signedUpForEvent);
-  if (signedUpForEvent === undefined) {
-    return <div>Na akci není přhlášen nikto</div>;
-  }
-
   let substitutesForEvent = await getSubstitutesForEvent(event_id);
-  console.log('these users are substitutes for this event', substitutesForEvent);
-  if (substitutesForEvent === undefined) {
-    return <div>Na akci není přhlášen žádný náhradník</div>;
-  }
 
   return (
     <div className="flex flex-col">
