e3684d6b2a840d14cd8200c11b84fc0ce26bd4fa
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 16:13:22 +0200

    feat(models/report): created report schema

--- models/report-models.ts
+++ models/report-models.ts
@@ -5,3 +5,13 @@ export const materialAmountSchema = z.object({
   amount: z.string().pipe(z.coerce.number()),
 });
 export type MaterialAmountModel = z.infer<typeof materialAmountSchema>;
+
+export const reportSchema = z.object({
+  eventId: z.string(),
+  numberOfAttendees: z.string().pipe(z.coerce.number()),
+  notes: z.string(),
+  ideas: z.string(),
+  materials: materialAmountSchema.array(),
+  // ambassadors: z.string(),
+});
+export type ReportModel = z.infer<typeof reportSchema>;
