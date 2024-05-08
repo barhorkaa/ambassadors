2e0a43f8ef58f5605eb18af4f094ef2c63aeadb2
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 20:58:42 +0200

    fix(ui/eventUserDetail): changed displaying

--- app/ui/event-user/event-user-detail.tsx
+++ app/ui/event-user/event-user-detail.tsx
@@ -8,21 +8,36 @@ export async function EventUserDetail({ event_id }: { event_id: string }) {
   return (
     <div className="flex flex-col">
       <div>
-        {signedUpForEvent.map((user, iterator) => (
-          <div key={iterator} className="flex flex-row gap-4 pl-8 py-2">
-            <UserIcon className="h-6" />
-            <p className="text-lg">{user.user_name}</p>
+        <h3 className="card-title">Přihlášení</h3>
+        {signedUpForEvent.length !== 0 ? (
+          <div>
+            {signedUpForEvent.map((user, iterator) => (
+              <div key={iterator} className="flex flex-row gap-4 pl-8 py-2">
+                <UserIcon className="h-6" />
+                <p className="text-lg">{user.user_name}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        ) : (
+          <div>Na akci není přihlášen nikto</div>
+        )}
       </div>
       <div>
-        <h2 className="card-title">Záskok</h2>
-        {substitutesForEvent.map((user, iterator) => (
-          <div key={iterator} className="flex flex-row gap-4 pl-16 py-2">
-            <UserIcon className="h-6" />
-            <p className="text-lg">{user.user_name}</p>
+        {/*<h3 className="card-title">Záskok</h3>*/}
+        {substitutesForEvent.length !== 0 ? (
+          <div>
+            <h3 className="card-title">Záskok</h3>
+
+            {substitutesForEvent.map((user, iterator) => (
+              <div key={iterator} className="flex flex-row gap-4 pl-16 py-2">
+                <UserIcon className="h-6" />
+                <p className="text-lg">{user.user_name}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        ) : (
+          <div></div>
+        )}
       </div>
     </div>
   );
