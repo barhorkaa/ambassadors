503f1911deb2453c60d1e434fd97978d51568d2e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 20:45:08 +0200

    feat(events/layout/id): added slots to layout

--- app/events/[id]/layout.tsx
+++ app/events/[id]/layout.tsx
@@ -4,6 +4,33 @@ export const metadata: Metadata = {
   title: 'Detail akce | AmbassadorsFIMU',
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+export default function Layout({
+  children,
+  event,
+  type,
+  users,
+  report,
+}: {
+  children: React.ReactNode;
+  event: React.ReactNode;
+  type: React.ReactNode;
+  users: React.ReactNode;
+  report: React.ReactNode;
+}) {
+  return (
+    <section>
+      <div className="align-text-bottom">
+        <h1 className="font-light text-xl">Detail akce</h1>
+      </div>
+      {children}
+      <div>
+        {event}
+        <div className="grid grid-cols-3 grid-rows-3">
+          {type}
+          {users}
+          {report}
+        </div>
+      </div>
+    </section>
+  );
 }
