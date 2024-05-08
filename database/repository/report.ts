cdd75d42d6282c9dae1a05806ff318a05e1a0cf8
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 16:17:08 +0200

    feat(repo/report): added function for creating material records

--- database/repository/report.ts
+++ database/repository/report.ts
@@ -13,9 +13,26 @@ export async function createReport(report: ReportModel) {
       .returning('id')
       .executeTakeFirstOrThrow();
 
+    await createMaterialReports(materials, reportId.id);
   } catch (e) {
     console.error(e);
     throw new DatabaseError({ name: 'DATABASE_CREATE_ERROR', message: 'Unable to create report', cause: e });
   }
 }
 
+export async function createMaterialReports(materials: MaterialAmountModel[], reportId: string) {
+  try {
+    const fullMaterials = materials.map((material) => {
+      return { material_id: material.materialId, amount: material.amount, report_id: reportId };
+    });
+
+    await db.insertInto('materialReport').values(fullMaterials).execute();
+  } catch (e) {
+    console.error(e);
+    throw new DatabaseError({
+      name: 'DATABASE_CREATE_ERROR',
+      message: 'Unable to create report material records',
+      cause: e,
+    });
+  }
+}
