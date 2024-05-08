196fe9468b9915542bc58b2e884994b9b9b81cbd
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 16:11:18 +0200

    feat(ui/materialReportAdd): created component to select material and amount

---
+++ app/ui/material-report/material-report-select.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { MaterialMinModel } from '@/models/material-models';
+import { useState } from 'react';
+
+export default function MaterialReportSelect(props: {
+  materials: MaterialMinModel[];
+  selectedEvent: string | undefined;
+}) {
+  const [material, setMaterial] = useState(props.selectedEvent);
+
+  const handleMaterialChange = (event: any) => {
+    setMaterial(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className="form-control">
+        <label className="label" htmlFor="material">
+          <span className="label-text">Typ akce</span>
+        </label>
+        <select
+          className="select select-bordered w-full max-w-xs"
+          id="eventType"
+          name="materialId"
+          value={material}
+          onChange={handleMaterialChange}
+        >
+          <option>Vyberte materiál</option>
+          {props.materials.map((material) => (
+            <option key={material.id} value={material.id}>
+              {material.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="form-control">
+        <label className="label" htmlFor="numerOfAttendees">
+          <span className="label-text">Počet kusů</span>
+        </label>
+        <input
+          id="amount"
+          type="number"
+          name="amount"
+          placeholder="Počet kusů"
+          className="input input-bordered"
+          required
+        />
+      </div>
+    </div>
+  );
+}
