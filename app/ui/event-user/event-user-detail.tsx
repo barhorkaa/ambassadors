01f0facb131c77428795b45ac604e82447874d13
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-08 20:45:33 +0200

    feat(ui/eventUserDetail): changed detail

--- app/ui/event-user/event-user-detail.tsx
+++ app/ui/event-user/event-user-detail.tsx
@@ -15,28 +15,23 @@ export async function EventUserDetail({ event_id }: { event_id: string }) {
   }
 
   return (
-    <div className="flex flex-col md:flex-row md:gap-16">
+    <div className="flex flex-col">
       <div>
-        <h2>
-          Na akci {signedUpForEvent.length > 1 ? 'jsou' : 'je'} přihlášen{signedUpForEvent.length > 1 ? 'i' : ''}
-        </h2>
         {signedUpForEvent.map((user, iterator) => (
-          <div key={iterator} className="flex flex-row gap-4 pl-16 py-2">
+          <div key={iterator} className="flex flex-row gap-4 pl-8 py-2">
             <UserIcon className="h-6" />
             <p className="text-lg">{user.user_name}</p>
           </div>
         ))}
       </div>
       <div>
-        <h2>
-          Náhradní{signedUpForEvent.length > 1 ? 'ci' : 'k'}
-          {substitutesForEvent.map((user, iterator) => (
-            <div key={iterator} className="flex flex-row gap-4 pl-16 py-2">
-              <UserIcon className="h-6" />
-              <p className="text-lg">{user.user_name}</p>
-            </div>
-          ))}
-        </h2>
+        <h2 className="card-title">Záskok</h2>
+        {substitutesForEvent.map((user, iterator) => (
+          <div key={iterator} className="flex flex-row gap-4 pl-16 py-2">
+            <UserIcon className="h-6" />
+            <p className="text-lg">{user.user_name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
