59e9f8f93682e30759de9184112d811cef32866e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-09 15:20:21 +0200

    feat(models/report): added optional field to amount schema

--- models/report-models.ts
+++ models/report-models.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const materialAmountSchema = z.object({
   materialId: z.string(),
+  materialName: z.string().optional(),
   amount: z.string().pipe(z.coerce.number()),
 });
 export type MaterialAmountModel = z.infer<typeof materialAmountSchema>;
