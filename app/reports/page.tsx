03e0f7bcac988cde77750d8c02fdaee3299afa00
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-09 16:19:32 +0200

    feat(reports/page): added displaying of unapproved reports events

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,3 +1,16 @@
-export default function () {
-  return <>Hello tu budu reporty</>;
+import { EventList } from '@/app/ui/utils/event-list';
+import { getEventsWithUnapprovedReports } from '@/database/repository/report';
+
+export default async function () {
+  const eventsWithUnapprovedReports = await getEventsWithUnapprovedReports();
+  return (
+    <>
+      <h1>Zprávy z výjezdů</h1>
+      <EventList
+        title={''}
+        list={eventsWithUnapprovedReports!}
+        emptyMessage={'V tento moment jsou všechny zprávy potvrzeny!'}
+      />
+    </>
+  );
 }
