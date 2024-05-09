bd9a984aaee8cafd4906e47e4f1265b731c5b6ae
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-09 16:31:50 +0200

    feat(events/id/@report/page): added approve button

--- app/events/[id]/@report/page.tsx
+++ app/events/[id]/@report/page.tsx
@@ -1,5 +1,8 @@
+import { approveReportAction } from '@/app/lib/actions/report';
+import ApproveButton from '@/app/ui/button/approve-button';
 import CreateReportModal from '@/app/ui/modals/create/create-report-modal';
 import ReportDetail from '@/app/ui/report/report-detail';
+import { auth } from '@/auth';
 import { getAllMaterials } from '@/database/repository/material';
 import { getEventReport } from '@/database/repository/report';
 import { MaterialMinModel } from '@/models/material-models';
@@ -8,11 +11,16 @@ export default async function ReportPage({ params }: { params: { id: string } })
   const eventReport = await getEventReport(params.id);
 
   const materials: MaterialMinModel[] = await getAllMaterials();
-
+  const session = await auth();
   return (
     <div className="data-display">
       <div className="card-body">
-        <h2 className="card-title">Zpráva z akce</h2>
+        <div className="flex flex-row justify-between items-end">
+          <h2 className="card-title">Zpráva z akce</h2>
+          {session?.user.role === 'manager' && eventReport && !eventReport.approved && (
+            <ApproveButton fun={approveReportAction} id={eventReport.id} />
+          )}
+        </div>
 
         {!eventReport ? (
           <div className="w-full h-full flex items-center justify-center">
