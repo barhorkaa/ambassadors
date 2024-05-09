6635664bbb46b85436fa2e8c93574c802d0f1d97
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-09 15:19:27 +0200

    feat(ui/reportDetail): created component for displaying details about report

---
+++ app/ui/report/report-detail.tsx
@@ -0,0 +1,30 @@
+import DetailRow from '@/app/ui/utils/detail-row';
+import { ReportDetailModel } from '@/models/report-models';
+
+export default function ReportDetail({ report }: { report: ReportDetailModel }) {
+  const materials = [...report.materials];
+  return (
+    <div>
+      <DetailRow label={'Kolik bylo na akci lidí?'} value={report.numberOfAttendees} />
+      <DetailRow label={'Co se na akci dělo?'} value={report.notes} />
+      <DetailRow label={'Co do přístě zlepšit?'} value={report.ideas} />
+      <p className="font-light text-sm">Materiály</p>
+      {report.materials.map((materialAmount) => (
+        <MaterialAmount
+          key={materialAmount.materialId}
+          material={materialAmount.materialName!}
+          amount={materialAmount.amount}
+        />
+      ))}
+    </div>
+  );
+}
+
+function MaterialAmount({ material, amount }: { material: string; amount: number }) {
+  return (
+    <div className="grid grid-cols-2">
+      <p className="col-start-1">{material}</p>
+      <p className="col-start-2">{amount}</p>
+    </div>
+  );
+}
