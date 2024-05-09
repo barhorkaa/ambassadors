a7d9a9ebcf9a89e2d83febcaa5d30fa72dd55372
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-09 21:21:31 +0200

    feat(ambassadors/layout/id): added slots to layout

--- app/ambassadors/layout.tsx
+++ app/ambassadors/layout.tsx
@@ -4,6 +4,38 @@ export const metadata: Metadata = {
   title: 'Uživatelé | AmbassadorsFIMU',
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+export default function Layout({
+  children,
+  ambassadors,
+  managers,
+  unapproved,
+}: {
+  children: React.ReactNode;
+  ambassadors: React.ReactNode;
+  managers: React.ReactNode;
+  unapproved: React.ReactNode;
+}) {
+  return (
+    <section>
+      <h1>Uživatelé</h1>
+      <hr className="w-full" />
+      <div role="tablist" className="tabs tabs-lifted">
+        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Nepotvrzeni" />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          {unapproved}
+        </div>
+
+        <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Ambasadoři" />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          {ambassadors}
+        </div>
+
+        <input type="radio" name="my_tabs_2" role="tab" className="tab w-fit" aria-label="Manažeři" />
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          {managers}
+        </div>
+      </div>
+      {children}
+    </section>
+  );
 }
