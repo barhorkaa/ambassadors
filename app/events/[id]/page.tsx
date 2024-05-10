bc6ebf14cfc5c304f175e361bdb834ce2a41e74b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-10 15:27:21 +0200

    feat(event/page/id): added control so that only managers see deleted event

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,10 +1,17 @@
 import { auth } from '@/auth';
+import { getEventById } from '@/database/repository/events';
+import { EventDetailModel } from '@/models/event-models';
 import { redirect } from 'next/navigation';
 
 export default async function Event({ params }: { params: { id: string } }) {
   const session = await auth();
+
   if (!session) {
     redirect('/login');
+  }
+  const event: EventDetailModel = await getEventById(params.id);
+  if (event.deletedAt !== null && session.user.role !== 'manager') {
+    redirect('/denied/role');
   }
 
   return null;
