37380f1f3ecbdbe5e34a1d54759d164ab791a71a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-10 16:20:03 +0200

    feat(modals/deleteEventType): created modal for deleting event types

---
+++ app/ui/modals/delete/delete-event-type-modal.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { deleteEventTypeAction } from '@/app/lib/actions/event-type';
+import { DeleteButton } from '@/app/ui/button/delete-button';
+import ModalLayout from '@/app/ui/modals/modal-layout';
+
+export default function DeleteEventTypeModal({ eventTypeId }: { eventTypeId: string }) {
+  return (
+    <ModalLayout id={'delete_event_type_modal'} title={'Opravdu chcete smazat tento typ akce?'} modalType={'delete'}>
+      <div className="pt-4 flex flex-col gap-4">
+        <p>Smazáni typu akce se nedá vrátit.</p>
+        <p>
+          Tento druh akce se již nebude moct použít pro vytvoření noých akcí. Stávajíci akce s tímto typem ale zůstanou
+          nezměneny.
+        </p>
+        <div className="flex justify-end">
+          <DeleteButton fun={deleteEventTypeAction} id={eventTypeId} modalId={'delete_event_type_modal'} />
+        </div>
+      </div>
+    </ModalLayout>
+  );
+}
