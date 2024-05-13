4bda985454cd349c1d199a13ba3e2d7e7ef41844
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 00:12:38 +0200

    feat(ui/eventForm): switched to using new component

--- app/ui/event/event-form.tsx
+++ app/ui/event/event-form.tsx
@@ -1,6 +1,7 @@
 import { createEventAction, updateEventAction } from '@/app/lib/actions/events';
 import SubmitButton from '@/app/ui/button/submit-button';
 import EventTypeSelect from '@/app/ui/event-type/event-type-select';
+import FormControl from '@/app/ui/utils/form-control';
 import { EventDetailModel } from '@/models/event-models';
 import { EventTypeMinModel } from '@/models/event-type-models';
 import { format } from 'date-fns';
@@ -14,44 +15,26 @@ export default function EventForm({
 }) {
   return (
     <form action={event === null ? createEventAction : updateEventAction} className="card-body">
-      <div className="form-control">
-        <label className="label" htmlFor="name">
-          <span className="label-text">Název akce</span>
-        </label>
-        <input
-          id="name"
-          defaultValue={event?.name}
-          type="text"
-          name="name"
-          placeholder="Gymnázium Třídy Kapitána Jaroše"
-          required
-        />
-      </div>
-      <div className="form-control">
-        <label className="label" htmlFor="name">
-          <span className="label-text">Limit počtu lidí</span>
-        </label>
-        <input
-          id="limit"
-          defaultValue={event?.limit}
-          type="number"
-          name="limit"
-          placeholder="Limit počtu lidí"
-          required
-        />
-      </div>
-      <div className="form-control">
-        <label className="label" htmlFor="who">
-          <span className="label-text">Datum akce (může zůstat nevyplněno)</span>
-        </label>
-        <input
-          id="date"
-          defaultValue={event?.date ? format(event?.date, 'yyyy-MM-dd') : ''}
-          type="date"
-          name="date"
-          placeholder=""
-        />
-      </div>
+      <FormControl
+        title={'Název akce'}
+        id={'name'}
+        defaultValue={event?.name}
+        placeholder={'Gymnázium Třídy Kapitána Jaroše'}
+      />
+      <FormControl
+        title={'Limit počtu lidí'}
+        id={'limit'}
+        type={'number'}
+        placeholder={'Kolik ambasadorů může na akci'}
+        defaultValue={event?.limit}
+      />
+      <FormControl
+        title={'Datum akce (může zůstat nevyplněno)'}
+        id={'date'}
+        type={'date'}
+        defaultValue={event?.date ? format(event?.date, 'yyyy-MM-dd') : ''}
+        required={false}
+      />
       <div className="form-control">
         <input id="id" value={event?.id} type="hidden" name="id" />
       </div>
