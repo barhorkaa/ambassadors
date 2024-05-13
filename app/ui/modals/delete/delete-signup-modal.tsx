7d3bf84a2ee62026cb442b3acc6a734191c67e08
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-13 20:11:57 +0200

    feat(modal/deleteSignUp): added delete modal when signing out of an event

---
+++ app/ui/modals/delete/delete-signup-modal.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { deleteSignUpAction } from '@/app/lib/actions/event-user';
+import ModalLayout from '@/app/ui/modals/modal-layout';
+
+export default function DeleteSignUpModal({ event_id, user_id }: { event_id: string; user_id: string }) {
+  return (
+    <ModalLayout id={'delete_signup'} title={'Opravdu se chcete odhlásit z akce?'} modalType={'delete'}>
+      <div className="pt-4 flex flex-col gap-4">
+        <p>Po odhlásení se můžete znova přihlásit.</p>
+        <p>Může se ale stát, že přijdete o své místo, pokud je na akci přihlášen nejký náhradník.</p>
+        <div className="flex justify-end">
+          <button
+            className="btn"
+            onClick={async () => {
+              await deleteSignUpAction(event_id, user_id);
+              if (document) {
+                (document.getElementById('delete_signup') as HTMLFormElement).close();
+              }
+            }}
+          >
+            Ohlásit se z akce
+          </button>
+          {/*<DeleteButton fun={deleteMaterialAction} id={materialId} modalId={"delete_signup"} />*/}
+        </div>
+      </div>
+    </ModalLayout>
+  );
+}
