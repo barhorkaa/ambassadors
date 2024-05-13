13c5c7a4cb58e268594e2a980c40750f637c8e4f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-13 19:15:51 +0200

    feat(ambassadors/id/@signups/page): created component to show managers ambassador's signups

---
+++ app/ambassadors/(without-layout)/[id]/@signups/page.tsx
@@ -0,0 +1,17 @@
+import { EventList } from '@/app/ui/utils/event-list';
+import { getUserSignUps } from '@/database/repository/event-user';
+import { EventModel } from '@/models/event-models';
+
+export default async function SignUps({ params }: { params: { id: string } }) {
+  const userEvents: EventModel[] = await getUserSignUps(params.id, false);
+  const userSubstitutes: EventModel[] = await getUserSignUps(params.id, true);
+
+  return (
+    <div className="data-display">
+      <div className="card-body">
+        <EventList title={'Akce kde je ambasador přihlášen'} list={userEvents} emptyMessage={''} />
+        <EventList title={'Akce kde je ambasador zástup'} list={userSubstitutes} emptyMessage={''} />
+      </div>
+    </div>
+  );
+}
