5c094dc348d4c9fd678dd51093a9fd3a972e8fce
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 00:17:13 +0200

    feat(ui/reportForm): switched to using new component

--- app/ui/report/report-form.tsx
+++ app/ui/report/report-form.tsx
@@ -1,36 +1,31 @@
 import { createReportAction } from '@/app/lib/actions/report';
 import SubmitButton from '@/app/ui/button/submit-button';
 import MaterialAdd from '@/app/ui/material-report/material-add';
+import FormControl from '@/app/ui/utils/form-control';
 import { MaterialMinModel } from '@/models/material-models';
 
 export default function ReportForm({ eventId, materials }: { eventId: string; materials: MaterialMinModel[] }) {
   return (
     <form action={createReportAction} className="card-body">
-      <div className="form-control">
-        <label className="label" htmlFor="numberOfAttendees">
-          <span className="label-text">Kolik lidí bylo na akci?</span>
-        </label>
-        <input
-          id="numberOfAttendees"
-          type="number"
-          name="numberOfAttendees"
-          placeholder="Počet zúčastněných"
-          required
-        />
-      </div>
+      <FormControl
+        title={'Kolik lidí bylo na akci?'}
+        id={'numberOfAttendees'}
+        type={'number'}
+        placeholder={'Počet zúčastněných'}
+      />
       <MaterialAdd materials={materials} />
-      <div className="form-control">
-        <label className="label" htmlFor="notes">
-          <span className="label-text">Co se na akci dělo?</span>
-        </label>
-        <textarea id="notes" name="notes" placeholder="Poznámky k dění na akci" required />
-      </div>
-      <div className="form-control">
-        <label className="label" htmlFor="ideas">
-          <span className="label-text">Co do budoucna zlepšit?</span>
-        </label>
-        <textarea id="ideas" name="ideas" placeholder="Vaše nápady na zlepšení" required />
-      </div>
+      <FormControl
+        title={'Co se na akci dělo?'}
+        id={'notes'}
+        inputType={'area'}
+        placeholder={'Poznámky k dění na akci'}
+      />
+      <FormControl
+        title={'Co do budoucna zlepšit?'}
+        id={'ideas'}
+        inputType={'area'}
+        placeholder={'Vaše nápady na zlepšení'}
+      />
       <input id="id" type="hidden" name="eventId" value={eventId} required />
       <SubmitButton title={'Odeslat'} modalId={'create_report_modal'} />
     </form>
