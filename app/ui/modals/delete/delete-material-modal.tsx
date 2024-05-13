35c046368a4eebbeb8bd92c057d093dcfd34f6d4
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-13 22:56:47 +0200

    fix(modals/deleteMaterial): fixed modal id

--- app/ui/modals/delete/delete-material-modal.tsx
+++ app/ui/modals/delete/delete-material-modal.tsx
@@ -6,7 +6,7 @@ import ModalLayout from '@/app/ui/modals/modal-layout';
 
 export default function DeleteMaterialModal({ materialId }: { materialId: string }) {
   return (
-    <ModalLayout id={materialId} title={'Opravdu chcete smazat tento materiál?'} modalType={'delete'}>
+    <ModalLayout id={'delete' + materialId} title={'Opravdu chcete smazat tento materiál?'} modalType={'delete'}>
       <div className="pt-4 flex flex-col gap-4">
         <p>Smazáni materiálu se nedá vrátit.</p>
         <p>
@@ -14,7 +14,7 @@ export default function DeleteMaterialModal({ materialId }: { materialId: string
           vyskytuje ale zůstanou nezměneny.
         </p>
         <div className="flex justify-end">
-          <DeleteButton fun={deleteMaterialAction} id={materialId} modalId={materialId} />
+          <DeleteButton fun={deleteMaterialAction} id={materialId} modalId={'delete' + materialId} />
         </div>
       </div>
     </ModalLayout>
