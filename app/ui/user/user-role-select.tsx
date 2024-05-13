011f516475a0906af2a4747034b74a20e63823ba
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 00:46:31 +0200

    feat(ui/userRoleSelect): created component for selecting the user role

---
+++ app/ui/user/user-role-select.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export default function UserRoleSelect({ currentRole }: { currentRole: 'ambassador' | 'manager' }) {
+  const [userRole, setUserRole] = useState(currentRole);
+
+  const handleEventTypeChange = (event: any) => {
+    setUserRole(event.target.value);
+  };
+
+  return (
+    <div className="form-control">
+      <label className="label" htmlFor="role">
+        <span className="label-text">Role uživatele</span>
+      </label>
+      <select
+        className="select select-bordered w-full max-w-xs"
+        id="role"
+        name="role"
+        value={userRole}
+        onChange={handleEventTypeChange}
+      >
+        <option key={'manager'} value={'manager'}>
+          Manažér
+        </option>
+        <option key={'ambassador'} value={'ambassador'}>
+          Ambasador
+        </option>
+      </select>
+    </div>
+  );
+}
