1b0c98087ef91d07388fe6498e893972da21545e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 00:28:52 +0200

    feat(ui/editUserForm): switched to using new component

--- app/ui/user/user-edit-form.tsx
+++ app/ui/user/user-edit-form.tsx
@@ -1,43 +1,17 @@
 import { editUserAction, editUserFullAction } from '@/app/lib/actions/users';
 import SubmitButton from '@/app/ui/button/submit-button';
+import FormControl from '@/app/ui/utils/form-control';
 import { UserModel } from '@/models/user-models';
 
 export default function UserEditForm({ user, full }: { user: UserModel; full: boolean }) {
   return (
     <form action={full ? editUserFullAction : editUserAction} className="card-body">
-      <div className="form-control">
-        <label className="label" htmlFor="name">
-          <span className="label-text">Celé jméno</span>
-        </label>
-        <input id="name" defaultValue={user.name} type="text" name="name" placeholder="Jméno" required />
-      </div>
-      <div className="form-control">
-        <label className="label" htmlFor="phoneNumber">
-          <span className="label-text">Telefonní číslo</span>
-        </label>
-        <input
-          id="phoneNumber"
-          defaultValue={Number(user.phone_number)}
-          type="number"
-          name="phoneNumber"
-          placeholder="Telefónní číslo"
-          required
-        />
-      </div>
+      <FormControl title={'Celé jméno'} id={'name'} defaultValue={user.name} placeholder={'Jméno'} />
+      <FormControl title={'Telefonní číslo'} id={'phoneNumber'} type={'number'} defaultValue={+user.phone_number} />
       {full && (
         <>
-          <div className="form-control">
-            <label className="label" htmlFor="uco">
-              <span className="label-text">UČO</span>
-            </label>
-            <input id="uco" defaultValue={Number(user.uco)} type="number" name="uco" placeholder="UČO" required />
-          </div>
-          <div className="form-control">
-            <label className="label" htmlFor="email">
-              <span className="label-text">Email</span>
-            </label>
-            <input id="email" defaultValue={user.email} type="email" name="email" placeholder="E-mail" required />
-          </div>
+          <FormControl title={'UČO'} id={'uco'} type={'number'} defaultValue={+user.uco} />
+          <FormControl title={'E-mail'} id={'email'} type={'email'} defaultValue={user.email} />
         </>
       )}
       <input id="id" type="hidden" name="id" value={user.id} required />
