cd932f6d815ac3cace10d6a75a556e1cfe049b6b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-13 17:16:20 +0200

    feat(denied/page/role): added content to page

--- app/denied/role/page.tsx
+++ app/denied/role/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/auth';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default async function DeniedRole() {
@@ -6,5 +7,19 @@ export default async function DeniedRole() {
   if (!session) {
     redirect('/');
   }
-  return <div>You dont have access to this page since you are not a manager</div>;
+
+  return (
+    <div className="hero">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="hero-title">Upss... </h1>
+          <h1 className="hero-title">K této části aplikace nemáte přístup</h1>
+          <p className="py-6">Stránka, kterou jste se pokusili načíst vám není přístupná.</p>
+          <Link href={'/'} className="btn">
+            Zpátky na úvodní stránku
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
