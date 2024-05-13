5328f751935fae3932a16d422ee9f13fbabe21b4
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-13 23:48:26 +0200

    feat(ui/eventTypeForm): switched to using new component

--- app/ui/event-type/event-type-form.tsx
+++ app/ui/event-type/event-type-form.tsx
@@ -1,47 +1,24 @@
 import { createEventTypeAction, editEventTypeAction } from '@/app/lib/actions/event-type';
 import SubmitButton from '@/app/ui/button/submit-button';
+import FormControl from '@/app/ui/utils/form-control';
 import { EventTypeDetailModel } from '@/models/event-type-models';
 
 export function EventTypeForm({ eventType }: { eventType: EventTypeDetailModel | null }) {
   return (
     <form action={eventType === null ? createEventTypeAction : editEventTypeAction} className="card-body">
-      <div className="form-control">
-        <label className="label" htmlFor="email">
-          <span className="label-text">Název typu akce</span>
-        </label>
-        <input
-          id="name"
-          defaultValue={eventType?.name}
-          type="text"
-          name="name"
-          placeholder="Název typu akce"
-          required
-        />
-      </div>
-      <div className="form-control">
-        <label className="label" htmlFor="password">
-          <span className="label-text">Popis akce</span>
-        </label>
-        <textarea
-          id="description"
-          defaultValue={eventType?.description}
-          name="description"
-          placeholder="Popis akce"
-          required
-        />
-      </div>
-      <div className="form-control">
-        <label className="label" htmlFor="password">
-          <span className="label-text">Instrukce pro ambasadory</span>
-        </label>
-        <textarea
-          id="instructions"
-          defaultValue={eventType?.instructions}
-          name="instructions"
-          placeholder="Instrukce pro ambasadory"
-          required
-        />
-      </div>
+      <FormControl title={'Název typu akce'} id={'name'} defaultValue={eventType?.name} />
+      <FormControl
+        title={'Popis typu akce'}
+        id={'description'}
+        inputType={'area'}
+        defaultValue={eventType?.description}
+      />
+      <FormControl
+        title={'Popis typu akce'}
+        id={'instructions'}
+        inputType={'area'}
+        defaultValue={eventType?.instructions}
+      />
       <div className="form-control">
         <input id="id" value={eventType?.id} type="hidden" name="id" />
       </div>
