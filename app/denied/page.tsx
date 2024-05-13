5e5882093db11d550335571f04b7d6bd17efd7cb
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-13 17:16:25 +0200

    feat(denied/page): added content to page

--- app/denied/page.tsx
+++ app/denied/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/auth';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default async function Unsuccessful() {
@@ -11,6 +12,21 @@ export default async function Unsuccessful() {
     redirect('/events');
   }
   return (
-    <div>Your account has not been verified yet, please wait until one of our managers verifies your registration</div>
+    <div className="hero">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="hero-title">Váš účet ješte nebyl potvrzen</h1>
+          <p className="py-6">
+            K této části aplikace nemáte přistup jelikož váš účet ješte nebyl potvrzen zaměstnancem OVVSP.
+          </p>
+          <p className="pb-6">
+            Zkuste se vrátit později. Kdyby váš účet i po týdnu nebyl potvrzen, napište email na propagace@fi.muni.cz{' '}
+          </p>
+          <Link href={'/'} className="btn">
+            Zpátky na úvodní stránku
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
