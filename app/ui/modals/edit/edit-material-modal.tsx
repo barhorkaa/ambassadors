a766d6ae6b9e102dead05ba3133969493491b60b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-13 22:56:55 +0200

    fix(modals/editMaterial): fixed modal id

--- app/ui/modals/edit/edit-material-modal.tsx
+++ app/ui/modals/edit/edit-material-modal.tsx
@@ -6,7 +6,7 @@ import { MaterialDetailModel } from '@/models/material-models';
 
 export default function EditMaterialModal(data: { material: MaterialDetailModel }) {
   return (
-    <ModalLayout id={data.material.id} title={'Upravit informace o materiálu'} modalType={'edit'}>
+    <ModalLayout id={'edit' + data.material.id} title={'Upravit informace o materiálu'} modalType={'edit'}>
       <MaterialForm material={data.material} />
     </ModalLayout>
   );
