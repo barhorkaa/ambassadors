d66b9da6a8b5f3a0fed4d70496ff555d6ed07d6e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 22:22:20 +0200

    fix(models/user): added min length for password for registration

--- models/user-models.ts
+++ models/user-models.ts
@@ -14,7 +14,7 @@ export type UserEditModel = z.infer<typeof userEditSchema>;
 export const userCreateSchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  password: z.string(),
+  password: z.string().min(6),
   uco: z.string().min(6).max(6).pipe(z.coerce.number()),
   phone_number: z.string().min(9).max(16),
 });
