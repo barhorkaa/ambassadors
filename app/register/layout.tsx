b0bd8b72610c2949f310ba02c42be3532c8eb257
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 22:05:34 +0200

    fix(register/layout): if session then redirect

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,9 +1,15 @@
+import { auth } from '@/auth';
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Registrace | AmbassadorsFIMU',
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+  if (session) {
+    redirect('/events');
+  }
   return <section>{children}</section>;
 }
