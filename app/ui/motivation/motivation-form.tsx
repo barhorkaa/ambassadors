8fe35f52b740fe108ef48bc1ddb7c8fbb1b4678b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 21:25:34 +0200

    fix(ui/motivationForm): changed placeholders

--- app/ui/motivation/motivation-form.tsx
+++ app/ui/motivation/motivation-form.tsx
@@ -5,15 +5,24 @@ import FormControl from '@/app/ui/utils/form-control';
 export default function MotivationForm({ userId }: { userId: string }) {
   return (
     <form action={createMotivationAction} className="card-body">
-      <FormControl title={'Proč jste se rozhodli k nám přidat?'} id={'why'} inputType={'area'} placeholder={'Proč'} />
+      <FormControl
+        title={'Proč jste se rozhodli k nám přidat?'}
+        id={'why'}
+        inputType={'area'}
+        placeholder={'Chci zvýšit povědomí o fakultě, přividělat si ...'}
+      />
       <FormControl
         title={'Doporučil vám někto přidat se k programu? Pokud ano, kto?'}
         id={'who'}
         inputType={'area'}
         placeholder={'Ne / Ano + kto'}
       />
-      <FormControl title={'Co byste chtěli jako ambasador dosáhnout?'} id={'goals'} placeholder={'Vaše cíle'} />
+      <FormControl
+        title={'Co byste chtěli jako ambasador dosáhnout?'}
+        id={'goals'}
         inputType={'area'}
+        placeholder={'Chci se podílet na propagaci, dostat merch...'}
+      />
       <FormControl
         title={'Jakých akcí byste se chtěli převážně zúčastňovat?'}
         id={'preferredEvents'}
@@ -24,7 +33,7 @@ export default function MotivationForm({ userId }: { userId: string }) {
         title={'Kolik času budete mít na ambasadorskou činnost?'}
         id={'time'}
         inputType={'area'}
-        placeholder={'Vaše časové možnosti'}
+        placeholder={'Na kolik akcí zhruba za rok byste chtěli jít?'}
       />
       <input id="id" type="hidden" name="userId" value={userId} required />
       <SubmitButton title={'Odeslat'} />
