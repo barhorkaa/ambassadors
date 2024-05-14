971de607977bd1821effe5707f3cee91ec268c8f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 20:31:27 +0200

    feat(app/loading): added a loading component for app

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+  return (
+    <div className="hero h-full">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="hero-title">Načitávam...</h1>
+          <p className="pb-6">Prosím čekejte</p>
+          <span className="loading loading-spinner loading-xl"></span>
+        </div>
+      </div>
+    </div>
+  );
+}
