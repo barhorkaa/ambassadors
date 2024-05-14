19bc496ddc581426747fa5768f95015f0b6cc46e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 21:01:51 +0200

    feat(events/page): removed from page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,13 +1,7 @@
-import { EventList } from '@/app/ui/utils/event-list';
-import { getAllActiveEvents } from '@/database/repository/events';
-import { EventModel } from '@/models/event-models';
 import { PlusIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
-export default async function Events() {
-  const allEvents: EventModel[] = await getAllActiveEvents(true);
-  const allUnapprovedEvents: EventModel[] = await getAllActiveEvents(false);
-
+export default function Events() {
   return (
     <>
       <div className="flex flex-row justify-between">
@@ -18,12 +12,6 @@ export default async function Events() {
         </Link>
       </div>
       <hr className="w-full" />
-      <EventList title={'Nepotvrzené akce'} list={allUnapprovedEvents} emptyMessage={'Všechny akce jsou potvrzeny'} />
-      <EventList
-        title={'Všechny akce'}
-        list={allEvents}
-        emptyMessage={'Momentálně nejsou k dispozici žádné akce. Jestli chcete někam jet, vtvořte návrh na novou akci.'}
-      />
     </>
   );
 }
