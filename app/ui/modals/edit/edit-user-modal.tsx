2e265c95ed2ddb140075ad2c11cc80d3cfcff846
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 21:15:22 +0200

    fix(modals/editUser): destructured prop

--- app/ui/modals/edit/edit-user-modal.tsx
+++ app/ui/modals/edit/edit-user-modal.tsx
@@ -4,11 +4,11 @@ import ModalLayout from '@/app/ui/modals/modal-layout';
 import UserEditForm from '@/app/ui/user/user-edit-form';
 import { UserModel } from '@/models/user-models';
 
-export default function EditUserModal(data: { user: UserModel; full: boolean }) {
+export default function EditUserModal({ user, full }: { user: UserModel; full: boolean }) {
   return (
-    <ModalLayout id={data.user.id} title={'Upravit informace o uživateli'} modalType={'edit'}>
+    <ModalLayout id={user.id} title={'Upravit informace o uživateli'} modalType={'edit'}>
       <p>Pokud si chcete změnit informace, které nevidíte ve formuláři, kontaktujte prosím propagace@fi.muni.cz</p>
-      <UserEditForm user={data.user} full={data.full} />
+      <UserEditForm user={user} full={full} />
     </ModalLayout>
   );
 }
