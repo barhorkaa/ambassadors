6d52d7c5b3a21e1b4e8c87697ef53e13d4d7b55a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 21:02:09 +0200

    feat(events/layout): added slots to layout

--- app/events/layout.tsx
+++ app/events/layout.tsx
@@ -1,9 +1,25 @@
+import TableSkeleton from '@/app/ui/utils/skeletons';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Akce | AmbassadorsFIMU',
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+export default function Layout({
+  children,
+  all,
+  unapproved,
+}: {
+  children: React.ReactNode;
+  all: React.ReactNode;
+  unapproved: React.ReactNode;
+}) {
+  return (
+    <section>
+      {children}
+      <Suspense fallback={<TableSkeleton />}>{unapproved}</Suspense>
+      <Suspense fallback={<TableSkeleton />}>{all}</Suspense>
+    </section>
+  );
 }
