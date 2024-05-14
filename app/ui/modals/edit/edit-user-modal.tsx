56b5ab723ace216434ee78cb592d9c01fbdbe0a2
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 21:16:23 +0200

    fix(modals/editUser): message now showing only to ambassadors

--- app/ui/modals/edit/edit-user-modal.tsx
+++ app/ui/modals/edit/edit-user-modal.tsx
@@ -7,7 +7,9 @@ import { UserModel } from '@/models/user-models';
 export default function EditUserModal({ user, full }: { user: UserModel; full: boolean }) {
   return (
     <ModalLayout id={user.id} title={'Upravit informace o uživateli'} modalType={'edit'}>
-      <p>Pokud si chcete změnit informace, které nevidíte ve formuláři, kontaktujte prosím propagace@fi.muni.cz</p>
+      {!full && (
+        <p>Pokud si chcete změnit informace, které nevidíte ve formuláři, kontaktujte prosím propagace@fi.muni.cz</p>
+      )}
       <UserEditForm user={user} full={full} />
     </ModalLayout>
   );
