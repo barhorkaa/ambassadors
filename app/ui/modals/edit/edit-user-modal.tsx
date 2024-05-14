7192719b1b4c5f6235f342dbef24f0ec836a2f50
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-14 21:14:30 +0200

    fix(modals/editUser): added info abou changing other info

--- app/ui/modals/edit/edit-user-modal.tsx
+++ app/ui/modals/edit/edit-user-modal.tsx
@@ -7,6 +7,7 @@ import { UserModel } from '@/models/user-models';
 export default function EditUserModal(data: { user: UserModel; full: boolean }) {
   return (
     <ModalLayout id={data.user.id} title={'Upravit informace o uživateli'} modalType={'edit'}>
+      <p>Pokud si chcete změnit informace, které nevidíte ve formuláři, kontaktujte prosím propagace@fi.muni.cz</p>
       <UserEditForm user={data.user} full={data.full} />
     </ModalLayout>
   );
