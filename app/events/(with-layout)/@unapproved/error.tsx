59462125e2cafafd18262bee89360e82259637aa
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-15 14:39:49 +0200

    fix(events/error): changed error styling

--- app/events/(with-layout)/@unapproved/error.tsx
+++ app/events/(with-layout)/@unapproved/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   return (
     <div className="page flex flex-col gap-6">
-      <h2>Nepodařilo se načíst akce</h2>
+      <h2>Nepodařilo se načíst nepotvrzené akce</h2>
       <button className="btn" onClick={() => reset()}>
         Zkusit znovu
       </button>
