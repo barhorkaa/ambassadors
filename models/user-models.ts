b782ac664cfa9e19f631692049c2814cf53f8b90
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-15 13:03:22 +0200

    fix(models/user): added error message to password

--- models/user-models.ts
+++ models/user-models.ts
@@ -14,7 +14,7 @@ export type UserEditModel = z.infer<typeof userEditSchema>;
 export const userCreateSchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  password: z.string().min(6),
+  password: z.string().min(6, { message: 'Heslo musí obsahovat alespoň 6 znaků' }),
   uco: z.string().min(6).max(6).pipe(z.coerce.number()),
   phone_number: z.string().min(9).max(16),
 });
