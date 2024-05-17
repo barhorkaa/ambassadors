83f0eb8d44ce1d89530499b06fd3f538c7776e95
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-17 16:24:49 +0200

    feat(ui/registerForm): added error handling

--- app/ui/auth/register-form.tsx
+++ app/ui/auth/register-form.tsx
@@ -5,26 +5,49 @@ import SubmitButton from '@/app/ui/button/submit-button';
 import FormControl from '@/app/ui/utils/form-control';
 import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import { useFormState } from 'react-dom';
+import { ZodIssue } from 'zod';
 
 export default function RegisterForm() {
-  const [errorMessage, dispatch] = useFormState(createUserAction, undefined);
+  const [state, dispatch] = useFormState(createUserAction, { errors: [], generic: undefined });
 
   return (
     <form action={dispatch} className="card-body">
-      <FormControl title={'Celé jméno'} id={'name'} placeholder={'Jméno'} />
-      <FormControl title={'E-mail'} id={'email'} />
-      <FormControl title={'Heslo'} id={'password'} type={'password'} />
-      <FormControl title={'UČO'} id={'uco'} />
-      <FormControl title={'Telefonní číslo'} id={'phoneNumber'} />
+      <FormControl
+        title={'Celé jméno'}
+        id={'name'}
+        placeholder={'Jméno'}
+        errorMessage={findErrors('name', state.errors)[0]}
+      />
+      <FormControl title={'E-mail'} id={'email'} errorMessage={findErrors('email', state.errors)[0]} />
+      <FormControl
+        title={'Heslo'}
+        id={'password'}
+        type={'password'}
+        errorMessage={findErrors('password', state.errors)[0]}
+      />
+      <FormControl title={'UČO'} id={'uco'} errorMessage={findErrors('uco', state.errors)[0]} />
+      <FormControl
+        title={'Telefonní číslo'}
+        id={'phoneNumber'}
+        errorMessage={findErrors('phone_number', state.errors)[0]}
+      />
       <SubmitButton title={'Registrovat se'} />
       <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
-        {errorMessage && (
+        {state.generic && (
           <>
             <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-            <p className="text-sm text-red-500">{errorMessage}</p>
+            <p className="text-sm text-red-500">{state.generic}</p>
           </>
         )}
       </div>
     </form>
   );
 }
+
+const findErrors = (fieldName: string, errors: ZodIssue[]) => {
+  return errors
+    .filter((item) => {
+      return item.path.includes(fieldName);
+    })
+    .map((item) => item.message);
+};
