e5de15ee7812be63fb19f73f04e978ddced7678f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-17 11:23:47 +0200

    fix(ui/navBarUser): removed vitej

--- app/ui/layout/nav-bar-user.tsx
+++ app/ui/layout/nav-bar-user.tsx
@@ -24,7 +24,7 @@ export default async function NavBarUser() {
         {/*  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>*/}
         {/*</button>*/}
         <Link className="hidden md:flex flex-row gap-4 text-black" href={'/me'}>
-          Vítej: <h3>{session?.user.name}</h3>
+          <h3>{session?.user.name}</h3>
         </Link>
       </div>
     </div>
