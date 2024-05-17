97c092c066f5212c2669d52425fe1b43fdbbf915
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-17 16:43:00 +0200

    fix(register/success/page): fixed typo

--- app/register/(without-layout)/success/page.tsx
+++ app/register/(without-layout)/success/page.tsx
@@ -8,7 +8,7 @@ export default function Success() {
           <h1 className="hero-title">Vaše registrace se zdařila!</h1>
           <p className="py-6">
             Nyní se přihlaste a v aplikaci vyplňte motivační dotazník. Poté vaši registraci zchválí někdo z našich
-            manažerů a se budete moct začín plně věnovat svým ambasadorským povinnostem.
+            manažerů a budete se moct začín plně věnovat svým ambasadorským povinnostem.
           </p>
           <Link href={'/login'}>
             <button className="btn">Přihlásit se</button>
