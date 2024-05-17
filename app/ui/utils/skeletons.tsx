5505892e010feb748cd49d33cca5c34e9a717158
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-17 16:37:40 +0200

    feat(ui/skeletons): added a card skeleton

--- app/ui/utils/skeletons.tsx
+++ app/ui/utils/skeletons.tsx
@@ -1,3 +1,7 @@
 export default function TableSkeleton() {
   return <div className="skeleton w-full h-64 my-6 rounded-none"></div>;
 }
+
+export function CardSkeleton() {
+  return <div className="skeleton w-3/5 h-96 my-6"></div>;
+}
