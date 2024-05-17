d8a28692424138e069cdd372f36c1bd9ca885a79
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-17 13:27:02 +0200

    fix(events/page): removed prefetch

--- app/events/(with-layout)/page.tsx
+++ app/events/(with-layout)/page.tsx
@@ -8,7 +8,7 @@ export default function Events() {
     <>
       <div className="flex flex-row justify-between items-end content">
         <h1>Akce</h1>
-        <Link className="btn" href={'/events/new'}>
+        <Link className="btn" href={'/events/new'} prefetch={false}>
           <PlusIcon className="h-5" />
           <p className="hidden md:block">Přidat akci</p>
         </Link>
