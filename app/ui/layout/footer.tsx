c50c4cd0589f03e0fdf0f6b87da29a2f96d18585
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-17 11:18:47 +0200

    fix(project): fixed dark mode of the application

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -1,6 +1,6 @@
 export async function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-fi-100 text-base-content">
+    <footer className="footer footer-center p-4 bg-fi-100 text-black">
       <aside>
         <p>Copyright © 2024 - All right reserved by OVVSP FI MU</p>
       </aside>
