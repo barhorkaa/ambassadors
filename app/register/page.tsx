3af9e1bedd48e16b90188daea150070eb77b1341
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-17 16:38:02 +0200

    feat(register/page): moved content elsewhere

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,20 +1,3 @@
-import RegisterForm from '@/app/ui/auth/register-form';
-
 export default function Register() {
-  return (
-    <div className="hero">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left p-4">
-          <h1>Přidejte se k nám a dělejte FI dobré jméno!</h1>
-          <p className="py-6">
-            Po přihlášení budete míť přístup ke všem akcím a možnost se na přihlásit. Pojďte s námi představit Fakultu
-            informatiky Masarykovy Univerzity světu!
-          </p>
-        </div>
-        <div className="card">
-          <RegisterForm />
-        </div>
-      </div>
-    </div>
-  );
+  return null;
 }
