5f75872212d20138f494f91e5d42a9886b755a0f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 22:59:00 +0200

    fix(events/page/types/all): added content

---
+++ app/events/(without-layout)/types/all/page.tsx
@@ -0,0 +1,15 @@
+import EventTypeDetail from '@/app/ui/event-type/event-type-detail';
+import { getAllEventTypes } from '@/database/repository/event-type';
+import { EventTypeDetailModel } from '@/models/event-type-models';
+
+export default async function Page() {
+  const allEventTypes: EventTypeDetailModel[] = await getAllEventTypes();
+
+  return (
+    <div className="flex flex-col gap-6 content">
+      {allEventTypes.map((eventType) => (
+        <EventTypeDetail key={eventType.id} eventType={eventType} />
+      ))}
+    </div>
+  );
+}
