bfca5593c0edae22fe62836e375ca7da307e6cbe
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 21:50:45 +0200

    fix(ui/userCard): fixed link

--- app/ui/user/user-card.tsx
+++ app/ui/user/user-card.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function UserCard({ user }: { user: UserModel }) {
   return (
-    <Link href={`ambassadors/${user.id}`} className="card shadow-lg">
+    <Link href={`/ambassadors/${user.id}`} className="card shadow-lg">
       <div className="card-body">
         <h2 className="card-title">{user.name}</h2>
         <hr className="w-full my-0" />
