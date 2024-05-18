cad8ded535b5e4a39ef26f3f5de1aa8541c49e11
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 21:30:08 +0200

    fix(register/page): added redirect to page

--- app/register/(with-layout)/page.tsx
+++ app/register/(with-layout)/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+
+export default async function Page() {
+  const session = await auth();
+  if (session) {
+    redirect('/events/all');
+  }
+
   return null;
 }
