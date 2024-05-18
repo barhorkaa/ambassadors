bded10adf8f75b63cbd2bed2a64d90da04953055
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 17:20:09 +0200

    fix(ui/userDetail): moved detail row to utils

--- app/ui/user/user-detail.tsx
+++ app/ui/user/user-detail.tsx
@@ -1,3 +1,4 @@
+import { DetailRowHorizontal } from '@/app/ui/utils/detail-row';
 import { UserRoles } from '@/app/utils/user-roles';
 import { UserModel } from '@/models/user-models';
 
@@ -12,16 +13,6 @@ export default function UserDetail({ user }: { user: UserModel }) {
       <DetailRowHorizontal label={'Potvrzen'} value={user.approved ? 'Ano' : 'Ne'} />
       <DetailRowHorizontal label={'Registrovaný'} value={user.created_at.toLocaleString('cs-CZ')} />
       <DetailRowHorizontal label={'Aktivní'} value={user.deleted_at ? 'Ne' : 'Ano'} />
-    </div>
-  );
-}
-
-function DetailRowHorizontal(detail: { label: string; value: string | number }) {
-  return (
-    <div className="grid grid-cols-3 items-end">
-      <p className="font-light">{detail.label}</p>
-      <p className="text-lg line-clamp-2 col-span-2">{detail.value}</p>
-      <hr className="w-full h-0.5 text-black col-span-3 m-1 bg-base-300" />
     </div>
   );
 }
--- app/ui/utils/detail-row.tsx
+++ app/ui/utils/detail-row.tsx
@@ -9,3 +9,13 @@ export default function DetailRow(detail: { label: string; value: string | numbe
     </div>
   );
 }
+
+export function DetailRowHorizontal(detail: { label: string; value: string | number }) {
+  return (
+    <div className="grid grid-cols-3 items-end">
+      <p className="font-light">{detail.label}</p>
+      <p className="text-lg line-clamp-2 col-span-2">{detail.value}</p>
+      <hr className="w-full h-0.5 text-black col-span-3 m-1 bg-base-300" />
+    </div>
+  );
+}
