ff1ffbce8cabcd6b6d5d533e77a8e5d77f113691
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 21:34:59 +0200

    fix(ui/eventTypeDetail): restyled

--- app/ui/event-type/event-type-detail.tsx
+++ app/ui/event-type/event-type-detail.tsx
@@ -13,30 +13,32 @@ export default async function EventTypeDetail({ eventType }: { eventType: EventT
   }
 
   return (
-    <div className="data-display card-body">
-      <div className="flex flex-row justify-between">
-        <h2>{eventType.name}</h2>
-        {session?.user.role === UserRoles.manager && (
-          <div className="flex gap-4">
-            <DeleteEventTypeModal eventTypeId={eventType.id} />
-            <EditEventTypeModal eventType={eventType} />
-          </div>
-        )}
-      </div>
-      <div>
-        <DetailRowVertical label={''} value={eventType.description} />
-        <DetailRowVertical label={'Instrukce pro ambasadory'} value={eventType.instructions} />
-      </div>
-      {session?.user.role == UserRoles.manager && (
-        <div>
-          <DetailRowVertical label={'Vytvořeno'} value={eventType.createdAt.toLocaleString('cs-CZ')} />
-          <DetailRowVertical label={'Upraveno'} value={eventType.updatedAt.toLocaleString('cs-CZ')} />
-          <DetailRowVertical
-            label={'Zmazáno'}
-            value={eventType.deletedAt == null ? 'Ne' : eventType.deletedAt.toLocaleString('cs-CZ')}
-          />
+    <div className="data-display">
+      <div className="card-body">
+        <div className="flex flex-row justify-between">
+          <h2>{eventType.name}</h2>
+          {session?.user.role === UserRoles.manager && (
+            <div className="flex gap-4">
+              <DeleteEventTypeModal eventTypeId={eventType.id} />
+              <EditEventTypeModal eventType={eventType} />
+            </div>
+          )}
         </div>
-      )}
+        <div>
+          <DetailRowVertical label={''} value={eventType.description} />
+          <DetailRowVertical label={'Instrukce pro ambasadory'} value={eventType.instructions} />
+        </div>
+        {session?.user.role == UserRoles.manager && (
+          <div>
+            <DetailRowVertical label={'Vytvořeno'} value={eventType.createdAt.toLocaleString('cs-CZ')} />
+            <DetailRowVertical label={'Upraveno'} value={eventType.updatedAt.toLocaleString('cs-CZ')} />
+            <DetailRowVertical
+              label={'Zmazáno'}
+              value={eventType.deletedAt == null ? 'Ne' : eventType.deletedAt.toLocaleString('cs-CZ')}
+            />
+          </div>
+        )}{' '}
+      </div>
     </div>
   );
 }
