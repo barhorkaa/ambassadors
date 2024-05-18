8119d1697a316c617ecdd5204c24e36b3e76c926
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 15:37:31 +0200

    fix(ui/userList): destructuring props

--- app/ui/utils/user-list.tsx
+++ app/ui/utils/user-list.tsx
@@ -2,16 +2,16 @@ import UserCard from '@/app/ui/user/user-card';
 import UserTable from '@/app/ui/user/user-table';
 import { UserModel } from '@/models/user-models';
 
-export function UserList(props: { title: string; list: UserModel[] }) {
+export function UserList({ title, list }: { title: string; list: UserModel[] }) {
   return (
     <>
-      <h2>{props.title}</h2>
       <hr className="w-full" />
+      <h2>{title}</h2>
       <div className="hidden md:block">
-        <UserTable users={props.list} />
+        <UserTable users={list} />
       </div>
       <div className="flex md:hidden flex-wrap justify-center gap-4">
-        {props.list.map((user) => (
+        {list.map((user) => (
           <UserCard key={user.id} user={user} />
         ))}
       </div>
