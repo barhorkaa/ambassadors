a15633a9c3987fde64e7fa7d525cb6f8a0858157
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 22:24:36 +0200

    fix(materials/page): removed content

--- app/materials/page.tsx
+++ app/materials/page.tsx
@@ -1,28 +1,9 @@
-import MaterialDetail from '@/app/ui/material/material-detail';
-import CreateMaterialModal from '@/app/ui/modals/create/create-material-modal';
-import { UserRoles } from '@/app/utils/user-roles';
-import { auth } from '@/auth';
-import { getAllMaterials } from '@/database/repository/material';
-import { MaterialDetailModel } from '@/models/material-models';
+import { SectionInfo } from '@/app/ui/utils/data-display';
 
-export default async function Page() {
-  const session = await auth();
-  const isManager = session?.user.role === UserRoles.manager;
-
-  const allMaterials: MaterialDetailModel[] = await getAllMaterials();
-
+export default function Page() {
   return (
     <>
-      <div className="flex flex-row justify-between content items-end">
-        <h1>Materiály</h1>
-        {isManager && <CreateMaterialModal />}
-      </div>
-      <hr className="w-full" />
-      <div className="flex flex-col gap-6 content">
-        {allMaterials.map((material) => (
-          <MaterialDetail key={material.id} material={material} />
-        ))}
-      </div>
+      <SectionInfo title={'Co jsou materiály?'} contents={[]} />
     </>
   );
 }
