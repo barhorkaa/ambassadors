d26fdbc30fc73537041aeb3120e3084faa051f88
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 21:59:15 +0200

    fix(ui/motivationDetail): deconstructed prop

--- app/ui/motivation/motivation-detail.tsx
+++ app/ui/motivation/motivation-detail.tsx
@@ -1,33 +1,30 @@
 import DetailRowVertical from '@/app/ui/utils/data-display';
 import { MotivationModel } from '@/models/motivation-models';
 
-export default function MotivationDetail(params: { motivation: MotivationModel | undefined }) {
-  if (params.motivation === undefined) {
+export default function MotivationDetail({ motivation }: { motivation: MotivationModel | undefined }) {
+  if (motivation === undefined) {
     return <p className="text-lg">Užiatel zatím nevyplnil motivační formulář</p>;
   }
 
   return (
     <div className="flex flex-col">
       <div className="pb-4">
-        <DetailRowVertical label={'Proč jste se rozhodli k nám přidat?'} value={params.motivation.why} />
+        <DetailRowVertical label={'Proč jste se rozhodli k nám přidat?'} value={motivation.why} />
       </div>
       <div className="pb-4">
-        <DetailRowVertical
-          label={'Doporučil vám někto přidat se k programu? Pokud ano, kto?'}
-          value={params.motivation.who}
-        />
+        <DetailRowVertical label={'Doporučil vám někto přidat se k programu? Pokud ano, kto?'} value={motivation.who} />
       </div>
       <div className="pb-4">
-        <DetailRowVertical label={'Co byste chtěli jako ambasador dosáhnout?'} value={params.motivation.goals} />
+        <DetailRowVertical label={'Co byste chtěli jako ambasador dosáhnout?'} value={motivation.goals} />
       </div>
       <div className="pb-4">
         <DetailRowVertical
           label={'Jakých akcí byste se chtěli převážně zúčastňovat?'}
-          value={params.motivation.preferredEvents}
+          value={motivation.preferredEvents}
         />
       </div>
       <div className="pb-4">
-        <DetailRowVertical label={'Kolik času budete mít na ambasadorskou činnost?'} value={params.motivation.time} />
+        <DetailRowVertical label={'Kolik času budete mít na ambasadorskou činnost?'} value={motivation.time} />
       </div>
     </div>
   );
