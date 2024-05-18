05a6b227417b6b8f090111006c97f27edb71d154
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 22:29:04 +0200

    fix(materials/page/deleted): added getting deleted materials

--- app/materials/deleted/page.tsx
+++ app/materials/deleted/page.tsx
@@ -1,7 +1,15 @@
+import MaterialDetail from '@/app/ui/material/material-detail';
+import { getAllMaterials } from '@/database/repository/material';
+import { MaterialDetailModel } from '@/models/material-models';
+
 export default async function Page() {
+  const deletedMaterials: MaterialDetailModel[] = await getAllMaterials(true);
+
   return (
-    <div>
-      <h2>hello</h2>
+    <div className="flex flex-col gap-6">
+      {deletedMaterials.map((material) => (
+        <MaterialDetail key={material.id} material={material} />
+      ))}
     </div>
   );
 }
