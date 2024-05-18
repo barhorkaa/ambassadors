7ff06aeefd770b118664f08d3c9e1aef2be1a961
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 18:54:22 +0200

    refactor(me): renamed page components to Page

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from '@/auth';
 import { permanentRedirect } from 'next/navigation';
 
-export default async function Me() {
+export default async function Page() {
   const session = await auth();
 
   permanentRedirect(`/ambassadors/${session?.user.id!}`);
