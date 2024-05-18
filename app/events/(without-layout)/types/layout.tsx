c500e8d05e45b960f5dc8a5b07772f0d3a0dce51
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 22:54:11 +0200

    fix(events/type): moved title to layout

--- app/events/(without-layout)/types/layout.tsx
+++ app/events/(without-layout)/types/layout.tsx
@@ -1,9 +1,24 @@
+import CreateEventTypeModal from '@/app/ui/modals/create/create-event-type-modal';
+import { UserRoles } from '@/app/utils/user-roles';
+import { auth } from '@/auth';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Druhy akcí | AmbassadorsFIMU',
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+
+  return (
+    <section>
+      <div className="flex flex-row justify-between content items-end">
+        <h1>Druhy akcí</h1>
+        {session?.user.role === UserRoles.manager && <CreateEventTypeModal />}
+      </div>
+      <hr className="w-full mb-0" />
+      <ul className="page-menu"></ul>
+      {children}
+    </section>
+  );
 }
--- app/events/(without-layout)/types/page.tsx
+++ app/events/(without-layout)/types/page.tsx
@@ -1,21 +1,12 @@
 import EventTypeDetail from '@/app/ui/event-type/event-type-detail';
-import CreateEventTypeModal from '@/app/ui/modals/create/create-event-type-modal';
-import { UserRoles } from '@/app/utils/user-roles';
-import { auth } from '@/auth';
 import { getAllEventTypes } from '@/database/repository/event-type';
 import { EventTypeDetailModel } from '@/models/event-type-models';
 
 export default async function Page() {
   const allEventTypes: EventTypeDetailModel[] = await getAllEventTypes();
-  const session = await auth();
 
   return (
     <>
-      <div className="flex flex-row justify-between content items-end">
-        <h1>Druhy akcí</h1>
-        {session?.user.role === UserRoles.manager && <CreateEventTypeModal />}
-      </div>
-      <hr className="w-full" />
       <div className="flex flex-col gap-6 content">
         {allEventTypes.map((eventType) => (
           <EventTypeDetail key={eventType.id} eventType={eventType} />
