29cc72e5d91c6ef3b5e967bb3ee091529ab7587d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 19:37:25 +0200

    fix(signups/layout): restyled layout

--- app/signups/layout.tsx
+++ app/signups/layout.tsx
@@ -1,9 +1,27 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Přihlášení na akce | AmbassadorsFIMU',
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+  return (
+    <section>
+      <h1 className="content">Přihlášení na akce</h1>
+      <hr className="w-full mb-0" />
+      <ul className="page-menu">
+        <li>
+          <Link href={'/signups'}>Informace</Link>
+        </li>
+        <li>
+          <Link href={'/signups/all'}>Všechna aktuání přihlášení</Link>
+        </li>
+        <li>
+          <Link href={'/signups/unapproved'}>Nepotvrzené přihlášení</Link>
+        </li>
+      </ul>
+      <div className="content">{children}</div>
+    </section>
+  );
 }
