cd943e33bee7d85809be4abc7fe9e84d75be540d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 23:12:30 +0200

    feat(modals/reviveEventType): created modal to revive event type

---
+++ app/ui/modals/revive/revive-event-type-modal.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { reviveEventTypeAction } from '@/app/lib/actions/event-type';
+import { ActionModalButton } from '@/app/ui/button/action-modal-button';
+import ModalLayout from '@/app/ui/modals/modal-layout';
+
+export default function ReviveEventTypeModal({ eventTypeId }: { eventTypeId: string }) {
+  return (
+    <ModalLayout id={'revive' + eventTypeId} title={'Opravdu chcete obnovit tento materiál?'} modalType={'revive'}>
+      <div className="pt-4 flex flex-col gap-4">
+        <p>
+          Obnovením tohoto druhu akce bude moct být opět používan při vytváření nových akcí a úpravě již existujících.
+        </p>
+        <p>Bude taktéž viditelný pro všechny uživatele aplikace.</p>
+        <div className="flex justify-end">
+          <ActionModalButton fun={reviveEventTypeAction} id={eventTypeId} modalId={'revive' + eventTypeId} />
+        </div>
+      </div>
+    </ModalLayout>
+  );
+}
