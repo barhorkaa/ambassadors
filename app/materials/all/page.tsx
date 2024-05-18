9f3e8ce5ad47fb242e539b6c4445c557212bc96e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 22:22:52 +0200

    feat(materials/pages): added routes

---
+++ app/materials/all/page.tsx
@@ -0,0 +1,15 @@
+import MaterialDetail from '@/app/ui/material/material-detail';
+import { getAllMaterials } from '@/database/repository/material';
+import { MaterialDetailModel } from '@/models/material-models';
+
+export default async function Page() {
+  const allMaterials: MaterialDetailModel[] = await getAllMaterials();
+
+  return (
+    <div className="flex flex-col gap-6">
+      {allMaterials.map((material) => (
+        <MaterialDetail key={material.id} material={material} />
+      ))}
+    </div>
+  );
+}
