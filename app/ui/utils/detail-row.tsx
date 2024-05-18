91af6414d5f9b308923889b1a1a777d88d4387cb
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 17:21:54 +0200

    fix(ui/detailRow): deconstructed props

--- app/ui/utils/detail-row.tsx
+++ app/ui/utils/detail-row.tsx
@@ -1,20 +1,20 @@
-export default function DetailRowVertical(detail: { label: string; value: string | number }) {
+export default function DetailRowVertical({ label, value }: { label: string; value: string | number }) {
   return (
     <div>
       <div className="grid grid-rows-[min-content_min-content] items-end">
-        <p className="font-light text-sm">{detail.label}</p>
-        <p className="text-xl">{detail.value}</p>
+        <p className="font-light text-sm">{label}</p>
+        <p className="text-xl">{value}</p>
       </div>
       <hr className="w-full h-0.5 text-black my-1 bg-base-300" />
     </div>
   );
 }
 
-export function DetailRowHorizontal(detail: { label: string; value: string | number }) {
+export function DetailRowHorizontal({ label, value }: { label: string; value: string | number }) {
   return (
     <div className="grid grid-cols-3 items-end">
-      <p className="font-light">{detail.label}</p>
-      <p className="text-lg line-clamp-2 col-span-2">{detail.value}</p>
+      <p className="font-light">{label}</p>
+      <p className="text-lg line-clamp-2 col-span-2">{value}</p>
       <hr className="w-full h-0.5 text-black col-span-3 m-1 bg-base-300" />
     </div>
   );
