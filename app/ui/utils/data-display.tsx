ba28b76a5bc8044ee0d645a90e6449557573814b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 21:40:35 +0200

    fix(ui/dataDisplay): removed class

--- app/ui/utils/data-display.tsx
+++ app/ui/utils/data-display.tsx
@@ -5,7 +5,7 @@ export default function DetailRowVertical({ label, value }: { label: string; val
         <p className="font-light text-sm">{label}</p>
         <p className="text-lg">{value}</p>
       </div>
-      <hr className="w-full h-0.5 text-black my-1 bg-base-300" />
+      <hr className="w-full h-0.5 my-1 bg-base-300" />
     </div>
   );
 }
@@ -15,7 +15,7 @@ export function DetailRowHorizontal({ label, value }: { label: string; value: st
     <div className="grid grid-cols-3 items-end">
       <p className="font-light">{label}</p>
       <p className="text-lg line-clamp-2 col-span-2">{value}</p>
-      <hr className="w-full h-0.5 text-black col-span-3 m-1 bg-base-300" />
+      <hr className="w-full h-0.5 col-span-3 m-1 bg-base-300" />
     </div>
   );
 }
