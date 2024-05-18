41cdbeeb4514f6d32666a06bc80db2a936b496a0
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 22:23:14 +0200

    fix(materials/layout): included navigation

--- app/materials/layout.tsx
+++ app/materials/layout.tsx
@@ -1,9 +1,37 @@
+import CreateMaterialModal from '@/app/ui/modals/create/create-material-modal';
+import { UserRoles } from '@/app/utils/user-roles';
+import { auth } from '@/auth';
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Materiály | AmbassadorsFIMU',
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+
+  return (
+    <section>
+      <div className="flex flex-row justify-between content items-end">
+        <h1>Materiály</h1>
+        {session?.user.role === UserRoles.manager && <CreateMaterialModal />}
+      </div>
+      <hr className="w-full mb-0" />
+      <ul className="page-menu">
+        <li>
+          <Link href={'/materials'}>Informace</Link>
+        </li>
+        <li>
+          <Link href={'/materials/all'}>Dostupné materiály</Link>
+        </li>
+        {session?.user.role === UserRoles.manager && (
+          <li>
+            <Link href={'/materials/deleted'}>Vymazané materiály</Link>
+          </li>
+        )}
+      </ul>
+      <div className="content">{children}</div>
+    </section>
+  );
 }
