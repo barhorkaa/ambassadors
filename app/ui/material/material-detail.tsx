5b5ec9c47faab7f7e4b6aa429e947501385869ab
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 22:11:37 +0200

    fix(ui/materialDetail): restyled

--- app/ui/material/material-detail.tsx
+++ app/ui/material/material-detail.tsx
@@ -9,29 +9,31 @@ export default async function MaterialDetail({ material }: { material: MaterialD
   const session = await auth();
 
   return (
-    <div className="data-display card-body">
-      <div className="flex flex-row justify-between">
-        <h2>{material.name}</h2>
-        {session?.user.role === UserRoles.manager && (
-          <div className="flex gap-4">
-            <DeleteMaterialModal materialId={material.id} />
-            <EditMaterialModal material={material} />
+    <div className="data-display">
+      <div className="card-body">
+        <div className="flex flex-row justify-between">
+          <h2>{material.name}</h2>
+          {session?.user.role === UserRoles.manager && (
+            <div className="flex gap-4">
+              <DeleteMaterialModal materialId={material.id} />
+              <EditMaterialModal material={material} />
+            </div>
+          )}
+        </div>
+        <div>
+          <DetailRowVertical label={''} value={material.description!} />
+        </div>
+        {session?.user.role == UserRoles.manager && (
+          <div>
+            <DetailRowVertical label={'Vytvořeno'} value={material.createdAt.toLocaleString('cs-CZ')} />
+            <DetailRowVertical label={'Upraveno'} value={material.updatedAt.toLocaleString('cs-CZ')} />
+            <DetailRowVertical
+              label={'Zmazáno'}
+              value={material.deletedAt == null ? 'Ne' : material.deletedAt.toLocaleString('cs-CZ')}
+            />
           </div>
         )}
       </div>
-      <div>
-        <DetailRowVertical label={''} value={material.description!} />
-      </div>
-      {session?.user.role == UserRoles.manager && (
-        <div>
-          <DetailRowVertical label={'Vytvořeno'} value={material.createdAt.toLocaleString('cs-CZ')} />
-          <DetailRowVertical label={'Upraveno'} value={material.updatedAt.toLocaleString('cs-CZ')} />
-          <DetailRowVertical
-            label={'Zmazáno'}
-            value={material.deletedAt == null ? 'Ne' : material.deletedAt.toLocaleString('cs-CZ')}
-          />
-        </div>
-      )}
     </div>
   );
 }
