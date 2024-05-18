a3668f1e5fc166719e8481c5c00d322ba07e23ec
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 15:15:52 +0200

    fix(login/page): removed content

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,3 @@
-import LoginForm from '@/app/ui/auth/login-form';
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 
@@ -8,20 +7,5 @@ export default async function Login() {
     redirect('/events');
   }
 
-  return (
-    <div className="hero">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left p-4">
-          <h1 className="hero-title">Přihlašte se a naplánujte svou akci!</h1>
-          <p className="py-6 sm:py-2">
-            Po přihlášení budete míť přístup ke všem akcím a možnost se na přihlásit. Pojďte s námi představit Fakultu
-            informatiky Masarykovy Univerzity světu!
-          </p>
-        </div>
-        <div className="card">
-          <LoginForm />
-        </div>
-      </div>
-    </div>
-  );
+  return null;
 }
