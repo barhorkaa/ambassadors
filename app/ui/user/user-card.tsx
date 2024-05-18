ad533c48b727db43b00e5fdc16c8f41d8e8e6277
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 21:50:17 +0200

    fix(ui/userCard): restyled card

--- app/ui/user/user-card.tsx
+++ app/ui/user/user-card.tsx
@@ -3,18 +3,13 @@ import Link from 'next/link';
 
 export default function UserCard({ user }: { user: UserModel }) {
   return (
-    <div className="card shadow-lg w-5/6">
-      <Link href={`ambassadors/${user.id}`}>
-        <div className="card-body">
-          <h2 className="card-title">{user.name}</h2>
-          <div className="flex flex-row justify-between">
-            <div>
-              <p>E-mail: {user.email}</p>
-              <p>Tel. číslo: {user.phone_number}</p>
-            </div>
-          </div>
-        </div>
-      </Link>
-    </div>
+    <Link href={`ambassadors/${user.id}`} className="card shadow-lg">
+      <div className="card-body">
+        <h2 className="card-title">{user.name}</h2>
+        <hr className="w-full my-0" />
+        <p>E-mail: {user.email}</p>
+        <p>Tel. číslo: {user.phone_number}</p>
+      </div>
+    </Link>
   );
 }
