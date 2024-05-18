6164f46d328bb37bba94681a35320a19029529bc
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 17:23:43 +0200

    fix(ui/userDetail): made some info manager specific

--- app/ui/user/user-detail.tsx
+++ app/ui/user/user-detail.tsx
@@ -1,8 +1,11 @@
 import { DetailRowHorizontal } from '@/app/ui/utils/detail-row';
 import { UserRoles } from '@/app/utils/user-roles';
+import { auth } from '@/auth';
 import { UserModel } from '@/models/user-models';
 
-export default function UserDetail({ user }: { user: UserModel }) {
+export default async function UserDetail({ user }: { user: UserModel }) {
+  const session = await auth();
+
   return (
     <div className="flex flex-col">
       <DetailRowHorizontal label={'Jméno'} value={user.name} />
@@ -11,8 +14,12 @@ export default function UserDetail({ user }: { user: UserModel }) {
       <DetailRowHorizontal label={'UČO'} value={user.uco} />
       <DetailRowHorizontal label={'Role'} value={user.role === UserRoles.manager ? 'Manažér' : 'Ambasador'} />
       <DetailRowHorizontal label={'Potvrzen'} value={user.approved ? 'Ano' : 'Ne'} />
-      <DetailRowHorizontal label={'Registrovaný'} value={user.created_at.toLocaleString('cs-CZ')} />
-      <DetailRowHorizontal label={'Aktivní'} value={user.deleted_at ? 'Ne' : 'Ano'} />
+      {session?.user.role === UserRoles.manager && (
+        <div>
+          <DetailRowHorizontal label={'Registrovaný'} value={user.created_at.toLocaleString('cs-CZ')} />
+          <DetailRowHorizontal label={'Aktivní'} value={user.deleted_at ? 'Ne' : 'Ano'} />
+        </div>
+      )}
     </div>
   );
 }
