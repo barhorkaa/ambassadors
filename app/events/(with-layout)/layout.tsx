943f0865942392fa47bba582d048ff44bcc28b24
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 18:27:44 +0200

    fix(events/layout): removed slots restyled layout

--- app/events/(with-layout)/layout.tsx
+++ app/events/(with-layout)/layout.tsx
@@ -1,29 +1,34 @@
-import TableSkeleton from '@/app/ui/utils/skeletons';
+import { PlusIcon } from '@heroicons/react/24/outline';
 import { Metadata } from 'next';
-import { Suspense } from 'react';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Akce | AmbassadorsFIMU',
 };
 
-export default function Layout({
-  children,
-  all,
-  unapproved,
-}: {
-  children: React.ReactNode;
-  all: React.ReactNode;
-  unapproved: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
+      <div className="flex flex-row justify-between items-end content">
+        <h1>Akce</h1>
+        <Link className="btn" href={'/events/new'} prefetch={false}>
+          <PlusIcon className="h-5" />
+          <p className="hidden md:block">Přidat akci</p>
+        </Link>
+      </div>
+      <hr className="w-full mb-0" />
+      <ul className="menu menu-vertical lg:menu-horizontal bg-fi-100 w-full text-black font-semibold">
+        <li>
+          <Link href={'/events'}>Informace</Link>
+        </li>
+        <li>
+          <Link href={'/events/all'}>Aktivní akce</Link>
+        </li>
+        <li>
+          <Link href={'/events/unapproved'}>Nepotvrzené akce</Link>
+        </li>
+      </ul>
       {children}
-      <Suspense fallback={<TableSkeleton />}>
-        <div className="content">{unapproved}</div>
-      </Suspense>
-      <Suspense fallback={<TableSkeleton />}>
-        <div className="content">{all}</div>
-      </Suspense>
     </section>
   );
 }
