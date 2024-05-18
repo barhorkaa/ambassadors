ea8cb36e784f19998e6bbc0ecd65fc2e4695ee64
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 15:20:43 +0200

    fix(ambassadors/slots): removed slots replaced with routes

--- app/ambassadors/(with-layout)/layout.tsx
+++ app/ambassadors/(with-layout)/layout.tsx
@@ -4,36 +4,20 @@ export const metadata: Metadata = {
   title: 'Uživatelé | AmbassadorsFIMU',
 };
 
-export default function Layout({
-  children,
-  ambassadors,
-  managers,
-  unapproved,
-}: {
-  children: React.ReactNode;
-  ambassadors: React.ReactNode;
-  managers: React.ReactNode;
-  unapproved: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
       <h1 className="content">Uživatelé</h1>
       <hr className="w-full" />
       <div role="tablist" className="tabs tabs-lifted content">
         <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Nepotvrzeni" />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          {unapproved}
-        </div>
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6"></div>
 
         <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Ambasadoři" />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          {ambassadors}
-        </div>
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6"></div>
 
         <input type="radio" name="my_tabs_2" role="tab" className="tab w-fit" aria-label="Manažeři" />
-        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-          {managers}
-        </div>
+        <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6"></div>
       </div>
       {children}
     </section>
