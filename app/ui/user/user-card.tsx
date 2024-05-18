41de10fb3d712a17cc9ee6a06d4b08095ed07101
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-18 21:45:20 +0200

    fix(ui/userCard): deconstructed prop

--- app/ui/user/user-card.tsx
+++ app/ui/user/user-card.tsx
@@ -1,17 +1,17 @@
 import { UserModel } from '@/models/user-models';
 import Link from 'next/link';
 
-export default async function UserCard(props: { user: UserModel }) {
-  console.log('user in card is', props.user);
+export default async function UserCard({ user }: { user: UserModel }) {
+  console.log('user in card is', user);
   return (
     <div className="card shadow-lg w-5/6">
-      <Link href={`ambassadors/${props.user.id}`}>
+      <Link href={`ambassadors/${user.id}`}>
         <div className="card-body">
-          <h2 className="card-title">{props.user.name}</h2>
+          <h2 className="card-title">{user.name}</h2>
           <div className="flex flex-row justify-between">
             <div>
-              <p>E-mail: {props.user.email}</p>
-              <p>Tel. číslo: {props.user.phone_number}</p>
+              <p>E-mail: {user.email}</p>
+              <p>Tel. číslo: {user.phone_number}</p>
             </div>
           </div>
         </div>
