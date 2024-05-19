f48440cfdabc08a62449a0c06cf1c05bdf55ddb9
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 10:26:55 +0200

    fix(events/page/id): moved logic here

--- app/events/(without-layout)/[id]/@event/page.tsx
+++ app/events/(without-layout)/[id]/@event/page.tsx
@@ -1,15 +1,51 @@
+import { approveEventAction } from '@/app/lib/actions/events';
+import ApproveButton from '@/app/ui/button/approve-button';
 import EventDetail from '@/app/ui/event/event-detail';
+import DeleteEventModal from '@/app/ui/modals/delete/delete-event-modal';
+import EditEventModal from '@/app/ui/modals/edit/edit-event-modal';
+import { UserRoles } from '@/app/utils/user-roles';
+import { auth } from '@/auth';
+import { getAllEventTypesBasics } from '@/database/repository/event-type';
+import { getSignUpsForEvent, userSignUpForEventStatus } from '@/database/repository/event-user';
 import { getEventById } from '@/database/repository/events';
 import { EventDetailModel } from '@/models/event-models';
+import { EventTypeMinModel } from '@/models/event-type-models';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const event: EventDetailModel = await getEventById(params.id);
+
+  const session = await auth();
+  const isManager = session?.user.role === UserRoles.manager;
+  const userStatus = await userSignUpForEventStatus(event.id, session?.user.id!);
+
+  const eventTypes: EventTypeMinModel[] = await getAllEventTypesBasics();
+
+  let signedUpForEvent = await getSignUpsForEvent(event.id!, false);
 
   return (
     <div>
       <div className="data-display">
         <div className="card-body">
-          <EventDetail event={event} />
+          <div className="flex flex-col md:flex-row justify-between">
+            <h1 className="w-1/2">{event.name}</h1>
+            {event.deletedAt === null && (
+              <div className="flex flex-row gap-4 ">
+                {!event.approved && session?.user.role == UserRoles.manager && (
+                  <ApproveButton fun={approveEventAction} id={event.id} />
+                )}
+                {(isManager ||
+                  (userStatus !== undefined &&
+                    !userStatus.substitute &&
+                    userStatus.approved &&
+                    (event.date === null || new Date() <= event.date))) && (
+                  <EditEventModal event={event} eventTypes={eventTypes} />
+                )}
+                {isManager && <DeleteEventModal eventId={event.id} />}
+              </div>
+            )}
+          </div>
+          <hr className="w-full" />
+          <EventDetail event={event} signedUpForEvent={signedUpForEvent.length} isManager={isManager} />
         </div>
       </div>
     </div>
