c45766ffc27ac764b854a965f9c7e0d2e00e7d00
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 11:39:31 +0200

    fix(ui/eventCard): removed wrapping div

--- app/ui/event/event-card.tsx
+++ app/ui/event/event-card.tsx
@@ -4,17 +4,15 @@ import Link from 'next/link';
 
 export default function EventCard({ event }: { event: EventModel }) {
   return (
-    <div className="card w-5/6 bg-base-100 shadow-lg">
-      <Link href={`/events/${event.id}`} prefetch={false}>
-        <div className="card-body">
-          <h2 className="">{event.name}</h2>
-          <div className="badge bg-fi-100">{event.eventTypeName}</div>
-          <div className="flex flex-row gap-2">
-            <CalendarDaysIcon className="h-5" />
-            <p className="">{event.date !== null ? event.date.toLocaleDateString() : 'Nezadáno'}</p>
-          </div>
+    <Link href={`/events/${event.id}`} prefetch={false} className="card w-5/6 bg-base-100 shadow-lg">
+      <div className="card-body">
+        <h2 className="">{event.name}</h2>
+        <div className="badge bg-fi-100">{event.eventTypeName}</div>
+        <div className="flex flex-row gap-2">
+          <CalendarDaysIcon className="h-5" />
+          <p className="">{event.date !== null ? event.date.toLocaleDateString() : 'Nezadáno'}</p>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
