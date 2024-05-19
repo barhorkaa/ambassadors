be4c97d904daad3b457efe62d5a0d9605aba9035
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 14:36:05 +0200

    fix(ui/materialForm): switched to new form layout

--- app/ui/material/material-form.tsx
+++ app/ui/material/material-form.tsx
@@ -1,13 +1,24 @@
 'use client';
 
 import { createMaterialAction, editMaterialAction } from '@/app/lib/actions/material';
-import SubmitButton from '@/app/ui/button/submit-button';
+import { FormLayout } from '@/app/ui/utils/component-layouts';
 import FormControl from '@/app/ui/utils/form-control';
+import { formActionInitialState } from '@/app/ui/utils/form-errors';
 import { MaterialManipulationModel } from '@/models/material-models';
+import { useFormState } from 'react-dom';
 
 export function MaterialForm({ material }: { material: MaterialManipulationModel | null }) {
+  const [state, dispatch] = useFormState(
+    material === null ? createMaterialAction : editMaterialAction,
+    formActionInitialState
+  );
+
   return (
-    <form action={material === null ? createMaterialAction : editMaterialAction} className="card-body">
+    <FormLayout
+      action={dispatch}
+      state={state}
+      modalId={material === null ? 'create_material_modal' : 'edit' + material.id}
+    >
       <FormControl title={'Název materiálu'} id={'name'} defaultValue={material?.name} />
       <FormControl
         title={'Popis materiálu'}
@@ -18,7 +29,6 @@ export function MaterialForm({ material }: { material: MaterialManipulationModel
       <div className="form-control">
         <input id="id" value={material?.id} type="hidden" name="id" />
       </div>
-      <SubmitButton title={'Odeslat'} modalId={material === null ? 'create_material_modal' : 'edit' + material.id} />
-    </form>
+    </FormLayout>
   );
 }
