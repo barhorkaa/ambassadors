d10f43b2db0a789c8bf02c68ac25815265746035
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 14:08:11 +0200

    feat(actions/eventType): added error handling

--- app/lib/actions/event-type.ts
+++ app/lib/actions/event-type.ts
@@ -3,8 +3,9 @@
 import { createEventType, deleteEventType, editEventType, reviveEventType } from '@/database/repository/event-type';
 import { eventTypeSchema } from '@/models/event-type-models';
 import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
 
-export async function editEventTypeAction(formData: FormData) {
+export async function editEventTypeAction(prevState: any, formData: FormData) {
   try {
     const eventTypeForm = {
       name: formData.get('name'),
@@ -17,13 +18,26 @@ export async function editEventTypeAction(formData: FormData) {
     await editEventType(parsedData);
   } catch (e) {
     console.error(e);
-    throw e;
+    if (e instanceof z.ZodError) {
+      return {
+        success: false,
+        errors: e.issues,
+        generic: undefined,
+      };
+    } else
+      return {
+        success: false,
+        errors: [],
+        generic: 'Something went wrong',
+      };
   }
   revalidatePath('/events/types');
+  return { success: true, errors: [], generic: undefined };
 }
 
-export async function createEventTypeAction(formData: FormData) {
+export async function createEventTypeAction(prevState: any, formData: FormData) {
   try {
+    console.log('called create action');
     const eventTypeForm = {
       name: formData.get('name'),
       description: formData.get('description'),
@@ -34,9 +48,16 @@ export async function createEventTypeAction(formData: FormData) {
     await createEventType(parsedData);
   } catch (e) {
     console.error(e);
-    throw e;
+    if (e instanceof z.ZodError) {
+      return {
+        success: false,
+        errors: e.issues,
+        generic: undefined,
+      };
+    } else return { success: false, errors: [], generic: 'Something went wrong' };
   }
   revalidatePath('/events/types');
+  return { success: true, errors: [], generic: undefined };
 }
 
 export async function deleteEventTypeAction(id: string) {
