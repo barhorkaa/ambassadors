0dccea95d1e066f67e7111ccc5c52aefa21e1aec
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 10:41:12 +0200

    fix(models/event): removed unused

--- models/event-models.ts
+++ models/event-models.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod';
-import { zfd } from 'zod-form-data';
 
 export const eventSchema = z.object({
   id: z.string().optional(),
@@ -10,15 +9,6 @@ export const eventSchema = z.object({
   approved: z.string().pipe(z.coerce.boolean()).default(''),
 });
 export type EventManipulationModel = z.infer<typeof eventSchema>;
-
-export const eventFormModel = zfd.formData({
-  id: zfd.text(z.string().optional()),
-  name: zfd.text(z.string()),
-  date: zfd.text(z.preprocess((arg) => (arg === '' ? null : arg), z.string().pipe(z.coerce.date()))),
-  limit: zfd.text(z.string().pipe(z.coerce.number())),
-  event_type_id: zfd.text(z.string()),
-  approved: zfd.text(z.string().pipe(z.coerce.boolean()).default('')),
-});
 
 export type EventModel = {
   id: string;
