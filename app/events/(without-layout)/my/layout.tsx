fc4d136fed79bd0018caefecc64b314d4339226f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 11:49:14 +0200

    fix(events/layout/my): added page navigation

--- app/events/(without-layout)/my/layout.tsx
+++ app/events/(without-layout)/my/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Moje akce | AmbassadorsFIMU',
@@ -10,7 +11,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="content flex items-end">
         <h1>Moje akce</h1>
       </div>
-      <hr className="w-full" />
+      <hr className="w-full mb-0" />
+      <ul className="page-menu">
+        <li>
+          <Link href={'/events/my'}>Informace</Link>
+        </li>
+        <li>
+          <Link href={'/events/my/current'}>Aktuální přihlášení</Link>
+        </li>
+        <li>
+          <Link href={'/events/my/history'}>Historie přihlášení</Link>
+        </li>
+      </ul>
       <div className="content">{children}</div>
     </section>
   );
