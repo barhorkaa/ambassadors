ed7b0cdf25324c6896d63fce758997d7fb4dde99
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 11:36:24 +0200

    fix(events/page/history): added fallback

--- app/events/(without-layout)/history/page.tsx
+++ app/events/(without-layout)/history/page.tsx
@@ -1,8 +1,14 @@
 import { EventList } from '@/app/ui/utils/content-list';
+import TableSkeleton from '@/app/ui/utils/skeletons';
 import { getAllHistoryEvents } from '@/database/repository/events';
+import { Suspense } from 'react';
 
 export default async function Page() {
   const eventsWithApprovedReports = await getAllHistoryEvents(true);
 
-  return <EventList title={''} list={eventsWithApprovedReports!} emptyMessage={'V historii nic zatím nemáme'} />;
+  return (
+    <Suspense fallback={<TableSkeleton />}>
+      <EventList title={''} list={eventsWithApprovedReports!} emptyMessage={'V historii nic zatím nemáme'} />
+    </Suspense>
+  );
 }
