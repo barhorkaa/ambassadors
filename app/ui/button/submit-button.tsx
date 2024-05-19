c976419856affb656b713c5021abe0495545ef1d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 14:05:47 +0200

    feat(ui/submitButton): added listening to form state

--- app/ui/button/submit-button.tsx
+++ app/ui/button/submit-button.tsx
@@ -1,16 +1,22 @@
 'use client';
 
-import { useFormStatus } from 'react-dom';
+import { useFormState, useFormStatus } from 'react-dom';
 
-export default function SubmitButton({ title, modalId }: { title: string; modalId?: string }) {
+export default function SubmitButton({ title, modalId, fun }: { title: string; modalId?: string; fun: any }) {
   const { pending } = useFormStatus();
+
+  const [state, dispatch] = useFormState(fun, {
+    success: undefined,
+    errors: [],
+    generic: undefined,
+  });
 
   return (
     <button
       className="btn mt-4"
       aria-disabled={pending}
       onClick={() => {
-        if (document && modalId) {
+        if (document && modalId && state.success) {
           (document.getElementById(modalId) as HTMLFormElement).close();
         }
       }}
