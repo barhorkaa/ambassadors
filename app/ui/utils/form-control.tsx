8182f09f02f2c3315283e55ce9bf380cb07eab68
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 14:06:46 +0200

    feat(ui/formControl): added layout for forms

--- app/ui/utils/form-control.tsx
+++ app/ui/utils/form-control.tsx
@@ -1,3 +1,5 @@
+import SubmitButton from '@/app/ui/button/submit-button';
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import { ZodIssue } from 'zod';
 
 export default function FormControl({
@@ -60,3 +62,28 @@ export const findErrors = (fieldName: string, errors: ZodIssue[]) => {
     })
     .map((item) => item.message);
 };
+
+export function FormLayout({
+  action,
+  state,
+  modalId,
+  children,
+}: {
+  action: any;
+  state: any;
+  modalId: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <form action={action} className="card-body">
+      {children}
+      <SubmitButton title={'Odeslat'} modalId={modalId} fun={action} />
+      {state.generic && (
+        <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
+          <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+          <p className="text-sm text-red-500">{state.generic}</p>
+        </div>
+      )}
+    </form>
+  );
+}
