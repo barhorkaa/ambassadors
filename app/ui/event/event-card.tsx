1cef126774d7eb86f277a8f4bb6142e6c950a20f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-19 11:40:13 +0200

    fix(ui/eventCard): added card-title class

--- app/ui/event/event-card.tsx
+++ app/ui/event/event-card.tsx
@@ -6,7 +6,7 @@ export default function EventCard({ event }: { event: EventModel }) {
   return (
     <Link href={`/events/${event.id}`} prefetch={false} className="card w-5/6 bg-base-100 shadow-lg">
       <div className="card-body">
-        <h2 className="">{event.name}</h2>
+        <h2 className="card-title">{event.name}</h2>
         <div className="badge bg-fi-100">{event.eventTypeName}</div>
         <div className="flex flex-row gap-2">
           <CalendarDaysIcon className="h-5" />
