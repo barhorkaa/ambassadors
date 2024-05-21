b0328aa9cb1858820e12304e5ba387d7f21c3c86
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 01:26:03 +0200

    feat(materials/pages): switched to new listing component

--- app/materials/all/page.tsx
+++ app/materials/all/page.tsx
@@ -1,21 +1,9 @@
-import MaterialDetail from '@/app/ui/material/material-detail';
+import { MaterialList } from '@/app/ui/utils/content-list';
 import { getAllMaterials } from '@/database/repository/material';
 import { MaterialDetailModel } from '@/models/material-models';
 
 export default async function Page() {
   const allMaterials: MaterialDetailModel[] = await getAllMaterials(false);
 
-  return (
-    <>
-      {allMaterials.length === 0 ? (
-        <h2>Aktuálně nejsou k dispoici žádné materiály</h2>
-      ) : (
-        <div className="flex flex-col gap-6">
-          {allMaterials.map((material) => (
-            <MaterialDetail key={material.id} material={material} />
-          ))}
-        </div>
-      )}
-    </>
-  );
+  return <MaterialList materials={allMaterials} emptyMessage={'Aktuálně nejsou k dispoici žádné materiály'} />;
 }
--- app/materials/deleted/page.tsx
+++ app/materials/deleted/page.tsx
@@ -1,21 +1,9 @@
-import MaterialDetail from '@/app/ui/material/material-detail';
+import { MaterialList } from '@/app/ui/utils/content-list';
 import { getAllMaterials } from '@/database/repository/material';
 import { MaterialDetailModel } from '@/models/material-models';
 
 export default async function Page() {
   const deletedMaterials: MaterialDetailModel[] = await getAllMaterials(true);
 
-  return (
-    <>
-      {deletedMaterials.length === 0 ? (
-        <h2>Aktuálně jsou všechny materiály používány</h2>
-      ) : (
-        <div className="flex flex-col gap-6">
-          {deletedMaterials.map((material) => (
-            <MaterialDetail key={material.id} material={material} />
-          ))}
-        </div>
-      )}
-    </>
-  );
+  return <MaterialList materials={deletedMaterials} emptyMessage={'Aktuálně jsou všechny materiály používány'} />;
 }
