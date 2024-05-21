0df18cff46af518cdb93c789444d9d93335d0939
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 00:26:31 +0200

    fix(models/editEvent): deconstructed prop

--- app/ui/modals/edit/edit-event-modal.tsx
+++ app/ui/modals/edit/edit-event-modal.tsx
@@ -5,10 +5,16 @@ import ModalLayout from '@/app/ui/modals/modal-layout';
 import { EventDetailModel } from '@/models/event-models';
 import { EventTypeMinModel } from '@/models/event-type-models';
 
-export default function EditEventModal(data: { eventTypes: EventTypeMinModel[]; event: EventDetailModel }) {
+export default function EditEventModal({
+  eventTypes,
+  event,
+}: {
+  eventTypes: EventTypeMinModel[];
+  event: EventDetailModel;
+}) {
   return (
-    <ModalLayout id={data.event.id} title={'Upravit akci'} modalType={'edit'}>
-      <EventForm event={data.event} eventTypes={data.eventTypes} />
+    <ModalLayout id={event.id} title={'Upravit akci'} modalType={'edit'}>
+      <EventForm event={event} eventTypes={eventTypes} />
     </ModalLayout>
   );
 }
