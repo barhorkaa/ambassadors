b748e20ff9f580debd0895491a07a42b63d872e5
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 16:56:08 +0200

    cleanup(events/page/unapproved): cleanup

--- app/events/(with-layout)/unapproved/page.tsx
+++ app/events/(with-layout)/unapproved/page.tsx
@@ -5,9 +5,7 @@ import { EventModel } from '@/models/event-models';
 import { Suspense } from 'react';
 
 export default async function Page() {
-  console.log('calling get all unapproved events at: ', new Date());
   const allUnapprovedEvents: EventModel[] = await getAllActiveEvents(false);
-  console.log('got result in component from get all unapproved events at: ', new Date());
 
   return (
     <Suspense fallback={<TableSkeleton />}>
