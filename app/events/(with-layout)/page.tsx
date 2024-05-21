82533398f9bc1b6be8bbd37c165e5263df3c3a80
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 17:01:16 +0200

    fix(events/page): changed text

--- app/events/(with-layout)/page.tsx
+++ app/events/(with-layout)/page.tsx
@@ -6,25 +6,24 @@ export default function Page() {
       <SectionInfo
         title={'Informace o akcích'}
         contents={[
-          'Jako ambasador se můžete podílet na spoustě akcí pořádaných na fakultě, ale i mimo ní. ' +
-            'Počet akcí, na které pojedete počas semestru je jedině na vás!',
+          'Jako ambasador se můžeš podílet na spoustě akcí pořádaných na fakultě, ale i mimo ní. ' +
+            'Počet akcí, na které pojedeš počas semestru je jedině na tobě!',
         ]}
       />
       <hr />
       <SectionInfo
         title={'Jak se na akci přihlásit?'}
         contents={[
-          'V sekci Aktivní akce si najděte akci, které byste se chtěli zúčastnit a rozklikněte si její detail. Na ' +
-            'stránce detailu uvidíte tlačítko Přihlásit se, kterým se přihlásite na danou akci. Vaše přihlášení poté ' +
-            'musí zchválit manažer, po schválení se u vašeho jména objeví ikonka Check. Toto znamená, že oddělení o vašem ' +
-            'zájmu ví a počíta s Vámi.',
-          'Pokud v sekci Aktivní akce nevidíte akci, na kterou by ste chtěli jet, můžete založit nový návrh na akci. ' +
-            'Jednoduše klikněte na Přidat v pravé horní části této stránky a přidejte novou akci. Tato akce se poté objeví ' +
+          'V sekci Aktivní akce si nájdi akci, které byses se chtěl/a zúčastnit a rozklikni si její detail. Na ' +
+            'stránce detailu uvidíš tlačítko Přihlásit se, kterým se přihlásíš na danou akci. Tvoje přihlášení poté ' +
+            'musí zchválit manažer, po schválení se u tvojeho jména objeví ikonka Check. To znamená, že oddělení o tvém ' +
+            'zájmu ví a počíta s tebou.',
+          'Pokud v sekci Aktivní akce nevidíš akci, na kterou bys chtěl/a jet, můžeš založit nový návrh na akci. ' +
+            'Jednoduše klikni na Přidat v pravé horní části této stránky a přidej novou akci. Tato akce se poté objeví ' +
             'v sekci Nepotvrzené akce. Váš návrh bude muset opět schválit manažer a potom, co se tomu tak stane, se akce ' +
-            'přesune do sekce Aktivbí akce a budete se na ní moc přihlásit. ',
+            'přesune do sekce Aktivbí akce a budeš se na ní moc přihlásit.',
         ]}
       />
-      <hr />
     </div>
   );
 }
