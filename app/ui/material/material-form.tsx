72fac269be97d5035ee4161511c8faed41401c04
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 15:34:29 +0200

    fix(ui/materialForm): added error messages

--- app/ui/material/material-form.tsx
+++ app/ui/material/material-form.tsx
@@ -2,7 +2,7 @@
 
 import { createMaterialAction, editMaterialAction } from '@/app/lib/actions/material';
 import { FormLayout } from '@/app/ui/utils/component-layouts';
-import FormControl from '@/app/ui/utils/form-control';
+import FormControl, { findErrors } from '@/app/ui/utils/form-control';
 import { formActionInitialState } from '@/app/ui/utils/form-errors';
 import { MaterialManipulationModel } from '@/models/material-models';
 import { useFormState } from 'react-dom';
@@ -19,12 +19,18 @@ export function MaterialForm({ material }: { material: MaterialManipulationModel
       state={state}
       modalId={material === null ? 'create_material_modal' : 'edit' + material.id}
     >
-      <FormControl title={'Název materiálu'} id={'name'} defaultValue={material?.name} />
+      <FormControl
+        title={'Název materiálu'}
+        id={'name'}
+        defaultValue={material?.name}
+        errorMessage={findErrors('name', state.errors)[0]}
+      />
       <FormControl
         title={'Popis materiálu'}
         id={'description'}
         defaultValue={material?.description}
         inputType={'area'}
+        errorMessage={findErrors('description', state.errors)[0]}
       />
       <div className="form-control">
         <input id="id" value={material?.id} type="hidden" name="id" />
