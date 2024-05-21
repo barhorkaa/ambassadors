64322ffda7a460848176c930e7176452ce015290
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 14:39:37 +0200

    fix(types/page/deleted): moved styling

--- app/events/(without-layout)/types/deleted/page.tsx
+++ app/events/(without-layout)/types/deleted/page.tsx
@@ -6,16 +6,16 @@ export default async function Page() {
   const deletedEventTypes: EventTypeDetailModel[] = await getAllEventTypes(true);
 
   return (
-    <div className="flex flex-col gap-6">
+    <>
       {deletedEventTypes.length === 0 ? (
         <h2>Aktuálně jsou všechny typy akcí používány</h2>
       ) : (
-        <>
+        <div className="flex flex-col gap-6">
           {deletedEventTypes.map((eventType) => (
             <EventTypeDetail key={eventType.id} eventType={eventType} />
           ))}
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 }
