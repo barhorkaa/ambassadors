b500800a920cb7d59b8da546eece8ce0e9ef8713
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 14:40:08 +0200

    fix(materials/page/deleted): moved styling

--- app/materials/deleted/page.tsx
+++ app/materials/deleted/page.tsx
@@ -6,16 +6,16 @@ export default async function Page() {
   const deletedMaterials: MaterialDetailModel[] = await getAllMaterials(true);
 
   return (
-    <div className="flex flex-col gap-6">
+    <>
       {deletedMaterials.length === 0 ? (
         <h2>Aktuálně jsou všechny materiály používány</h2>
       ) : (
-        <>
+        <div className="flex flex-col gap-6">
           {deletedMaterials.map((material) => (
             <MaterialDetail key={material.id} material={material} />
           ))}
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 }
