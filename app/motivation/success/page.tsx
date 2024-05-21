2eee8c3881c8ed658952791f0e5d309a10c387c9
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 01:14:49 +0200

    fix(motivation/page/success): added signout action

--- app/motivation/success/page.tsx
+++ app/motivation/success/page.tsx
@@ -1,4 +1,5 @@
-import { auth, signOut } from '@/auth';
+import { signOutAction } from '@/app/lib/actions/authentication';
+import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
@@ -6,18 +7,14 @@ export default async function Page() {
   if (!session) {
     redirect('/');
   }
+
   return (
     <div className="page hero">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="hero-title">Váš motivační dotazník byl úspešně zaslán!</h1>
           <p className="py-6">Nyní se odhlaste aby se změny projevili. Po přihlášení uvidíte v aplikaci další kroky.</p>
-          <form
-            action={async () => {
-              'use server';
-              await signOut();
-            }}
-          >
+          <form action={signOutAction}>
             <button className="btn">Odhlásit se</button>
           </form>
         </div>
