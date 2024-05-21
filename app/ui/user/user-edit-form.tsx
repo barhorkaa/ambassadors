3fa38d57f4452f542fe9ed7cba262e2ac7e3f4fd
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 15:28:06 +0200

    fix(actions/register): added handle error

--- app/ui/user/user-edit-form.tsx
+++ app/ui/user/user-edit-form.tsx
@@ -3,7 +3,7 @@
 import { editUserAction, editUserFullAction } from '@/app/lib/actions/users';
 import UserRoleSelect from '@/app/ui/user/user-role-select';
 import { FormLayout } from '@/app/ui/utils/component-layouts';
-import FormControl from '@/app/ui/utils/form-control';
+import FormControl, { findErrors } from '@/app/ui/utils/form-control';
 import { formActionInitialState } from '@/app/ui/utils/form-errors';
 import { UserModel } from '@/models/user-models';
 import { useFormState } from 'react-dom';
@@ -13,12 +13,36 @@ export default function UserEditForm({ user, full }: { user: UserModel; full: bo
 
   return (
     <FormLayout action={dispatch} state={state} modalId={user.id}>
-      <FormControl title={'Celé jméno'} id={'name'} defaultValue={user.name} placeholder={'Jméno'} />
-      <FormControl title={'Telefonní číslo'} id={'phoneNumber'} type={'text'} defaultValue={user.phone_number} />
+      <FormControl
+        title={'Celé jméno'}
+        id={'name'}
+        defaultValue={user.name}
+        placeholder={'Jméno'}
+        errorMessage={findErrors('name', state.errors)[0]}
+      />
+      <FormControl
+        title={'Telefonní číslo'}
+        id={'phoneNumber'}
+        type={'text'}
+        defaultValue={user.phone_number}
+        errorMessage={findErrors('phone_number', state.errors)[0]}
+      />
       {full && (
         <>
-          <FormControl title={'UČO'} id={'uco'} type={'number'} defaultValue={+user.uco} />
-          <FormControl title={'E-mail'} id={'email'} type={'email'} defaultValue={user.email} />
+          <FormControl
+            title={'UČO'}
+            id={'uco'}
+            type={'number'}
+            defaultValue={+user.uco}
+            errorMessage={findErrors('uco', state.errors)[0]}
+          />
+          <FormControl
+            title={'E-mail'}
+            id={'email'}
+            type={'email'}
+            defaultValue={user.email}
+            errorMessage={findErrors('email', state.errors)[0]}
+          />
           <UserRoleSelect currentRole={user.role} />
         </>
       )}
