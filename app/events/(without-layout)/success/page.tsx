d5a1d1ed04ccac87121b9be07e0b1957d7551dd8
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 01:51:16 +0200

    fix(events/page/success): switched to user role

--- app/events/(without-layout)/success/page.tsx
+++ app/events/(without-layout)/success/page.tsx
@@ -1,4 +1,5 @@
 import { HeroCenterLayout } from '@/app/ui/utils/component-layouts';
+import { UserRoles } from '@/app/utils/user-roles';
 import { auth } from '@/auth';
 
 export default async function Page() {
@@ -6,7 +7,7 @@ export default async function Page() {
 
   return (
     <HeroCenterLayout title={'Akce byla úspěšně vytvořena!'} url={'/events'} buttonTitle={'Zpátky na akce'}>
-      {session?.user.role === 'ambassador' && (
+      {session?.user.role === UserRoles.ambassador && (
         <p className="pb-6">
           Váš návrh nyní musí schválit zaměstnanec propagace. Po schválení se bute moct na akci přihlásit.
         </p>
