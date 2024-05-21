86bb3f9f45f5cad8d3b84b76850288e70cf5f853
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 23:35:36 +0200

    fix(models/login): added error messages

--- models/login-model.ts
+++ models/login-model.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
 export const LoginModel = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
+  email: z.string({ required_error: 'Email je povinný údaj' }).email('Email nemá validní formát'),
+  password: z
+    .string({ required_error: 'Heslo je povinný údaj' })
+    .min(6, { message: 'Heslo musí obsahovat alespoň 6 znaků' }),
 });
