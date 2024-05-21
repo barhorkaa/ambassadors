9892b696a035c7532205fc28b91b674b1dbb8180
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 01:07:33 +0200

    feat(signups/layout): switched to page navigation

--- app/signups/layout.tsx
+++ app/signups/layout.tsx
@@ -1,26 +1,23 @@
+import PageNavigation from '@/app/ui/layout/page-navigation';
+import { PageUrl } from '@/app/utils/pages';
 import { Metadata } from 'next';
-import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Přihlášení na akce | AmbassadorsFIMU',
 };
+
+const signupsPages: PageUrl[] = [
+  { name: 'Informace', url: '/signups' },
+  { name: 'Všechna aktuální přihlášení', url: '/signups/all' },
+  { name: 'Nepotvrzená přihlášení', url: '/signups/unapproved' },
+];
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
       <h1 className="content">Přihlášení na akce</h1>
       <hr className="w-full mb-0" />
-      <ul className="page-menu">
-        <li>
-          <Link href={'/signups'}>Informace</Link>
-        </li>
-        <li>
-          <Link href={'/signups/all'}>Všechna aktuální přihlášení</Link>
-        </li>
-        <li>
-          <Link href={'/signups/unapproved'}>Nepotvrzená přihlášení</Link>
-        </li>
-      </ul>
+      <PageNavigation pages={signupsPages} />
       <div className="content">{children}</div>
     </section>
   );
