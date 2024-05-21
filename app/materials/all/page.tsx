65a515dd838172889849fa1a82477a18e8c4d640
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 14:40:29 +0200

    fix(materials/page/all): moved styling

--- app/materials/all/page.tsx
+++ app/materials/all/page.tsx
@@ -6,16 +6,16 @@ export default async function Page() {
   const allMaterials: MaterialDetailModel[] = await getAllMaterials(false);
 
   return (
-    <div className="flex flex-col gap-6">
+    <>
       {allMaterials.length === 0 ? (
         <h2>Aktuálně nejsou k dispoici žádné materiály</h2>
       ) : (
-        <>
+        <div className="flex flex-col gap-6">
           {allMaterials.map((material) => (
             <MaterialDetail key={material.id} material={material} />
           ))}
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 }
