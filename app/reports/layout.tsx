32ae6e4a1cb65e9441482170f04c89fefb4b889e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 21:40:08 +0200

    fix(reports/page): moved static to layout

--- app/reports/layout.tsx
+++ app/reports/layout.tsx
@@ -5,5 +5,11 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+  return (
+    <section>
+      <h1 className="content">Nové zprávy z akcí</h1>
+      <hr className="w-full" />
+      <div className="content">{children}</div>
+    </section>
+  );
 }
--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -3,17 +3,12 @@ import { getAllHistoryEvents } from '@/database/repository/events';
 
 export default async function Page() {
   const eventsWithUnapprovedReports = await getAllHistoryEvents(false);
+
   return (
-    <>
-      <h1 className="content">Nové zprávy z akcí</h1>
-      <hr className="w-full" />
-      <div className="content">
-        <EventList
-          title={''}
-          list={eventsWithUnapprovedReports!}
-          emptyMessage={'V tento moment jsou všechny zprávy potvrzeny!'}
-        />
-      </div>
-    </>
+    <EventList
+      title={''}
+      list={eventsWithUnapprovedReports!}
+      emptyMessage={'V tento moment jsou všechny zprávy potvrzeny!'}
+    />
   );
 }
