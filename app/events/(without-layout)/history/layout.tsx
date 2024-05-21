b2cb28a8614a713c740ad108802938da90ec837e
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 21:47:39 +0200

    fix(events/layout/history): added info about page

--- app/events/(without-layout)/history/layout.tsx
+++ app/events/(without-layout)/history/layout.tsx
@@ -9,7 +9,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <section>
       <h1 className="content">Historie akcí</h1>
       <hr className="w-full" />
-      <div className="content">{children}</div>
+      <div className="content">
+        <p>Na této stránce naleznete seznam akcí, které už úspěšně poběhli!</p>
+        <hr />
+        {children}
+      </div>
     </section>
   );
 }
