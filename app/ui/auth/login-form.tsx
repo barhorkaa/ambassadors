7b06b323b8597b61fc4be33872c3f2dd7f51bfc3
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 00:47:16 +0200

    cleanup(ui/loginForm): cleanup

--- app/ui/auth/login-form.tsx
+++ app/ui/auth/login-form.tsx
@@ -14,14 +14,12 @@ export default function LoginForm() {
       <FormControl title={'E-mail'} id={'email'} />
       <FormControl title={'Heslo'} id={'password'} type={'password'} />
       <SubmitButton title={'Přihlásit se'} />
-      <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
-        {errorMessage && (
-          <>
-            <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-            <p className="text-sm text-red-500">{errorMessage}</p>
-          </>
-        )}
-      </div>
+      {errorMessage && (
+        <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
+          <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+          <p className="text-sm text-red-500">{errorMessage}</p>
+        </div>
+      )}
     </form>
   );
 }
