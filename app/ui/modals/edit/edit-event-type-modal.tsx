a429ca3524dfb4bdeb03df458c1926bac9472f5d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 00:26:00 +0200

    fix(models/editType): deconstructed prop

--- app/ui/modals/edit/edit-event-type-modal.tsx
+++ app/ui/modals/edit/edit-event-type-modal.tsx
@@ -4,10 +4,10 @@ import { EventTypeForm } from '@/app/ui/event-type/event-type-form';
 import ModalLayout from '@/app/ui/modals/modal-layout';
 import { EventTypeDetailModel } from '@/models/event-type-models';
 
-export default function EditEventTypeModal(data: { eventType: EventTypeDetailModel }) {
+export default function EditEventTypeModal({ eventType }: { eventType: EventTypeDetailModel }) {
   return (
-    <ModalLayout id={'edit' + data.eventType.id} title={'Upravit informace o typu akce'} modalType={'edit'}>
-      <EventTypeForm eventType={data.eventType} />
+    <ModalLayout id={'edit' + eventType.id} title={'Upravit informace o typu akce'} modalType={'edit'}>
+      <EventTypeForm eventType={eventType} />
     </ModalLayout>
   );
 }
