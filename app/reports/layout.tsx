87ed907b019e28967f673b878692cc86df99d34a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-21 21:43:55 +0200

    fix(reports/layout): added info about page

--- app/reports/layout.tsx
+++ app/reports/layout.tsx
@@ -9,7 +9,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <section>
       <h1 className="content">Nové zprávy z akcí</h1>
       <hr className="w-full" />
-      <div className="content">{children}</div>
+      <div className="content">
+        <p>
+          V této sekci aplikace naleznete seznam všech akcí, které nemají potvrzenou zprávu. Klinutím na nějakou akci se
+          dostanete na její detail, kde si budete moct zprávu přečíst a potvrdit.
+        </p>
+        <hr />
+        {children}
+      </div>
     </section>
   );
 }
