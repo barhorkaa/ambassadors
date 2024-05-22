849caa629349ef59a5250c8c3a38298da52a24f9
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 02:20:16 +0200

    fix(ambassadors/layout): added page navigation

--- app/ambassadors/(with-layout)/layout.tsx
+++ app/ambassadors/(with-layout)/layout.tsx
@@ -1,29 +1,24 @@
+import PageNavigation from '@/app/ui/layout/page-navigation';
+import { PageUrl } from '@/app/utils/pages';
 import { Metadata } from 'next';
-import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Uživatelé | AmbassadorsFIMU',
 };
+
+const ambassadorPages: PageUrl[] = [
+  { name: 'Informace', url: '/ambassadors' },
+  { name: 'Nepotvrzení uživatelé', url: '/ambassadors/unapproved' },
+  { name: 'Ambasadoři', url: '/ambassadors/ambassadors' },
+  { name: 'Manažeři', url: '/ambassadors/managers' },
+];
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section>
       <h1 className="content">Uživatelé</h1>
       <hr className="w-full mb-0" />
-      <ul className="page-menu">
-        <li>
-          <Link href={'/ambassadors'}>Informace</Link>
-        </li>
-        <li>
-          <Link href={'/ambassadors/unapproved'}>Nepotvrzení uživatelé</Link>
-        </li>
-        <li>
-          <Link href={'/ambassadors/ambassadors'}>Ambasadoři</Link>
-        </li>
-        <li>
-          <Link href={'/ambassadors/managers'}>Manažeři</Link>
-        </li>
-      </ul>
+      <PageNavigation pages={ambassadorPages} />
       {children}
     </section>
   );
