9be1353127362ec26dfd779d48b8c9b091e41d49
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 02:33:43 +0200

    fix(banner): returned div back

--- app/ui/main-page/banner.tsx
+++ app/ui/main-page/banner.tsx
@@ -9,10 +9,13 @@ export default function Banner() {
       >
         <div className="hero-overlay bg-opacity-50"></div>
         <div className="hero-content text-left text-white sm:px-2 py-20 md:py-44">
-          <h1 className="hero-title">Staň se ambasadorem FI ještě dnes!</h1>
-          <p className="mb-5">
-            Přidej se k ambasadorskému programu FI a pomoz nám inspirovat novou generaci studentů a představit FI světu.
-          </p>
+          <div>
+            <h1 className="hero-title">Staň se ambasadorem FI ještě dnes!</h1>
+            <p className="mb-5">
+              Přidej se k ambasadorskému programu FI a pomoz nám inspirovat novou generaci studentů a představit FI
+              světu.
+            </p>
+          </div>
         </div>
       </div>
     </div>
