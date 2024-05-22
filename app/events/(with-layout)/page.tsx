9e9c1f264197032b694026e1413b05169429943c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 02:11:21 +0200

    fix(event/page): added content

--- app/events/(with-layout)/page.tsx
+++ app/events/(with-layout)/page.tsx
@@ -18,6 +18,12 @@ export default function Page() {
             'stránce detailu uvidíš tlačítko Přihlásit se, kterým se přihlásíš na danou akci. Tvoje přihlášení poté ' +
             'musí zchválit manažer, po schválení se u tvojeho jména objeví ikonka Check. To znamená, že oddělení o tvém ' +
             'zájmu ví a počíta s tebou.',
+        ]}
+      />
+      <hr />
+      <SectionInfo
+        title={'Založení nové akce'}
+        contents={[
           'Pokud v sekci Aktivní akce nevidíš akci, na kterou bys chtěl/a jet, můžeš založit nový návrh na akci. ' +
             'Jednoduše klikni na Přidat v pravé horní části této stránky a přidej novou akci. Tato akce se poté objeví ' +
             'v sekci Nepotvrzené akce. Váš návrh bude muset opět schválit manažer a potom, co se tomu tak stane, se akce ' +
