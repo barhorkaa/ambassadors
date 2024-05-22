dbc841c4ff916eb9181871e0ca6f1acd891e508c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-05-22 02:02:33 +0200

    fix(events/page): typos

--- app/events/(without-layout)/types/page.tsx
+++ app/events/(without-layout)/types/page.tsx
@@ -9,14 +9,14 @@ export default async function Page() {
     <>
       <SectionInfo
         title={''}
-        contents={['V této části aplikace jsou informace o různých druzích akcí, kterých se můžes zúčastnit.']}
+        contents={['V této části aplikace jsou informace o různých druzích akcí, kterých se můžeš zúčastnit.']}
       />
       <hr />
       <SectionInfo
         title={'K čemu slouží druhy akcí?'}
         contents={[
           'Všechny akce, kterých se můžeš zučasnit jsou nějakého typu.',
-          'Tento druh lze zvolit při vytvoření akce a bude reporezentovat kategorii, do které akce zapadá.',
+          'Tento typ lze zvolit při vytvoření akce a bude reporezentovat kategorii, do které akce zapadá.',
           'Podle toho se potom při detailu akce ukáží všeobecné pokyny pro ambasadory.',
         ]}
       />
@@ -34,17 +34,17 @@ export default async function Page() {
           <SectionInfo
             title={'Co můžu s druhy akcí dělat?'}
             contents={[
-              'Jakožto manažer můžete libovolně přidávat, upravovat a manazat všechny typy akcí.',
-              'Ambasadoři můžou vidět pouze ty druhy akcí, které nejsou vymyzány.',
+              'Jakožto manažer můžete libovolně přidávat, upravovat a mazat všechny typy akcí.',
+              'Ambasadoři můžou vidět pouze ty druhy akcí, které nejsou smazány.',
             ]}
           />
           <hr />
           <SectionInfo
-            title={'Mazánií a obnovování druhů akcí'}
+            title={'Mazání a obnovování druhů akcí'}
             contents={[
               'Druh akce vymažete tak, že stisknete tlačítko Smazat u daného typu.',
               'Když se vymaže nejaký druh akce tak se již nebude moct používat jako typ akce při vytváření nových akcí.',
-              'Pro obnovení nejakého druhu akce stačí přejít na Vymazané druhy akcí a stistknout tlačítko obnovit. ' +
+              'Pro obnovení nejakého druhu akce stačí přejít na Vymazané druhy akcí a stistknout tlačítko Obnovit. ' +
                 'Poté bude typ znova viditelný pro všechny.',
             ]}
           />
