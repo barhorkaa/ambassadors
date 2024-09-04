f5be4363281b76a14461da77fc70bf2877099056
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-04 16:08:20 +0200

    feat(models/notifications): created types for notifications

---
+++ models/notifications-models.ts
@@ -0,0 +1,21 @@
+type NotificationBase = {
+  id: string;
+  userId: string;
+};
+
+export type UserNotifications = NotificationBase & {
+  registrationApprove: boolean;
+  signupApprove: boolean;
+  eventApprove: boolean;
+  reportApprove: boolean;
+  personalInfoChange: boolean;
+  eventChange: boolean;
+  newEvent: boolean;
+};
+
+export type ManagerNotifications = NotificationBase & {
+  newEventSuggestion: boolean;
+  newRegistration: boolean;
+  newReport: boolean;
+  newSignup: boolean;
+};
