923aa98b4c8e938cced6aef16dfea1a14bc0cbe1
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-09 17:11:25 +0200

    feat(emails/skeleton): added skeleton for emails

---
+++ emails/utils/email-skeleton.tsx
@@ -0,0 +1,64 @@
+import {
+  Body,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Link,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components';
+
+interface EmailSkeletonProps {
+  children: React.ReactNode;
+  title: string;
+}
+
+const EmailSkeleton = ({ children, title }: EmailSkeletonProps) => (
+  <Html>
+    <Head>
+      <Font
+        fontFamily="Roboto"
+        fallbackFontFamily="Verdana"
+        webFont={{
+          url: 'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
+          format: 'woff2',
+        }}
+        fontWeight={400}
+        fontStyle="normal"
+      />
+    </Head>
+    <Preview>[Ambasadorský program] Vítejte mezi námi</Preview>
+    <Tailwind
+      config={{
+        theme: {
+          extend: {
+            colors: {
+              brand: '#007291',
+            },
+          },
+        },
+      }}
+    >
+      <Body>
+        <Container className="shadow-xl content-center bg-base-100 pt-0">
+          <Heading as="h1" className="bg-[#F2D45C] p-4">
+            {title}
+          </Heading>
+          <Section className="px-4">{children}</Section>
+          <Hr />
+          <Text className="p-4">
+            Pokud si nepřejete dostávat e-maily s těmito informacemi, můžete si nastavit preference notifikací na{' '}
+            <Link href={process.env['HOSTING'] + '/me'}>své osobní stránce</Link> v sekci Notifikace.
+          </Text>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
+);
+
+export default EmailSkeleton;
