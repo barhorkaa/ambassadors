d2577f0d63f1f6fec573417a78e87f3c140164a8
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-10 14:39:28 +0200

    fix(emails/newEventTemplate): changed to use new display component

--- emails/new-event-template.tsx
+++ emails/new-event-template.tsx
@@ -1,23 +1,12 @@
+import EmailEventDisplay from '@/emails/utils/email-event-display';
 import EmailSkeleton from '@/emails/utils/email-skeleton';
 import { EventDetailModel } from '@/models/event-models';
-import { Container, Heading, Link, Text } from '@react-email/components';
+import { Link, Text } from '@react-email/components';
 
 const NewEventTemplate = ({ event }: { event: EventDetailModel }) => (
   <EmailSkeleton title={'Byla přidaná nová akce'} preview={'[Ambasadorský program] Přidali jsme novou akci'}>
     <Text>Přidali jsme novou akci!</Text>
-    <Container className="shadow-xl px-4 py-2 ">
-      <Heading as="h2" className="">
-        {event.name}
-      </Heading>
-      <Text>
-        <strong>Datum: </strong>
-        {event.date ? event.date.toLocaleDateString() : 'Zatím nezadáno'}
-      </Text>
-      <Text>
-        <strong>Limit: </strong>
-        {event.limit}
-      </Text>
-    </Container>
+    <EmailEventDisplay event={event} />
     <Text>
       Jestli máte zájem zúčastnit se na akci můžete se přihlásit na{' '}
       <Link href={process.env['HOSTING'] + '/events/' + event.id}>stránce akce</Link>.
