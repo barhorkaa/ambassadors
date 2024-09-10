0ed880fedb71f1b5bf3bc889e381e2bf504c8114
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-10 14:38:47 +0200

    feat(emails/newSignUpTemplate): created template for new signups

---
+++ emails/manager/new-signup-template.tsx
@@ -0,0 +1,30 @@
+import EmailSkeleton from '@/emails/utils/email-skeleton';
+import { EventDetailModel } from '@/models/event-models';
+import { UserModel } from '@/models/user-models';
+import { Container, Link, Text } from '@react-email/components';
+
+interface NewSignupProps {
+  event: EventDetailModel;
+  user: UserModel;
+  substitute: boolean;
+}
+
+const NewSignupTemplate = ({ event, user, substitute }: NewSignupProps) => (
+  <EmailSkeleton title={'Nové přihlášení na akci'} preview={''}>
+    <Text>V aplikaci přibylo noé přihlášení na akci.</Text>
+    <Container>
+      <Text>
+        Uživatel: <Link href={process.env['HOSTING'] + '/ambassadors/' + user.id}>{user.name}</Link>
+      </Text>
+      <Text>
+        Akce: <Link href={process.env['HOSTING'] + '/events/' + event.id}>{event.name}</Link>
+      </Text>
+      <Text>Náhradník: {substitute ? 'Ano' : 'Ne'}</Text>
+    </Container>
+    <Text>
+      Přihlášení můžete potvrdit <Link href={process.env['HOSTING'] + '/signups/unapproved'}>zde</Link>
+    </Text>
+  </EmailSkeleton>
+);
+
+export default NewSignupTemplate;
