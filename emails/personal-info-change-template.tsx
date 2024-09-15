0179762cf4856e2f44bb01e8164e6dd5f7037332
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-15 19:37:37 +0200

    feat(emails/personalInfoChange): added contents to email about personal info change

---
+++ emails/personal-info-change-template.tsx
@@ -0,0 +1,21 @@
+import EmailSkeleton from '@/emails/utils/email-skeleton';
+import EmailUserDisplay from '@/emails/utils/email-user-display';
+import { UserModel } from '@/models/user-models';
+import { Heading, Text } from '@react-email/components';
+
+interface PersonalInfoChangeProps {
+  oldInfo: UserModel;
+  newInfo: UserModel;
+}
+
+const PersonalInfoChangeTemplate = ({ oldInfo, newInfo }: PersonalInfoChangeProps) => (
+  <EmailSkeleton title={'Došlo ke změně vašich osobních údajů'} preview={''}>
+    <Text>Vaše osobní informace byli změněny.</Text>
+    <Heading as="h3">Informace před změnou: </Heading>
+    <EmailUserDisplay user={oldInfo} compareUser={newInfo} />
+    <Heading as="h3">Informace po změně: </Heading>
+    <EmailUserDisplay user={newInfo} compareUser={oldInfo} />
+  </EmailSkeleton>
+);
+
+export default PersonalInfoChangeTemplate;
