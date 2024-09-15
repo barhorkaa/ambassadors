6a2f646139cacc88542bc7e0ce3d137604e5ea9d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-15 22:08:33 +0200

    fix(actions/nodemailer): added try catch

--- app/lib/actions/nodemailer.ts
+++ app/lib/actions/nodemailer.ts
@@ -38,152 +38,198 @@ async function sendEmailNode(mailOptions: MailOptions) {
     console.log('Email sent:', info.response);
   } catch (error) {
     console.error('Error sending email:', error);
+    throw error;
   }
 }
 
 export async function emailNewEventAction(id: string) {
-  const event = await getEventById(id);
+  try {
+    const event = await getEventById(id);
 
-  // const recipients = await getRecipients('new_event');
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = await getRecipients('new_event');
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Nová akce',
-    html: render(NewEventTemplate({ event: event })),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Nová akce',
+      html: render(NewEventTemplate({ event: event })),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 export async function emailRegistrationApprove(userId: string) {
-  const user = await getUserById(userId);
+  try {
+    const user = await getUserById(userId);
 
-  // const recipients = user.email;
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = user.email;
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Registrace potvrzena',
-    html: render(RegistrationApproveTemplate()),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Registrace potvrzena',
+      html: render(RegistrationApproveTemplate()),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 export async function emailSignupApprove(userId: string, eventId: string, substitute: boolean) {
-  const event = await getEventById(eventId);
-  const user = await getUserById(userId);
+  try {
+    const event = await getEventById(eventId);
+    const user = await getUserById(userId);
 
-  // const recipients = user.email;
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = user.email;
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Potvrzení přihlášení na akci',
-    html: render(SignupApproveTemplate({ event: event, substitute: substitute })),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Potvrzení přihlášení na akci',
+      html: render(SignupApproveTemplate({ event: event, substitute: substitute })),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 export async function emailEventChange(oldEvent: EventDetailModel, eventId: string) {
-  const event = await getEventById(eventId);
+  try {
+    const event = await getEventById(eventId);
 
-  // const recipients = await getRecipientsForEventChange(eventId);
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = await getRecipientsForEventChange(eventId);
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Změna akce',
-    html: render(EventChangeTemplate({ newEvent: event, oldEvent: oldEvent })),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Změna akce',
+      html: render(EventChangeTemplate({ newEvent: event, oldEvent: oldEvent })),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 export async function emailPersonalInfoChangeAction(oldInfo: UserModel) {
-  const newInfo = await getUserById(oldInfo.id);
+  try {
+    const newInfo = await getUserById(oldInfo.id);
 
-  // const recipients = newInfo.email;
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = newInfo.email;
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Změna osobních údajů',
-    html: render(PersonalInfoChangeTemplate({ newInfo: newInfo, oldInfo: oldInfo })),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Změna osobních údajů',
+      html: render(PersonalInfoChangeTemplate({ newInfo: newInfo, oldInfo: oldInfo })),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 export async function emailManagerNewSuggestionAction(id: string) {
-  const event = await getEventById(id);
+  try {
+    const event = await getEventById(id);
 
-  // const recipients = await getManagerRecipients('new_event_suggestion');
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = await getManagerRecipients('new_event_suggestion');
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Nový návrh na akci',
-    html: render(NewSuggestionTemplate({ event: event })),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Nový návrh na akci',
+      html: render(NewSuggestionTemplate({ event: event })),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 export async function emailManagerNewRegistrationAction(id: string) {
-  const user = await getUserById(id);
+  try {
+    const user = await getUserById(id);
 
-  // const recipients = await getManagerRecipients('new_registration');
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = await getManagerRecipients('new_registration');
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Nová registrace',
-    html: render(NewRegistrationTemplate({ user: user })),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Nová registrace',
+      html: render(NewRegistrationTemplate({ user: user })),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 export async function emailManagerNewReportAction(eventId: string) {
-  const event = await getEventById(eventId);
-  const report = await getEventReport(eventId);
+  try {
+    const event = await getEventById(eventId);
+    const report = await getEventReport(eventId);
 
-  // const recipients = await getManagerRecipients('new_report');
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = await getManagerRecipients('new_report');
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Nová zpáva z akce',
-    html: render(NewReportTemplate({ event: event, report: report! })),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Nová zpáva z akce',
+      html: render(NewReportTemplate({ event: event, report: report! })),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
 
 export async function emailManagerNewSignupAction(eventId: string, userId: string, isSubstitute: boolean) {
-  const event = await getEventById(eventId);
-  const user = await getUserById(userId);
+  try {
+    const event = await getEventById(eventId);
+    const user = await getUserById(userId);
 
-  // const recipients = await getManagerRecipients('new_signup');
-  const recipients = 'barculka1.3@gmail.com';
+    // const recipients = await getManagerRecipients('new_signup');
+    const recipients = 'barculka1.3@gmail.com';
 
-  const mailOptions = {
-    ...BaseOptions,
-    bcc: recipients,
-    subject: '[Ambasadorský program] Nové přihlášení na akci',
-    html: render(NewSignupTemplate({ event: event, user: user, substitute: isSubstitute })),
-  };
+    const mailOptions = {
+      ...BaseOptions,
+      bcc: recipients,
+      subject: '[Ambasadorský program] Nové přihlášení na akci',
+      html: render(NewSignupTemplate({ event: event, user: user, substitute: isSubstitute })),
+    };
 
-  await sendEmailNode(mailOptions);
+    await sendEmailNode(mailOptions);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 }
