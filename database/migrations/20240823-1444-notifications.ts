3615664fcc9c0703d1a6a347a726f2fd2bc1cc20
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-16 10:53:12 +0200

    fix(migrations/notifications): made true default value for all notifications

--- database/migrations/20240823-1444-notifications.ts
+++ database/migrations/20240823-1444-notifications.ts
@@ -6,11 +6,11 @@ export async function up(db: Kysely<any>): Promise<void> {
     .ifNotExists()
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('user_id', 'char(36)', (col) => col.references('user.id').notNull())
-    .addColumn('registration_approve', 'boolean', (col) => col.notNull().defaultTo(false))
-    .addColumn('signup_approve', 'boolean', (col) => col.notNull().defaultTo(false))
-    .addColumn('personal_info_change', 'boolean', (col) => col.notNull().defaultTo(false))
-    .addColumn('event_change', 'boolean', (col) => col.notNull().defaultTo(false))
-    .addColumn('new_event', 'boolean', (col) => col.notNull().defaultTo(false))
+    .addColumn('registration_approve', 'boolean', (col) => col.notNull().defaultTo(true))
+    .addColumn('signup_approve', 'boolean', (col) => col.notNull().defaultTo(true))
+    .addColumn('personal_info_change', 'boolean', (col) => col.notNull().defaultTo(true))
+    .addColumn('event_change', 'boolean', (col) => col.notNull().defaultTo(true))
+    .addColumn('new_event', 'boolean', (col) => col.notNull().defaultTo(true))
     .execute();
 
   await db.schema
@@ -18,10 +18,10 @@ export async function up(db: Kysely<any>): Promise<void> {
     .ifNotExists()
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('user_id', 'char(36)', (col) => col.references('user.id').notNull())
-    .addColumn('new_event_suggestion', 'boolean', (col) => col.notNull().defaultTo(false))
-    .addColumn('new_registration', 'boolean', (col) => col.notNull().defaultTo(false))
-    .addColumn('new_signup', 'boolean', (col) => col.notNull().defaultTo(false))
-    .addColumn('new_report', 'boolean', (col) => col.notNull().defaultTo(false))
+    .addColumn('new_event_suggestion', 'boolean', (col) => col.notNull().defaultTo(true))
+    .addColumn('new_registration', 'boolean', (col) => col.notNull().defaultTo(true))
+    .addColumn('new_signup', 'boolean', (col) => col.notNull().defaultTo(true))
+    .addColumn('new_report', 'boolean', (col) => col.notNull().defaultTo(true))
     .execute();
 }
 
