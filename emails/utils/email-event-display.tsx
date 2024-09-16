41285869437ae177b0c3ee19de944b5c4a8170c4
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-16 10:13:44 +0200

    fix(emails/eventDisplay): made compare prop optional

--- emails/utils/email-event-display.tsx
+++ emails/utils/email-event-display.tsx
@@ -4,21 +4,21 @@ import { Container, Heading } from '@react-email/components';
 
 interface EventDisplayProps {
   event: EventDetailModel;
-  eventCompare: EventDetailModel;
+  eventCompare?: EventDetailModel;
 }
 
 const EmailEventDisplay = ({ event, eventCompare }: EventDisplayProps) => (
   <Container className="shadow-xl px-4 py-2 ">
-    <Heading as="h2" className={event.name !== eventCompare.name ? 'text-[#FF0000]' : ''}>
+    <Heading as="h2" className={eventCompare && event.name !== eventCompare.name ? 'text-[#FF0000]' : ''}>
       {event.name}
     </Heading>
     <DisplayRow
       tag={'Datum'}
       property={event.date ? event.date.toLocaleDateString() : ''}
-      propertyComp={eventCompare.date ? eventCompare.date.toLocaleDateString() : ''}
+      propertyComp={eventCompare?.date ? eventCompare?.date.toLocaleDateString() : ''}
       value={event.date ? event.date.toLocaleDateString() : 'Zatím nezadáno'}
     />
-    <DisplayRow tag={'Limit'} property={event.limit} propertyComp={eventCompare.limit} />
+    <DisplayRow tag={'Limit'} property={event.limit} propertyComp={eventCompare?.limit} />
   </Container>
 );
 
