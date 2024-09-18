7c2ccc009d77c7d8302d7b110a11d78d9c774267
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-18 18:23:56 +0200

    feat(emails/verifyEmail): created email for email verification

---
+++ emails/verify-email-template.tsx
@@ -0,0 +1,15 @@
+import EmailSkeleton from '@/emails/utils/email-skeleton';
+import { Link, Text } from '@react-email/components';
+
+interface VerifyEmailProps {
+  token: string;
+}
+
+const VerifyEmailTemplate = ({ token }: VerifyEmailProps) => (
+  <EmailSkeleton title={'Potvrďte svůj e-mail'} preview={'Pro dokončení registrace musíte potvrdit svůj e-mail'}>
+    <Text>Svůj e-mail potvrďtě kliknutím na následujíci link.</Text>
+    <Link href={process.env['HOSTING'] + '/email/verify/' + token}>Potvrdit e-mail</Link>
+  </EmailSkeleton>
+);
+
+export default VerifyEmailTemplate;
