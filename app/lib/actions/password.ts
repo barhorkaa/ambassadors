cd41053274f95b35a55ed5d94df9c35e09db2d08
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-19 19:38:40 +0200

    feat(actions/password): added action to get email

--- app/lib/actions/password.ts
+++ app/lib/actions/password.ts
@@ -32,3 +32,26 @@ export async function changePassword(prevState: any, formData: FormData) {
   await signOutAction();
   return { success: true, errors: [], generic: undefined };
 }
+
+export async function getEmailAction(prevState: any, formData: FormData) {
+  try {
+    const emailForm = {
+      email: formData.get('email'),
+    };
+
+    const parsedEmail = emailSchema.parse(emailForm);
+
+    const user = await getUserByEmail(parsedEmail.email);
+    if (user === undefined) return { success: false, errors: [], generic: 'Zadali jste špatný e-mail' };
+
+    const user_token = jwt.sign({ id: user.id }, process.env['RESET_TOKEN_SECRET'] as Secret, {
+      expiresIn: '1h',
+    });
+
+    await emailResetPassword(parsedEmail.email, user_token);
+  } catch (e) {
+    console.error(e);
+    return handleError(e);
+  }
+  return { success: true, errors: [], generic: undefined };
+}
