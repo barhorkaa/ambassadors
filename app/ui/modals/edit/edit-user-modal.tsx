a0dd1c5dc0cb2e831788cb8fa03d53984ce59124
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-19 15:48:48 +0200

    feat(modals/editUser): added changing of password redirect

--- app/ui/modals/edit/edit-user-modal.tsx
+++ app/ui/modals/edit/edit-user-modal.tsx
@@ -3,13 +3,15 @@
 import ModalLayout from '@/app/ui/modals/modal-layout';
 import UserEditForm from '@/app/ui/user/user-edit-form';
 import { UserModel } from '@/models/user-models';
+import Link from 'next/link';
 
 interface EditUserModalProps {
   user: UserModel;
   full: boolean;
+  canEditPassword: boolean;
 }
 
-export default function EditUserModal({ user, full }: EditUserModalProps) {
+export default function EditUserModal({ user, full, canEditPassword }: EditUserModalProps) {
   return (
     <ModalLayout id={user.id} title={'Upravit informace o uživateli'} modalType={'edit'}>
       {!full && (
@@ -19,6 +21,19 @@ export default function EditUserModal({ user, full }: EditUserModalProps) {
         </p>
       )}
       <UserEditForm user={user} full={full} />
+      {canEditPassword && (
+        <>
+          <hr className="w-full" />
+          <div className=" form-control">
+            <label className="label" htmlFor="password">
+              <span className="label-text">Heslo</span>
+            </label>
+            <Link href={'/password/change'} className="btn bg-fi-100 m-0">
+              Změnit heslo
+            </Link>
+          </div>
+        </>
+      )}
     </ModalLayout>
   );
 }
