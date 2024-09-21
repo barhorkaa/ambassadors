ca37c2ed3df77d17e9cf90f5bea5329a5e3e8d25
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-21 19:33:16 +0200

    fix(emails/resetPassword): removed email from props

--- emails/reset-password-template.tsx
+++ emails/reset-password-template.tsx
@@ -3,10 +3,9 @@ import { Link, Text } from '@react-email/components';
 
 interface ResetPasswordProps {
   token: string;
-  email: string;
 }
 
-const ResetPasswordTemplate = ({ token, email }: ResetPasswordProps) => (
+const ResetPasswordTemplate = ({ token }: ResetPasswordProps) => (
   <EmailSkeleton title={'Obnovení hesla'} preview={'Obnovte své heslo'}>
     <Text>Heslo obnovíte na stránce, na kterou se dostanete kliknutím na následujíci link.</Text>
     <Link href={process.env['HOSTING'] + '/password/reset/' + token}>Resetovat heslo</Link>
