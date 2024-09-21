db3424b12b7521a8258bb80c55f812cda2fbd543
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-21 16:05:35 +0200

    feat(models/password): added model for password reset

--- models/password-models.ts
+++ models/password-models.ts
@@ -9,3 +9,14 @@ export const passwordSchema = z.object({
     .string({ required_error: 'Nové heslo je povinný údaj' })
     .min(6, { message: 'Heslo musí obsahovat alespoň 6 znaků' }),
 });
+
+export const emailSchema = z.object({
+  email: z.string({ required_error: 'Email je povinný údaj' }).email('Email nemá validní formát'),
+});
+
+export const emailResetSchema = z.object({
+  email: z.string().email(),
+  newPassword: z
+    .string({ required_error: 'Nové heslo je povinný údaj' })
+    .min(6, { message: 'Heslo musí obsahovat alespoň 6 znaků' }),
+});
