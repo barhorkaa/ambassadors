eee12b983df881b80aedcc4f1023a8189f18c35c
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-23 10:34:31 +0200

    feat(utils/formLayoutProps): added props for layouts with form

--- app/utils/interface-props.ts
+++ app/utils/interface-props.ts
@@ -6,3 +6,8 @@ export interface ErrorProps {
 export interface BaseLayoutProps {
   children: React.ReactNode;
 }
+
+export interface FormLayoutProps {
+  children: React.ReactNode;
+  form: React.ReactNode;
+}
