3fca14fbee0b991d84076fc515eb4f40809297b6
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-23 10:21:48 +0200

    feat(event/w-layout/error): extracted props

--- app/events/(with-layout)/error.tsx
+++ app/events/(with-layout)/error.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+import { ErrorProps } from '@/app/utils/interface-props';
+
+export default function Error({ error, reset }: ErrorProps) {
   return (
     <div className="page flex flex-col gap-6">
       <h2>Nepodařilo se načíst akce</h2>
