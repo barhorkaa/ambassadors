a994f6cd41f4b9ef211d2ff3701506b065cfa81f
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-23 10:15:28 +0200

    feat(utils/interfaceProps): added base layout props

--- app/ambassadors/(with-layout)/layout.tsx
+++ app/ambassadors/(with-layout)/layout.tsx
@@ -1,4 +1,5 @@
 import PageNavigation from '@/app/ui/layout/page-navigation';
+import { BaseLayoutProps } from '@/app/utils/interface-props';
 import { PageUrl } from '@/app/utils/pages';
 import { Metadata } from 'next';
 
@@ -13,7 +14,7 @@ const ambassadorPages: PageUrl[] = [
   { name: 'Manažeři', url: '/ambassadors/managers' },
 ];
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: BaseLayoutProps) {
   return (
     <section>
       <h1 className="content">Uživatelé</h1>
