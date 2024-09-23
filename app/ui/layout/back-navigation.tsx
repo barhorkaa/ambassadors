2e112ff900191dc54f3be5b018aab5fc0cea35ba
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-23 10:57:42 +0200

    feat(ui/backNavigation): extracted props

--- app/ui/layout/back-navigation.tsx
+++ app/ui/layout/back-navigation.tsx
@@ -1,7 +1,12 @@
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
-export default function BackNavigation({ href, tooltip }: { href: string; tooltip: string }) {
+interface BackNavigationProps {
+  href: string;
+  tooltip: string;
+}
+
+export default function BackNavigation({ href, tooltip }: BackNavigationProps) {
   return (
     <Link href={href}>
       <ArrowLeftIcon title={tooltip} className="h-5" />
