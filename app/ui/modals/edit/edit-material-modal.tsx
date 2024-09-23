1fb1db85c8af557ec1e061bdc87bd5d5fe1acb41
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-23 12:16:57 +0200

    feat(modal/editEMaterial): extracted props

--- app/ui/modals/edit/edit-material-modal.tsx
+++ app/ui/modals/edit/edit-material-modal.tsx
@@ -4,7 +4,11 @@ import { MaterialForm } from '@/app/ui/material/material-form';
 import ModalLayout from '@/app/ui/modals/modal-layout';
 import { MaterialDetailModel } from '@/models/material-models';
 
-export default function EditMaterialModal({ material }: { material: MaterialDetailModel }) {
+interface EditMaterialModalProps {
+  material: MaterialDetailModel;
+}
+
+export default function EditMaterialModal({ material }: EditMaterialModalProps) {
   return (
     <ModalLayout id={'edit' + material.id} title={'Upravit informace o materiálu'} modalType={'edit'}>
       <MaterialForm material={material} />
