c322d03ef2997de0a5b4e390ef6b52885ebfffe1
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-23 12:16:39 +0200

    feat(modal/editEvent): extracted props

--- app/ui/modals/edit/edit-event-modal.tsx
+++ app/ui/modals/edit/edit-event-modal.tsx
@@ -5,13 +5,12 @@ import ModalLayout from '@/app/ui/modals/modal-layout';
 import { EventDetailModel } from '@/models/event-models';
 import { EventTypeMinModel } from '@/models/event-type-models';
 
-export default function EditEventModal({
-  eventTypes,
-  event,
-}: {
+interface EditEventModelProps {
   eventTypes: EventTypeMinModel[];
   event: EventDetailModel;
-}) {
+}
+
+export default function EditEventModal({ eventTypes, event }: EditEventModelProps) {
   return (
     <ModalLayout id={event.id} title={'Upravit akci'} modalType={'edit'}>
       <EventForm event={event} eventTypes={eventTypes} />
