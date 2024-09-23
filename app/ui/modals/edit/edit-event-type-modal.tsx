2fb1860b15b0c0642bada5b7ab582273ecce100d
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-23 12:16:47 +0200

    feat(modal/editEventType): extracted props

--- app/ui/modals/edit/edit-event-type-modal.tsx
+++ app/ui/modals/edit/edit-event-type-modal.tsx
@@ -4,7 +4,11 @@ import { EventTypeForm } from '@/app/ui/event-type/event-type-form';
 import ModalLayout from '@/app/ui/modals/modal-layout';
 import { EventTypeDetailModel } from '@/models/event-type-models';
 
-export default function EditEventTypeModal({ eventType }: { eventType: EventTypeDetailModel }) {
+interface EditEventTypeModalProps {
+  eventType: EventTypeDetailModel;
+}
+
+export default function EditEventTypeModal({ eventType }: EditEventTypeModalProps) {
   return (
     <ModalLayout id={'edit' + eventType.id} title={'Upravit informace o typu akce'} modalType={'edit'}>
       <EventTypeForm eventType={eventType} />
