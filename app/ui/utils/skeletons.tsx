c7e6e9d4295ef814e4010d925480c88c03b5e231
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-23 20:37:28 +0200

    fix(ui/skeletons): changed functions to const in skeletons

--- app/ui/utils/skeletons.tsx
+++ app/ui/utils/skeletons.tsx
@@ -1,7 +1,3 @@
-export function TableSkeleton() {
-  return <div className="skeleton w-full h-64 my-6 rounded-none"></div>;
-}
+export const TableSkeleton = () => <div className="skeleton w-full h-64 my-6 rounded-none"></div>;
 
-export function CardSkeleton() {
-  return <div className="skeleton w-3/5 h-96 my-6"></div>;
-}
+export const CardSkeleton = () => <div className="skeleton w-3/5 h-96 my-6"></div>;
