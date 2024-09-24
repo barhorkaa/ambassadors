f3a4148336f0b8dd081a312ed7510398be88ec7b
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-09-24 17:09:56 +0200

    feat(ui/footer): added bug report info to footer

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -1,8 +1,23 @@
+import { auth } from '@/auth';
+import Link from 'next/link';
+
 export async function Footer() {
+  const session = await auth();
+
   return (
     <footer className="footer footer-center p-4 bg-fi-100 text-black">
       <aside>
         <p>Copyright © 2024 - All right reserved by OVVSP FI MU</p>
+        {session && (
+          <p>
+            V případě, že najdete v aplikaci nějaký bug, nesrovnalost, nebo vám napadne nějaká feature, která by do
+            aplikace mohla přidat vyplňte prosím{' '}
+            <Link href={''} className="link">
+              Google formulář
+            </Link>{' '}
+            a co nejdříve se tomu budeme věnovat.
+          </p>
+        )}
       </aside>
     </footer>
   );
