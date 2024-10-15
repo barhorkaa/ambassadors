edc21741642bc71afe4988fe65bac4a9b00a06bf
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-15 18:03:35 +0200

    feat(migrations/eventGroupEmails): added table to hold group emails with additional info about event

---
+++ database/migrations/20241015-1756-group-emails.ts
@@ -0,0 +1,15 @@
+import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('eventGroupEmails')
+    .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn('event_id', 'char(36)', (col) => col.references('event.id').notNull())
+    .addColumn('contents', 'varchar')
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('eventGroupEmails').execute();
+}
