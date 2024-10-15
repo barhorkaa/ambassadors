7f91ef95fccc2dfa5904b4231f99d89c2b62fb3a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-15 17:26:37 +0200

    feat(events/id/layout): changed layout to include conditional slots rendering to declutter UI

--- app/events/(without-layout)/[id]/layout.tsx
+++ app/events/(without-layout)/[id]/layout.tsx
@@ -8,12 +8,11 @@ export const metadata: Metadata = {
 
 interface LayoutProps {
   event: React.ReactNode;
-  type: React.ReactNode;
   users: React.ReactNode;
-  report: React.ReactNode;
+  info: React.ReactNode;
 }
 
-export default function Layout({ children, event, type, users, report }: BaseLayoutProps & LayoutProps) {
+export default function Layout({ children, event, users, info }: BaseLayoutProps & LayoutProps) {
   return (
     <section>
       <div className="align-text-base flex flex-row gap-4">
@@ -26,8 +25,7 @@ export default function Layout({ children, event, type, users, report }: BaseLay
         <div className="grid grid-cols-[min-content_min-conent] gap-4 md:grid-cols-9 md:grid-rows-[min-content_min-conent]">
           <div className="row-start-1 md:col-span-6">{event}</div>
           <div className="md:row-start-1 md:row-span-2 md:col-start-7 md:col-span-4">{users}</div>
-          <div className="row-start-3 md:row-start-2 md:col-start-1 md:col-span-6">{type}</div>
-          <div className="md:col-start-2 md:row-start-3  md:col-span-5 ">{report}</div>
+          <div className="md:row-start-2 md:col-start-1 md:col-span-6">{info}</div>
         </div>
       </div>
     </section>
