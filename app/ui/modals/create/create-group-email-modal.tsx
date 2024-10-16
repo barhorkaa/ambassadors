51c2b154360234d771220aebbbc050acfc807b2a
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-16 08:17:59 +0200

    feat(modals/createGroupEmail): created modal for creating a group email

---
+++ app/ui/modals/create/create-group-email-modal.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import GroupEmailForm from '@/app/ui/event-group-emails/group-email-form';
+import ModalLayout from '@/app/ui/modals/modal-layout';
+
+export default function CreateGroupEmailModal({ eventId }: { eventId: string }) {
+  return (
+    <ModalLayout
+      id={'create_group_email_modal'}
+      title={'Hromadný e-mail pro přihlášené uživatele'}
+      modalType={'create'}
+      buttonTitle={'Poslat hromadný e-mail'}
+    >
+      <GroupEmailForm eventId={eventId} />
+      <p className="text-sm">
+        E-mail se pošle všem uživatelům, kteří jsou přihlášeni na akci i náhradníkům. Správa se také uloží do sekce
+        stránky "Doplňující informace" a bude dostupná všem ambasadorům, kteří se na akci podívají.
+      </p>
+    </ModalLayout>
+  );
+}
