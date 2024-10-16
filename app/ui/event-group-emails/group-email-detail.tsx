d2d31ac4d8c35310a70ab04bb0fbccba4c227dfc
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-16 16:38:48 +0200

    feat(ui/groupEmailDetail): adjusted detail component

--- app/ui/event-group-emails/group-email-detail.tsx
+++ app/ui/event-group-emails/group-email-detail.tsx
@@ -1,7 +1,13 @@
-export default function GroupEmailDetail() {
+import { EventGroupEmailModel } from '@/models/event-group-emails-models';
+import parse from 'html-react-parser';
+
+export default function GroupEmailDetail({ email }: { email: EventGroupEmailModel }) {
   return (
-    <>
-      <p>tu bude detail</p>
-    </>
+    <div className="data-display">
+      <div className="card-body">
+        <p className="text-sm underline">Zasláno: {email.createdAt?.toLocaleDateString()}</p>
+        {parse(email.contents)}
+      </div>
+    </div>
   );
 }
