a7da13916b2ff6da8b68f7a8e770fdb0dba92342
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-16 08:15:52 +0200

    feat(models/eventGroupEmails): created models for group emails

---
+++ models/event-group-emails-models.ts
@@ -0,0 +1,12 @@
+export type EventGroupEmailModel = {
+  id: string;
+  eventId: string;
+  contents: string;
+  createdAt: Date;
+};
+
+export type EventGroupEmailSendingModel = EventGroupEmailModel & {
+  title: string;
+  preview?: string;
+  recipients: string[];
+};
