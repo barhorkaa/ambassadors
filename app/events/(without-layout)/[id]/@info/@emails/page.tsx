d08b2bcfa9c56b9e9ede22517516231f80434f77
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-16 16:35:14 +0200

    feat(events/id/@info/@emails/page): added emails displaying

--- app/events/(without-layout)/[id]/@info/@emails/page.tsx
+++ app/events/(without-layout)/[id]/@info/@emails/page.tsx
@@ -1,7 +1,14 @@
-export default function Page({ params }: { params: { id: string } }) {
+import GroupEmailDetail from '@/app/ui/event-group-emails/group-email-detail';
+import { getEventGroupEmails } from '@/database/repository/event-group-emails';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const eventEmails = await getEventGroupEmails(params.id);
+
   return (
     <>
-      <p>tu budu emaily</p>
+      {eventEmails.map((email) => (
+        <GroupEmailDetail email={email} />
+      ))}
     </>
   );
 }
