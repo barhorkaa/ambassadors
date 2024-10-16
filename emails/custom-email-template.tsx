b09abc9baa0a5b640020d0834a9c8027828bfbc3
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-16 16:32:24 +0200

    feat(emails/customEmail): changed template to include parsed content

--- emails/custom-email-template.tsx
+++ emails/custom-email-template.tsx
@@ -1,16 +1,16 @@
 import EmailSkeleton from '@/emails/utils/email-skeleton';
-import { Text } from '@react-email/components';
+import parse from 'html-react-parser';
 
 interface CustomEmailProps {
   title: string;
   preview?: string;
-  contents: string[];
+  contents: string;
 }
 
 const CustomEmailTemplate = ({ title, contents, preview = title }: CustomEmailProps) => (
   <EmailSkeleton title={title} preview={preview} includeFooter={false}>
-    {contents.map((content, index) => (
-      <Text key={index}>{content}</Text>
-    ))}
+    {parse(contents)}
   </EmailSkeleton>
 );
+
+export default CustomEmailTemplate;
