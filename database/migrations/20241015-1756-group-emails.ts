d677e1266f6412756490e61b0e9b3d1f5fda6cd6
Barbora Hornakova <xhornak2@fi.muni.cz>
2024-10-16 16:30:40 +0200

    fix(migrations/groupEmail): added not null and default

--- database/migrations/20241015-1756-group-emails.ts
+++ database/migrations/20241015-1756-group-emails.ts
@@ -5,7 +5,7 @@ export async function up(db: Kysely<any>): Promise<void> {
     .createTable('eventGroupEmails')
     .addColumn('id', 'char(36)', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
     .addColumn('event_id', 'char(36)', (col) => col.references('event.id').notNull())
-    .addColumn('contents', 'varchar')
+    .addColumn('contents', 'varchar', (col) => col.notNull().defaultTo(''))
     .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
     .execute();
 }
