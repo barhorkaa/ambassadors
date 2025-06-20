fa7e896646a8eccfcc1917f824274a55923b6f52
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-20 20:09:10 +0200

    fix(events/id/@info/emails/page): explicit type cast

--- app/events/(without-layout)/[id]/@info/emails/page.tsx
+++ app/events/(without-layout)/[id]/@info/emails/page.tsx
@@ -1,8 +1,9 @@
 import GroupEmailDetail from '@/app/ui/event-group-emails/group-email-detail';
 import { getEventGroupEmails } from '@/database/repository/event-group-emails';
+import { EventGroupEmailModel } from '@/models/event-group-emails-models';
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const eventEmails = await getEventGroupEmails(params.id);
+  const eventEmails: EventGroupEmailModel[] = await getEventGroupEmails(params.id);
 
   return (
     <>
