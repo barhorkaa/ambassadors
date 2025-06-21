0db949fad2efb29d84f55f511a4dd7511954e0af
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-21 14:01:25 +0200

    feat(ambassadors/wo/layout): changed back navigation based on user role

--- app/ambassadors/(without-layout)/[id]/layout.tsx
+++ app/ambassadors/(without-layout)/[id]/layout.tsx
@@ -2,6 +2,7 @@ import BackNavigation from '@/app/ui/layout/back-navigation';
 import { BaseLayoutProps } from '@/app/utils/interface-props';
 import { UserRoles } from '@/app/utils/user-roles';
 import { auth } from '@/auth';
+import { getUserById } from '@/database/repository/user';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -12,16 +13,21 @@ interface LayoutProps {
   motivation: React.ReactNode;
   user: React.ReactNode;
   signups: React.ReactNode;
+  params: { id: string };
 }
 
-export default async function Layout({ children, motivation, user, signups }: BaseLayoutProps & LayoutProps) {
+export default async function Layout({ children, motivation, user, signups, params }: BaseLayoutProps & LayoutProps) {
   const session = await auth();
+  const shownUser = await getUserById(params.id);
 
   return (
     <section>
       <div className="align-text-bottom flex flex-row gap-4">
         {session?.user.role === UserRoles.manager && (
-          <BackNavigation href={'/ambassadors'} tooltip={'Zpátky na Uživatele'} />
+          <BackNavigation
+            href={shownUser.role === UserRoles.ambassador ? '/ambassadors' : '/ambassadors/managers'}
+            tooltip={'Zpátky na Uživatele'}
+          />
         )}
         <h1 className="font-light text-xl">Informace o uživateli</h1>
       </div>
