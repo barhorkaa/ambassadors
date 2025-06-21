e384b3ef38cc0ea3f53275f25542227f6dc002fa
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-21 14:31:33 +0200

    feat(events/w/page): made event list the default page

--- app/events/(with-layout)/page.tsx
+++ app/events/(with-layout)/page.tsx
@@ -1,35 +1,21 @@
-import { SectionInfo } from '@/app/ui/utils/data-display';
+import { EventList } from '@/app/ui/utils/content-list';
+import { TableSkeleton } from '@/app/ui/utils/skeletons';
+import { getAllActiveEvents } from '@/database/repository/event';
+import { EventModel } from '@/models/event-models';
+import { Suspense } from 'react';
 
-export default function Page() {
+export default async function Page() {
+  const allEvents: EventModel[] = await getAllActiveEvents(true);
+
   return (
-    <>
-      <SectionInfo
-        title={'Informace o akcích'}
-        contents={[
-          'Jako ambasador se můžeš podílet na spoustě akcí pořádaných na fakultě, ale i mimo ní. ' +
-            'Počet akcí, na které pojedeš počas semestru je jedině na tobě!',
-        ]}
+    <Suspense fallback={<TableSkeleton />}>
+      <EventList
+        title={'Aktivní akce'}
+        list={allEvents}
+        emptyMessage={
+          'Momentálně nejsou k dispozici žádné akce. Jestli chceš někam jet, vytvoř návrh na novou akci stlačením tlačítka Přidat.'
+        }
       />
-      <hr />
-      <SectionInfo
-        title={'Jak se na akci přihlásit?'}
-        contents={[
-          'V sekci Aktivní akce si najdi akci, které byses se chtěl/a zúčastnit a rozklikni si její detail. Na ' +
-            'stránce detailu uvidíš tlačítko Přihlásit se, kterým se přihlásíš na danou akci. Tvoje přihlášení poté ' +
-            'musí zchválit manažer, po schválení se u tvojeho jména objeví ikonka Check. To znamená, že oddělení o tvém ' +
-            'zájmu ví a počíta s tebou.',
-        ]}
-      />
-      <hr />
-      <SectionInfo
-        title={'Založení nové akce'}
-        contents={[
-          'Pokud v sekci Aktivní akce nevidíš akci, na kterou bys chtěl/a jet, můžeš založit nový návrh na akci. ' +
-            'Jednoduše klikni na Přidat v pravé horní části této stránky a přidej novou akci. Tato akce se poté objeví ' +
-            'v sekci Nepotvrzené akce. Váš návrh bude muset opět schválit manažer a potom, co se tomu tak stane, se akce ' +
-            'přesune do sekce Aktivbí akce a budeš se na ní moc přihlásit.',
-        ]}
-      />
-    </>
+    </Suspense>
   );
 }
