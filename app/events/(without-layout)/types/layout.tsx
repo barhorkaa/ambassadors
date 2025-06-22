dc230555c5daf0c20e481a0cfa20e076b09c07e2
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-22 13:43:28 +0200

    feat(events/wo/types/layout): changed to page navigation

--- app/events/(without-layout)/types/layout.tsx
+++ app/events/(without-layout)/types/layout.tsx
@@ -1,37 +1,36 @@
+import PageNavigation from '@/app/ui/layout/page-navigation';
 import CreateEventTypeModal from '@/app/ui/modals/create/create-event-type-modal';
 import { BaseLayoutProps } from '@/app/utils/interface-props';
+import { PageUrl } from '@/app/utils/pages';
 import { UserRoles } from '@/app/utils/user-roles';
 import { auth } from '@/auth';
 import { Metadata } from 'next';
-import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Druhy akcí | AmbassadorsFIMU',
 };
 
+const eventTypePages: PageUrl[] = [
+  { name: 'Informace', url: '/events/types/info' },
+  { name: 'Dostupné druhy akcí', url: '/events/types' },
+];
+
 export default async function Layout({ children }: BaseLayoutProps) {
   const session = await auth();
+  const isManager = session?.user.role === UserRoles.manager;
+
+  if (isManager) {
+    eventTypePages.push({ name: 'Vymazané druhy akcí', url: '/events/types/deleted' });
+  }
 
   return (
     <section>
       <div className="flex flex-row justify-between content items-end">
         <h1>Druhy akcí</h1>
-        {session?.user.role === UserRoles.manager && <CreateEventTypeModal />}
+        {isManager && <CreateEventTypeModal />}
       </div>
       <hr className="w-full mb-0" />
-      <ul className="page-menu">
-        <li>
-          <Link href={'/events/types'}>Informace</Link>
-        </li>
-        <li>
-          <Link href={'/events/types/all'}>Dostupné druhy akcí</Link>
-        </li>
-        {session?.user.role === UserRoles.manager && (
-          <li>
-            <Link href={'/events/types/deleted'}>Vymazané druhy akcí</Link>
-          </li>
-        )}
-      </ul>
+      <PageNavigation pages={eventTypePages} />
       <div className="content">{children}</div>
     </section>
   );
