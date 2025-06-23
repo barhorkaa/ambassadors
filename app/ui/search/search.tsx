b511ccdbc4b6ae34ba43f6acd36cb75d1b285a2d
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-23 14:46:52 +0200

    feat(ui/search): created a search field

---
+++ app/ui/search/search.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
+export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+    console.log(term);
+  }, 300);
+
+  return (
+    <label className="input w-full">
+      <MagnifyingGlassIcon className="h-5 w-5" />
+      <input
+        type="search"
+        required
+        placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
+      />
+    </label>
+  );
+}
