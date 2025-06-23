11f2db8597ffaf44a916e7189b5a351bbe93f0b2
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-23 14:50:12 +0200

    feat(events/wo/page): added search and pagination to main events page

--- app/events/(with-layout)/page.tsx
+++ app/events/(with-layout)/page.tsx
@@ -1,21 +1,40 @@
+import Pagination from '@/app/ui/search/pagination';
+import Search from '@/app/ui/search/search';
 import { EventList } from '@/app/ui/utils/content-list';
 import { TableSkeleton } from '@/app/ui/utils/skeletons';
-import { getAllActiveEvents } from '@/database/repository/event';
+import { getAllFilteredActiveEvents, getAllFilteredActiveEventsCount } from '@/database/repository/event';
 import { EventModel } from '@/models/event-models';
 import { Suspense } from 'react';
 
-export default async function Page() {
-  const allEvents: EventModel[] = await getAllActiveEvents(true);
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const allFilteredEvents: EventModel[] = await getAllFilteredActiveEvents(true, query, currentPage);
+  const eventsPages = await getAllFilteredActiveEventsCount(true, query);
 
   return (
-    <Suspense fallback={<TableSkeleton />}>
+    <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+      <h2 className="pb-2">Aktivní akce</h2>
+      <div className="flex flex-col gap-2">
+        <Search placeholder={'Vyhledat akci'} />
+        <Pagination totalPages={eventsPages} />
+      </div>
+
       <EventList
         title={'Aktivní akce'}
-        list={allEvents}
+        list={allFilteredEvents}
         emptyMessage={
           'Momentálně nejsou k dispozici žádné akce. Jestli chceš někam jet, vytvoř návrh na novou akci stlačením tlačítka Přidat.'
         }
       />
+      <Pagination totalPages={eventsPages} />
     </Suspense>
   );
 }
