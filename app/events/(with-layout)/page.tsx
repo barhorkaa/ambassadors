130f63725c3c71c4a83975057da25281e28eb6a1
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-27 01:06:35 +0200

    feat(events/wo/page): added date search

--- app/events/(with-layout)/page.tsx
+++ app/events/(with-layout)/page.tsx
@@ -1,3 +1,4 @@
+import DateSearch from '@/app/ui/search/date-search';
 import Pagination from '@/app/ui/search/pagination';
 import Search from '@/app/ui/search/search';
 import { EventList } from '@/app/ui/utils/content-list';
@@ -9,24 +10,29 @@ import { Suspense } from 'react';
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
+    dateFrom?: string;
+    dateTo?: string;
     page?: string;
   }>;
 }) {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
+  const dateFrom = new Date(searchParams?.dateFrom || '2000-01-01');
+  const dateTo = new Date(searchParams?.dateTo || '3000-01-01');
   const currentPage = Number(searchParams?.page) || 1;
 
-  const allFilteredEvents: EventModel[] = await getAllFilteredActiveEvents(true, query, currentPage);
-  const eventsPages = await getAllFilteredActiveEventsCount(true, query);
+  const allFilteredEvents: EventModel[] = await getAllFilteredActiveEvents(true, query, currentPage, dateFrom, dateTo);
+  const eventsPages = await getAllFilteredActiveEventsCount(true, query, dateFrom, dateTo);
 
   return (
     <>
       <h2 className="pb-2">Aktivní akce</h2>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-row gap-2">
         <Search placeholder={'Vyhledat akci'} />
+        <DateSearch />
       </div>
       <Pagination totalPages={eventsPages} />
-      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+      <Suspense key={query + currentPage + dateFrom + dateTo} fallback={<TableSkeleton />}>
         <EventList
           title={'Aktivní akce'}
           list={allFilteredEvents}
