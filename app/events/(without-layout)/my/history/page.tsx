4b039eb1d9cf246fa061e87d21f8234b738f1746
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-27 17:51:56 +0200

    feat(events/wo/my/history/page): added search and pagination

--- app/events/(without-layout)/my/history/page.tsx
+++ app/events/(without-layout)/my/history/page.tsx
@@ -1,26 +1,58 @@
+import DateSearch from '@/app/ui/search/date-search';
+import Pagination from '@/app/ui/search/pagination';
+import Search from '@/app/ui/search/search';
 import { EventList } from '@/app/ui/utils/content-list';
+import { TableSkeleton } from '@/app/ui/utils/skeletons';
 import { auth } from '@/auth';
-import { getUserSignUps } from '@/database/repository/event-user';
-import { EventModel } from '@/models/event-models';
+import { getUserSignUps, getUserSignUpsCount } from '@/database/repository/event-user';
+import { EventUserStateModel } from '@/models/event-models';
+import { Suspense } from 'react';
 
-export default async function Page() {
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    dateFrom?: string;
+    dateTo?: string;
+    page?: string;
+  }>;
+}) {
   const session = await auth();
 
-  const userEvents: EventModel[] = await getUserSignUps(session!.user.id, false, false);
-  const userSubstitutes: EventModel[] = await getUserSignUps(session!.user.id, true, false);
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const dateFrom = new Date(searchParams?.dateFrom || '2000-01-01');
+  const dateTo = new Date(searchParams?.dateTo || '3000-01-01');
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const userEvents: EventUserStateModel[] = await getUserSignUps(
+    session!.user.id,
+    false,
+    false,
+    query,
+    currentPage,
+    dateFrom,
+    dateTo
+  );
+  const eventsPages = await getUserSignUpsCount(session!.user.id, false, false, query, dateFrom, dateTo);
 
   return (
     <>
-      <EventList
-        title={'Akce, kde jsem byl/a přihlášen/a'}
-        list={userEvents}
-        emptyMessage={'Zatím jsi nebyl/a na žádné akci.'}
-      />
-      <EventList
-        title={'Akce, kde jsem byl/a náhradník'}
-        list={userSubstitutes}
-        emptyMessage={'Zatím jsi nebyl/a nahraníkem na žádné akci.'}
-      />
+      <h2 className="pb-2">Historie mých přihlášení</h2>
+      <div className="flex flex-col md:flex-row gap-4 md:gap-2">
+        <Search placeholder={'Vyhledat akci'} />
+        <DateSearch />
+      </div>
+      <Pagination totalPages={eventsPages} />
+      <Suspense key={query + currentPage + dateFrom + dateTo} fallback={<TableSkeleton />}>
+        <EventList
+          title={'Akce, kde jsem přihlášen/a'}
+          list={userEvents}
+          emptyMessage={
+            searchParams ? 'Žádná tvá akce nevyhovuje hledanému pojmu.' : 'Zatím jsi nebyl/a na žádné akci.'
+          }
+        />
+      </Suspense>
+      <Pagination totalPages={eventsPages} />
     </>
   );
 }
