b8b5a0d91ae9990b9404abb21665d2bc50f7274a
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-27 15:08:13 +0200

    feat(events/wo/unapproved/page): added search and pagination to unapproved events page

--- app/events/(with-layout)/unapproved/page.tsx
+++ app/events/(with-layout)/unapproved/page.tsx
@@ -1,15 +1,47 @@
+import DateSearch from '@/app/ui/search/date-search';
+import Pagination from '@/app/ui/search/pagination';
+import Search from '@/app/ui/search/search';
 import { EventList } from '@/app/ui/utils/content-list';
 import { TableSkeleton } from '@/app/ui/utils/skeletons';
-import { getAllActiveEvents } from '@/database/repository/event';
+import { getAllFilteredActiveEvents, getAllFilteredActiveEventsCount } from '@/database/repository/event';
 import { EventModel } from '@/models/event-models';
 import { Suspense } from 'react';
 
-export default async function Page() {
-  const allUnapprovedEvents: EventModel[] = await getAllActiveEvents(false);
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    dateFrom?: string;
+    dateTo?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const dateFrom = new Date(searchParams?.dateFrom || '2000-01-01');
+  const dateTo = new Date(searchParams?.dateTo || '3000-01-01');
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const allFilteredEvents: EventModel[] = await getAllFilteredActiveEvents(false, query, currentPage, dateFrom, dateTo);
+  const eventsPages = await getAllFilteredActiveEventsCount(false, query, dateFrom, dateTo);
 
   return (
-    <Suspense fallback={<TableSkeleton />}>
-      <EventList title={'Nepotvrzené akce'} list={allUnapprovedEvents} emptyMessage={'Všechny akce jsou potvrzené!'} />
-    </Suspense>
+    <>
+      <h2 className="pb-2">Nepotvrzené akce</h2>
+      <div className="flex flex-col md:flex-row gap-4 md:gap-2">
+        <Search placeholder={'Vyhledat akci'} />
+        <DateSearch />
+      </div>
+      <Pagination totalPages={eventsPages} />
+      <Suspense key={query + currentPage + dateFrom + dateTo} fallback={<TableSkeleton />}>
+        <EventList
+          title={'Aktivní akce'}
+          list={allFilteredEvents}
+          emptyMessage={
+            query !== null ? 'Nemáme žádnou akci, ktrá by vyhovovala hledanému pojmu.' : 'Všechny akce jsou potvrzené!'
+          }
+        />
+      </Suspense>
+      <Pagination totalPages={eventsPages} />
+    </>
   );
 }
