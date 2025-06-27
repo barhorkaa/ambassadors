2f0ea69ea57a7dbbbce0ed5c61be3086148d07cf
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-06-27 17:44:19 +0200

    feat(events/wo/my/page): added search and pagination

--- app/events/(without-layout)/my/page.tsx
+++ app/events/(without-layout)/my/page.tsx
@@ -1,23 +1,61 @@
+import DateSearch from '@/app/ui/search/date-search';
+import Pagination from '@/app/ui/search/pagination';
+import Search from '@/app/ui/search/search';
 import { HeroCenterLayout } from '@/app/ui/utils/component-layouts';
 import { EventList } from '@/app/ui/utils/content-list';
+import { TableSkeleton } from '@/app/ui/utils/skeletons';
 import { auth } from '@/auth';
-import { getUserSignUps } from '@/database/repository/event-user';
+import { getUserSignUps, getUserSignUpsCount } from '@/database/repository/event-user';
 import { EventUserStateModel } from '@/models/event-models';
+import { Suspense } from 'react';
 
-export default async function Page() {
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    dateFrom?: string;
+    dateTo?: string;
+    page?: string;
+  }>;
+}) {
   const session = await auth();
 
-  const userEvents: EventUserStateModel[] = await getUserSignUps(session!.user.id, false, true);
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const dateFrom = new Date(searchParams?.dateFrom || '2000-01-01');
+  const dateTo = new Date(searchParams?.dateTo || '3000-01-01');
+  const currentPage = Number(searchParams?.page) || 1;
 
-  if (userEvents.length === 0) return <SignUpPrompt />;
+  const userEvents: EventUserStateModel[] = await getUserSignUps(
+    session!.user.id,
+    false,
+    true,
+    query,
+    currentPage,
+    dateFrom,
+    dateTo
+  );
+  const eventsPages = await getUserSignUpsCount(session!.user.id, false, true, query, dateFrom, dateTo);
+
+  if (userEvents.length === 0 && !searchParams) return <SignUpPrompt />;
 
   return (
     <>
-      <EventList
-        title={'Akce, kde jsem přihlášen/a'}
-        list={userEvents}
-        emptyMessage={'Zatím nejsi přihlášen/a na žádnou akci.'}
-      />
+      <h2 className="pb-2">Moje aktuální přihlášení</h2>
+      <div className="flex flex-col md:flex-row gap-4 md:gap-2">
+        <Search placeholder={'Vyhledat akci'} />
+        <DateSearch />
+      </div>
+      <Pagination totalPages={eventsPages} />
+      <Suspense key={query + currentPage + dateFrom + dateTo} fallback={<TableSkeleton />}>
+        <EventList
+          title={'Akce, kde jsem přihlášen/a'}
+          list={userEvents}
+          emptyMessage={
+            searchParams ? 'Žádná tvá akce nevyhovuje hledanému pojmu.' : 'Zatím nejsi přihlášen/a na žádnou akci.'
+          }
+        />
+      </Suspense>
+      <Pagination totalPages={eventsPages} />
     </>
   );
 }
