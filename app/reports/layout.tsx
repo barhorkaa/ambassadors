79d9b175db81cc35bf4cccd5281599c73a1431dc
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-07-11 17:21:37 +0200

    feat(reports/layout): added a component for info dropdowns

--- app/reports/layout.tsx
+++ app/reports/layout.tsx
@@ -1,5 +1,5 @@
+import InfoDropDown from '@/app/ui/utils/info-dropdown';
 import { BaseLayoutProps } from '@/app/utils/interface-props';
-import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -7,30 +7,18 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({ children }: BaseLayoutProps) {
+  const info =
+    'V této sekci aplikace naleznete seznam všech akcí, které nemají potvrzenou zprávu. Klinutím na nějakou akci se ' +
+    'dostanete na její detail, kde si budete moct zprávu přečíst a potvrdit.';
+
   return (
     <section>
       <div className="flex flex-row justify-between items-center">
         <h1 className="content">Nové zprávy z akcí</h1>
-        <ReportsInfoDropDown />
+        <InfoDropDown info={info} />
       </div>
       <hr className="w-full mb-8" />
       <div className="content">{children}</div>
     </section>
   );
 }
-
-const ReportsInfoDropDown = () => (
-  <div className="dropdown dropdown-end dropdown-hover">
-    <div tabIndex={0} role="button">
-      <QuestionMarkCircleIcon className="h-5" />
-    </div>
-    <div tabIndex={0} className="card dropdown-content bg-base-100 rounded-none z-1 w-80 shadow-sm">
-      <div tabIndex={0} className="card-body">
-        <p>
-          V této sekci aplikace naleznete seznam všech akcí, které nemají potvrzenou zprávu. Klinutím na nějakou akci se
-          dostanete na její detail, kde si budete moct zprávu přečíst a potvrdit.
-        </p>
-      </div>
-    </div>
-  </div>
-);
