9bf095819b67bf67f132c1d1c14feef1ecce51ef
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-07-11 17:02:50 +0200

    feat(reports): added search and pagination to reports

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,14 +1,49 @@
+import DateSearch from '@/app/ui/search/date-search';
+import Pagination from '@/app/ui/search/pagination';
+import Search from '@/app/ui/search/search';
 import { EventList } from '@/app/ui/utils/content-list';
-import { getAllHistoryEvents } from '@/database/repository/event';
+import { TableSkeleton } from '@/app/ui/utils/skeletons';
+import { getAllHistoryEvents, getAllHistoryEventsCount } from '@/database/repository/event';
+import { Suspense } from 'react';
 
-export default async function Page() {
-  const eventsWithUnapprovedReports = await getAllHistoryEvents(false);
+export default async function Page(
+  props: {
+    searchParams?: Promise<{
+      query?: string;
+      dateFrom?: string;
+      dateTo?: string;
+      page?: string;
+    }>;
+  } & { params: { id: string } }
+) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const dateFrom = new Date(searchParams?.dateFrom || '2000-01-01');
+  const dateTo = new Date(searchParams?.dateTo || '3000-01-01');
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const allFilteredEvents = await getAllHistoryEvents(false, query, currentPage, dateFrom, dateTo);
+  const eventsPages = await getAllHistoryEventsCount(true, query, dateFrom, dateTo);
 
   return (
-    <EventList
-      title={''}
-      list={eventsWithUnapprovedReports!}
-      emptyMessage={'V tento moment jsou všechny zprávy potvrzeny!'}
-    />
+    <>
+      <div className="flex flex-col md:flex-row gap-4 md:gap-2">
+        <Search placeholder={'Vyhledat akci'} />
+        <DateSearch />
+      </div>
+      <Pagination totalPages={eventsPages!} />
+      <Suspense key={query + currentPage + dateFrom + dateTo} fallback={<TableSkeleton />}>
+        <EventList
+          title={''}
+          list={allFilteredEvents!}
+          emptyMessage={
+            query !== null
+              ? 'Nemáme žádnou akci, ktrá by vyhovovala hledanému pojmu.'
+              : 'V tento moment jsou všechny zprávy potvrzeny!'
+          }
+        />
+      </Suspense>
+      <Pagination totalPages={eventsPages!} />
+    </>
   );
 }
