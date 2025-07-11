3043516fcabdcd6e9fdda3f913873e37aacf7a0f
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-07-11 16:16:14 +0200

    feat(ui/layout/pageNavigation): added separate info icon

--- app/ui/layout/page-navigation.tsx
+++ app/ui/layout/page-navigation.tsx
@@ -1,18 +1,27 @@
 import { PageUrl } from '@/app/utils/pages';
+import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 interface PageNavigationProps {
   pages: PageUrl[];
+  infoPageUrl: string;
 }
 
-export default function PageNavigation({ pages }: PageNavigationProps) {
+export default function PageNavigation({ pages, infoPageUrl }: PageNavigationProps) {
   return (
-    <ul className="page-menu">
-      {pages.map((page, index) => (
-        <li key={index}>
-          <Link href={page.url}>{page.name}</Link>
-        </li>
-      ))}
+    <ul className="menu menu-vertical lg:menu-horizontal bg-fi-100 text-black font-semibold w-full mb-4 flex flex-row justify-between">
+      <div className="flex flex-col md:flex-row">
+        {pages.map((page, index) => (
+          <li key={index}>
+            <Link href={page.url}>{page.name}</Link>
+          </li>
+        ))}
+      </div>
+      <li>
+        <Link href={infoPageUrl} className="self-center tooltip tooltip-bottom" data-tip="Informace o sekci">
+          <QuestionMarkCircleIcon className="h-5" />
+        </Link>
+      </li>
     </ul>
   );
 }
