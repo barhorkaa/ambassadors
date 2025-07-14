5790701d25cb88406c35bd021019bb4ce07d2be7
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-07-14 10:47:51 +0200

    feat(db/repo/materials): added functions to include search and pagination

--- database/repository/material.ts
+++ database/repository/material.ts
@@ -1,16 +1,43 @@
 import { db } from '@/database/database';
 import { DatabaseError } from '@/database/errors/database-error';
+import { LIMITED_ITEMS_PER_PAGE } from '@/database/repository/utils/consts';
 import { MaterialManipulationModel } from '@/models/material-models';
 import { objectToCamel } from 'ts-case-convert';
 
-export async function getAllMaterials(deleted: boolean) {
+export async function getAllFilteredMaterials(deleted: boolean, query: string, currentPage: number) {
+  const offset = (currentPage - 1) * LIMITED_ITEMS_PER_PAGE;
+
   try {
     const result = await db
       .selectFrom('material')
       .where('deleted_at', deleted ? 'is not' : 'is', null)
+      .where((eb) =>
+        eb.or([eb('material.name', 'ilike', `%${query}%`), eb('material.description', 'ilike', `%${query}%`)])
+      )
+      .selectAll()
+      .limit(LIMITED_ITEMS_PER_PAGE)
+      .offset(offset)
+      .execute();
+
+    return objectToCamel(result);
+  } catch (e) {
+    console.error(e);
+    throw new DatabaseError({ name: 'DATABASE_GET_ERROR', message: 'Unable to get all materials', cause: e });
+  }
+}
+
+export async function getAllFilteredMaterialsCount(deleted: boolean, query: string) {
+  try {
+    const result = await db
+      .selectFrom('material')
+      .where('deleted_at', deleted ? 'is not' : 'is', null)
+      .where((eb) =>
+        eb.or([eb('material.name', 'ilike', `%${query}%`), eb('material.description', 'ilike', `%${query}%`)])
+      )
       .selectAll()
       .execute();
-    return objectToCamel(result);
+
+    return Math.ceil(result.length / LIMITED_ITEMS_PER_PAGE);
   } catch (e) {
     console.error(e);
     throw new DatabaseError({ name: 'DATABASE_GET_ERROR', message: 'Unable to get all materials', cause: e });
