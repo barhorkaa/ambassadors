37e4865f632466f9887e38ce579b70a3d041ff84
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-07-14 10:48:15 +0200

    feat(materials/deleted/page): added search and pagination

--- app/materials/deleted/page.tsx
+++ app/materials/deleted/page.tsx
@@ -1,15 +1,36 @@
+import Pagination from '@/app/ui/search/pagination';
+import Search from '@/app/ui/search/search';
 import { MaterialList } from '@/app/ui/utils/content-list';
-import { getAllMaterials } from '@/database/repository/material';
-import { MaterialDetailModel } from '@/models/material-models';
+import { TableSkeleton } from '@/app/ui/utils/skeletons';
+import { BasePageSearchProps } from '@/app/utils/interface-props';
+import { getAllFilteredMaterials, getAllFilteredMaterialsCount } from '@/database/repository/material';
+import { Suspense } from 'react';
 
-export default async function Page() {
-  const deletedMaterials: MaterialDetailModel[] = await getAllMaterials(true);
+export default async function Page(props: BasePageSearchProps) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const allMaterials = await getAllFilteredMaterials(true, query, currentPage);
+  const materialsPages = await getAllFilteredMaterialsCount(true, query);
 
   return (
-    <MaterialList
-      title={'Vymazané materiály'}
-      list={deletedMaterials}
-      emptyMessage={'Aktuálně jsou všechny materiály používány'}
-    />
+    <>
+      <h2 className="pb-2">Vymazané materiály</h2>
+      <Search placeholder={'Vyhledat materiál'} />
+      <Pagination totalPages={materialsPages} />
+      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+        <MaterialList
+          title={''}
+          list={allMaterials}
+          emptyMessage={
+            query !== null
+              ? 'Nemáme žádnou akci, ktrá by vyhovovala hledanému pojmu.'
+              : 'Aktuálně jsou všechny materiály používány.'
+          }
+        />
+      </Suspense>
+      <Pagination totalPages={materialsPages} />
+    </>
   );
 }
