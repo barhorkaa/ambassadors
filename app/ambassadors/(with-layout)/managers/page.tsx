3caa098f41c785ce9fc2d8c15cce856bb7877727
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-07-14 15:47:06 +0200

    feat(ams/w/managers/page): added search and pagination

--- app/ambassadors/(with-layout)/managers/page.tsx
+++ app/ambassadors/(with-layout)/managers/page.tsx
@@ -1,9 +1,36 @@
 import { UserList } from '@/app/ui/utils/content-list';
-import { getAllManagers } from '@/database/repository/user';
+import SearchPaginationLayout from '@/app/ui/utils/search-pagination-layout';
+import { BasePageSearchProps } from '@/app/utils/interface-props';
+import { UserRoles } from '@/app/utils/user-roles';
+import { getAllFilteredUsers, getAllFilteredUsersCount } from '@/database/repository/user';
 import { UserModel } from '@/models/user-models';
 
-export default async function Page() {
-  const allManagers: UserModel[] = await getAllManagers();
+export default async function Page(props: BasePageSearchProps) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
-  return <UserList title={'Manažeři'} list={allManagers} emptyMessage={'Aplikace nýní nemá, žádné manažery.'} />;
+  const allManagers: UserModel[] = await getAllFilteredUsers(UserRoles.manager, query, currentPage);
+  const managersPages = await getAllFilteredUsersCount(UserRoles.manager, query);
+
+  return (
+    <SearchPaginationLayout
+      title="Manažeři"
+      totalPages={managersPages}
+      query={query}
+      currentPage={currentPage}
+      placeHolder={'Vyhledat uživatele'}
+      includeDateSearch={false}
+    >
+      <UserList
+        title={''}
+        list={allManagers}
+        emptyMessage={
+          query !== null
+            ? 'Nemáme žádného uživatele, který by vyhovoval hledanému pojmu.'
+            : 'Aplikace nýní nemá, žádné manažery.'
+        }
+      />
+    </SearchPaginationLayout>
+  );
 }
