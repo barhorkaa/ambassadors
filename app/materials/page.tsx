7b301e213e3e61a5ae57a89cb7ceb3b1f84a0049
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-07-14 10:48:08 +0200

    feat(materials/page): added search and pagination

--- app/materials/page.tsx
+++ app/materials/page.tsx
@@ -1,15 +1,36 @@
+import Pagination from '@/app/ui/search/pagination';
+import Search from '@/app/ui/search/search';
 import { MaterialList } from '@/app/ui/utils/content-list';
-import { getAllMaterials } from '@/database/repository/material';
-import { MaterialDetailModel } from '@/models/material-models';
+import { TableSkeleton } from '@/app/ui/utils/skeletons';
+import { BasePageSearchProps } from '@/app/utils/interface-props';
+import { getAllFilteredMaterials, getAllFilteredMaterialsCount } from '@/database/repository/material';
+import { Suspense } from 'react';
 
-export default async function Page() {
-  const allMaterials: MaterialDetailModel[] = await getAllMaterials(false);
+export default async function Page(props: BasePageSearchProps) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const allMaterials = await getAllFilteredMaterials(false, query, currentPage);
+  const materialsPages = await getAllFilteredMaterialsCount(false, query);
 
   return (
-    <MaterialList
-      title={'Dostupné materiály'}
-      list={allMaterials}
-      emptyMessage={'Aktuálně nejsou k dispoici žádné materiály'}
-    />
+    <>
+      <h2 className="pb-2">Dostupné materiály</h2>
+      <Search placeholder={'Vyhledat materiál'} />
+      <Pagination totalPages={materialsPages} />
+      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+        <MaterialList
+          title={''}
+          list={allMaterials}
+          emptyMessage={
+            query !== null
+              ? 'Nemáme žádnou akci, ktrá by vyhovovala hledanému pojmu.'
+              : 'Aktuálně nejsou k dispoici žádné materiály.'
+          }
+        />
+      </Suspense>
+      <Pagination totalPages={materialsPages} />
+    </>
   );
 }
