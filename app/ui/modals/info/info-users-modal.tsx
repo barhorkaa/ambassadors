3e0b7fbfa6062802c2dffcbb0125f6fef1041766
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-08-07 18:20:41 +0200

    feat(ui/modals/info/users): added modal for partial user type info display

--- app/ui/modals/info/info-users-modal.tsx
+++ app/ui/modals/info/info-users-modal.tsx
@@ -1,10 +1,28 @@
-import UserInfo from '@/app/ui/info/users-info';
+import UserInfo, { AmbassadorsInfo, ManagersInfo, UnapprovedInfo } from '@/app/ui/info/users-info';
 import ModalLayout from '@/app/ui/modals/modal-layout';
+
+type PartialInfoType = 'unapproved' | 'ambassadors' | 'managers';
+
+type PartialUserInfoRecord = Record<PartialInfoType, React.ReactNode>;
+
+const partialModalContent: PartialUserInfoRecord = {
+  unapproved: <UnapprovedInfo />,
+  ambassadors: <AmbassadorsInfo />,
+  managers: <ManagersInfo />,
+};
 
 export default function InfoUsersModal() {
   return (
     <ModalLayout id="usersInfoModal" title="Uživatelé" modalType="info">
       <UserInfo />
+    </ModalLayout>
+  );
+}
+
+export function InfoPartialUserInfoModal({ type }: { type: PartialInfoType }) {
+  return (
+    <ModalLayout id={type} title="" modalType="info">
+      {partialModalContent[type]}
     </ModalLayout>
   );
 }
