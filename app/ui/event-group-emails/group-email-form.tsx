fe74de73ab28973e13ce41e00d8312610fcb8f51
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-08-08 12:20:49 +0200

    feat(ui/event/email/form): changed for new editor

--- app/ui/event-group-emails/group-email-form.tsx
+++ app/ui/event-group-emails/group-email-form.tsx
@@ -2,9 +2,9 @@
 
 import { creteGroupEmailAction } from '@/app/lib/actions/event-group-emails';
 import { findErrors, formActionInitialState } from '@/app/lib/actions/utils';
-import TextEditor from '@/app/ui/event-group-emails/text-editor';
 import { FormLayout } from '@/app/ui/utils/component-layouts';
 import FormControl from '@/app/ui/utils/form-control';
+import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor';
 import { useFormState } from 'react-dom';
 
 export default function GroupEmailForm({ eventId }: { eventId: string }) {
@@ -19,7 +19,12 @@ export default function GroupEmailForm({ eventId }: { eventId: string }) {
         errorMessage={findErrors('subject', state.errors)[0]}
         required={false}
       />
-      <TextEditor />
+      <div className="form-control">
+        <label className="label" htmlFor="event_type">
+          <span className="label-text">Obsah e-mailu</span>
+        </label>
+        <SimpleEditor initialContent="" />
+      </div>
       <input id="eventId" defaultValue={eventId} type="hidden" name="eventId" />
     </FormLayout>
   );
