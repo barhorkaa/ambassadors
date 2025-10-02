31117bee460ed69952f51d2afebf3f205a6ba51d
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-10-02 19:11:01 +0200

    fix(ui/utils/componentLayouts/form): made padding bigger

--- app/ui/utils/component-layouts.tsx
+++ app/ui/utils/component-layouts.tsx
@@ -12,7 +12,7 @@ interface FormLayoutProps {
 }
 
 export const FormLayout = ({ action, state, modalId, children, buttonTitle }: FormLayoutProps) => (
-  <form action={action} className="card-body p-2 md:p-6">
+  <form action={action} className="card-body p-4 md:p-6">
     {children}
     <SubmitButton title={buttonTitle ? buttonTitle : 'Odeslat'} modalId={modalId} fun={action} />
     {state.generic !== undefined && (
