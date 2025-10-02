6311a0513b7bd330ebcb63d6ca75a599f658529a
Barbora Hornakova <xhornak2@fi.muni.cz>
2025-10-02 18:48:17 +0200

    feat(ui/auth/loginForm): added form layout to the form to unify with others

--- app/ui/auth/login-form.tsx
+++ app/ui/auth/login-form.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import { authenticate } from '@/app/lib/actions/authentication';
-import SubmitButton from '@/app/ui/button/submit-button';
 import FormControl from '@/app/ui/utils/form-control';
-import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { useFormState } from 'react-dom';
+import { FormLayout } from '@/app/ui/utils/component-layouts';
+import { formActionInitialState } from '@/app/lib/actions/utils';
 
 export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+  const [errorMessage, dispatch] = useFormState(authenticate, formActionInitialState);
 
   return (
-    <form action={dispatch} className="card-body">
+    <FormLayout action={dispatch} state={errorMessage} buttonTitle="Přihlásit se">
       <FormControl title={'E-mail'} id={'email'} />
       <FormControl title={'Heslo'} id={'password'} type={'password'} />
       <div className="label">
@@ -19,13 +19,6 @@ export default function LoginForm() {
           <Link href={'/password'}>Zapoměni jste heslo?</Link>
         </span>
       </div>
-      <SubmitButton title={'Přihlásit se'} />
-      {errorMessage && (
-        <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
-          <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-          <p className="text-sm text-red-500">{errorMessage}</p>
-        </div>
-      )}
-    </form>
+    </FormLayout>
   );
 }
